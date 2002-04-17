f3ebaeeee4979bac1fc6cf5cea3bdcb6e30eb1b3
Mohamed Fawzi <mohamed@fhi-berlin.mpg.de>
2002-04-17 13:07:10 +0000

    renamed instatiateTemplates.py to instantiateTemplates.py prepare_ci.py more comments, more modular. added checkout preparation script.
    svn-origin-rev: 305

--- tools/instantiateTemplates.py
+++ tools/instantiateTemplates.py
@@ -1,14 +1,14 @@
-#!/apps/python/bin/python
+#! /usr/bin/env python
 
 import sys
 from sys import argv
 
-def instantiateTemplate(infile,outfile,subs):
+def instantiateTemplate(infile,outfile,subs,logFile=sys.stdout):
   import re
   # this ignores a unbalanced '[' or ']' and ignores a comment
   # like [ ! ], but well,...
   directive=re.compile("(\[[ \t]*([^\]]*)[ \t]*\])|(^[ \t]*![ \t]*\[[ \t]*template[ \t]*\(([^\)]*)\)\][ \t]*$)|(!)")
-  print "instantiating '",infile.name,"' to '",outfile.name,"'"
+  logFile.write("instantiating '"+infile.name+"' to '"+outfile.name+"'\n")
   lineNr=0
   while 1:
     line=infile.readline()
@@ -31,7 +31,7 @@ def instantiateTemplate(infile,outfile,subs):
 	  for arg in match.groups()[3].split(","):
 	    arg=arg.strip()
 	    if not subs.has_key(arg):
-	      print "ERROR: missing required argument:",arg
+	      logFile.write("ERROR: missing required argument:"+arg+"\n")
 	      outfile.write("! ERROR argument '"+arg+"' missing\n") 
           for arg in subs.keys():
 	    sost=subs[arg].split("\n")
@@ -47,7 +47,7 @@ def instantiateTemplate(infile,outfile,subs):
 	  break
 	elif not match.groups()[1]:
 	  if not inComment:
-	    print "WARNING, ingnoring empty group at line ",lineNr
+	    logFile.write("WARNING, ingnoring empty group at line %d\n"%lineNr)
 	  outfile.write(line[:match.end()])
 	  line=line[match.end():]
 	else:
@@ -56,49 +56,61 @@ def instantiateTemplate(infile,outfile,subs):
 	    line=line[match.end():]
 	  else:
 	    if not inComment:
-	      print "WARNING ignoring unknown token '",match.groups()[1],"' at line ",lineNr
+	      logFile.write("WARNING ignoring unknown token '%s' at line %d\n"%
+                            (match.groups()[1],lineNr))
 	    outfile.write(line[:match.end()])
 	    line=line[match.end():]
     except:
       import sys
-      print "error in ",infile.name," at line ",lineNr
-      print sys.exc_type, sys.exc_value
+      import traceback
+      logFile.write("error in '%s' at line %d\n"%(infile.name,lineNr))
+      traceback.print_exc(file=logFile)
   outfile.close()
   infile.close()
 
-
-if len(sys.argv)<2:
-    print "usage:", sys.argv[0]," template1.instantiation [template2.instantiation ...]"
-else:
-  for name in sys.argv[1:]:
+def evaluateInstantiationFile(instantiationFile,logFile=sys.stdout):
+    generatedFiles=[]
     try:
-      input = open(name,'r')
+      input = open(instantiationFile,'r')
       subst = eval(input.read())
       for substitution in subst:
         extension=".instantiation"
-        if not name[-len(extension):]==extension :
-          print "ERROR input",name,"is not a ",extension," file!!"
+        if not instantiationFile[-len(extension):]==extension :
+          logFile.write("ERROR input '"+ instantiationFile+"' is not a "+
+                        extension+" file!!\n")
           break
-        inName=input.name.replace(extension, ".template")
+        inName= instantiationFile.replace(extension, ".template")
         try: infile=open(inName,"r")
         except:
-          print("ERROR opening template '",inName,"'")
-          print sys.exc_type, sys.exc_value
-        outName=name[:-len(extension)]
+          logFile.write("ERROR opening template '"+inName+"'\n")
+          raise
+        outName=instantiationFile[:-len(extension)]
         for token in substitution.keys():
           outName=outName.replace("_"+token+"_",
                                   substitution[token])
         outName=outName+".F"
         try: outfile=open(outName,'w')
         except:
-          print "ERROR opening template '",outName,"'"
-          print sys.exc_type, sys.exc_value
-        instantiateTemplate(infile,outfile,substitution)
+          logFile.write("ERROR opening template '"+outName+"'\n")
+          raise
+        instantiateTemplate(infile,outfile,substitution,logFile)
+        generatedFiles.append(outName)
     except:
-      import sys
-      print "error evaluating substitutions from file ",name
-      print sys.exc_type, sys.exc_value
+        import sys
+        logFile.write("error evaluating substitutions from file '"+
+                      instantiationFile+"'\n")
+        import sys, traceback
+        logFile.write('-'*60+"\n")
+        traceback.print_exc(file=logFile)
+        logFile.write('-'*60+"\n")
+    return generatedFiles
 
+if __name__ == '__main__':
+    if len(sys.argv)<2:
+        print "usage:", sys.argv[0]," template1.instantiation [template2.instantiation ...]"
+    else:
+        for name in sys.argv[1:]:
+            evaluateInstantiationFile(name,sys.stdout)
 # Local Variables:
 # py-indent-offset: 2
 # End:
---
+++ tools/prepare_ci.py
@@ -0,0 +1,147 @@
+#! /usr/bin/env python
+# prepares cp2k for checkin
+
+import sys
+from sys import argv
+import os
+from os.path import join
+import commands
+import addSynopsis # .addSynopsisInDir
+import instantiateTemplates # .evaluateInstantiationFile
+import time
+
+def buildCp2k(cp2kRoot,buildType="sopt",logFilePath=None,clean=None):
+    if not logFilePath:
+        logFilePath=join(cp2kRoot,"build"+time.strftime("%y%m%d-%H:%M")+
+                "."+buildType+".log")
+    buildDir= join(cp2kRoot,"obj",
+	commands.getoutput(join(cp2kRoot,"tools","get_arch_code")),buildType)
+    if clean and os.access(buildDir,os.W_OK):
+        commands.getoutput('rm -rf "'+buildDir+'"')
+    os.chdir(join(cp2kRoot,"makefiles"))
+    if os.access("/usr/bin/gnumake",os.X_OK): makeCmd="/usr/bin/gnumake"
+    else: makeCmd="gmake"
+    pipe=os.popen("{ { "+makeCmd+" "+buildType+"; } 2>&1 ; } >>"+logFilePath)
+    logFile=open(logFilePath,'a')
+    if (pipe.close()):
+        logFile.write("\n+++ ERROR, build "+buildType+" FAILED! +++\n")
+        logFile.close()
+        return None
+    else:
+        logFile.write("\n+++ build "+buildType+" SUCESSFULLY! +++\n")
+        logFile.close()
+        return 1
+
+
+cp2kRoot=os.path.abspath(os.path.join(os.path.dirname(argv[0]),".."))
+logDirPath=join(cp2kRoot,"test-"+
+                commands.getoutput(join(cp2kRoot,"tools","get_arch_code"))+
+                "-"+time.strftime("%y%m%d-%H:%M"))
+os.mkdir(logDirPath)
+mainLog=open(join(logDirPath,"main.log"),'w')
+outDir= join(cp2kRoot,"src","outDir")
+
+print "logDirectory: "+logDirPath
+print "main log: "+mainLog.name
+
+mainLog.write("===== cleaning forpar =====\n")
+os.chdir(join(cp2kRoot,"tools"))
+if os.access("/usr/bin/gnumake",os.X_OK): makeCmd="/usr/bin/gnumake"
+else: makeCmd="gmake"
+os.popen("{ { "+makeCmd+" clean ; } 2>&1 ; } >/dev/null").close()
+os.popen("{ { "+makeCmd+" ; } 2>&1 ; } >/dev/null").close()
+
+mainLog.write("===== instantiating templates =====\n")
+mainLog.flush()
+import glob
+logFile=open(os.path.join(logDirPath,"templateInstantiation.log"),'w')
+instantiationFiles=glob.glob(os.path.join(cp2kRoot,"src","*.instantiation"))
+templateInstances=[]
+for instantiationFile in instantiationFiles:
+    templateInstances.extend(
+        instantiateTemplates.evaluateInstantiationFile(instantiationFile,
+                                                       logFile))
+mainLog.write(" template generation logFile in '%s'\n"%
+              os.path.basename(logFile.name))
+logFile.close()
+
+# build sdbg
+mainLog.write("====== dirty compiling cp2k sdbg ======\n")
+mainLog.flush()
+mainLog.write("  compilation logFile in 'cp2kBuildSdbg.log'\n")
+if not buildCp2k(cp2kRoot,"sdbg",join(logDirPath,"cp2kBuildSdbg.log")):
+    mainLog.write("+++ ERROR, build FAILED! +++\n")
+else:
+    mainLog.write("+++ build SUCESSFULL! +++\n")
+
+# add synopsis
+mainLog.write("====== updating synopsis ======\n")
+mainLog.flush()
+logFile=open(os.path.join(logDirPath,"addSynopsis.log"),'w')
+buildDir= join(cp2kRoot,"obj",
+    commands.getoutput(join(cp2kRoot,"tools","get_arch_code")),
+    "sdbg")
+outDir=join(cp2kRoot,"src","outDir")
+if os.access(outDir,os.W_OK): commands.getoutput('rm -rf "'+outDir+'"')
+os.mkdir(outDir)
+
+filesToSyn=glob.glob(os.path.join(cp2kRoot,"src","cp_*.F"))
+for fileToSyn in templateInstances:
+    if not fileToSyn[0:2]=="cp_":
+	filesToSyn.append(fileToSyn)
+addSynopsis.addSynopsisInDir(buildDir,outDir,filesToSyn,logFile)
+
+# check synopsis diffs
+os.chdir(outDir)
+filesSyn=glob.glob("*.F")
+for fileSyn in filesSyn:
+    diffs=commands.getoutput("diff "+fileSyn+" ../"+fileSyn)
+    if diffs=='':
+        diffOk=None
+    else:
+        diffOk=1
+        import re
+        commentOrHeader = re.compile("([0-9]+,*[0-9]*[adc][0-9]+,*[0-9]*$|[><] *|[><] *!| *$|---$)")
+        for line in diffs.splitlines():
+            if not commentOrHeader.match(line):
+                mainLog.write("+ WARNING checking synopsis for "+fileSyn+
+                              "  found the unaccetable line.\n  line='"+line+
+                              "'  addSynopsis not performed\n")
+                diffOk=None
+                break
+    if diffOk:
+        os.rename(fileSyn,join("..",fileSyn))
+mainLog.write("  addSynopsis logFile in '%s'\n"%os.path.basename(logFile.name))
+logFile.close()
+
+# clean compile
+mainLog.write("====== clean compile cp2k sopt ======\n")
+mainLog.flush()
+mainLog.write("  compilation logFile in 'cp2kBuildSopt.log'\n")
+if not buildCp2k(cp2kRoot,"sopt",join(logDirPath,"cp2kBuildSopt.log"),clean=1):
+    mainLog.write("+++ ERROR, build FAILED! +++\n")
+else:
+    mainLog.write("+++ build SUCESSFULL! +++\n")
+
+# do tests
+
+# cvs
+for arg in argv[1:]:
+    if (arg.lowercase()=='cvs'):
+        os.chdir(cp2kRoot)
+        # cvs -n update
+        mainLog.write("====== cvs -n update ======\n")
+        mainLog.flush()
+        os.popen('{ { cvs -n update; } 2>&1 ; } >"%s"'%
+                 os.path.join(logDirPath,"cvsUpdate.log"))
+        mainLog.write("  cvs update log in 'cvsUpdate.log'\n")
+
+        # cvs diff
+        mainLog.write("====== cvs diff ======\n")
+        mainLog.flush()
+        os.popen('{ { cvs diff; } 2>&1 ; } >"%s"'%
+                 os.path.join(logDirPath,"cvsDiff.log"))
+        mainLog.write("  cvs diff log in 'cvsDiff.log'\n")
+        break
+
+mainLog.close()
