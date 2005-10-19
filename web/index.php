557c73fbea705ace3a8f2a367e6aceb87c197602
Mohamed Fawzi <mohamed@fhi-berlin.mpg.de>
2005-10-19 10:35:22 +0000

    trying to fix our lost webpage...
    svn-origin-rev: 4176

---
+++ web/index.php
@@ -0,0 +1,90 @@
+<HTML>
+<HEAD><TITLE> The CP2K project homepage </TITLE>
+<META NAME="description" content="CP2K">
+<META NAME="keywords" contents="Fortran,scientific,computing,CPMD,CP2K,Quickstep,Car-Parrinello,Fist,GPW,GAPW>
+<META http-equiv="Pragma" content="no-cache">
+</HEAD>
+
+<BODY TEXT=BLACK BGCOLOR=WHITE LINK=blue VLINK=blue>
+
+<tr>
+<td BGCOLOR="#000000">
+<center><table CELLPADDING=5 WIDTH="100%" >
+<tr>
+<td BGCOLOR="#DDDDDD" BACKGROUND="background.jpg">
+<center><b><font size=+2>C P 2 K&nbsp;&nbsp;&nbsp; D E V E L O P E R S&nbsp;&nbsp;&nbsp;
+H O M E&nbsp;&nbsp;&nbsp; P A G E&nbsp;</font></b></center>
+</td>
+</tr>
+</table></center>
+</td>
+</tr>
+<center>
+<table width="80%" BGCOLOR="#DDDDDD" BACKGROUND="background.jpg" >
+<tr>
+<td> <center> <A HREF="download.html">Download</A> </center> </td>
+<td> <center> <A HREF="quickstep.html">Quickstep</A> </center> </td>
+<td> <center> <A HREF="regtest.html">Regtests</A> </center> </td>
+</table></center>
+
+
+<P><BR></P>
+
+<H2><CENTER>Welcome!</CENTER></H2>
+<H2><CENTER> What is CP2K?</CENTER></H2>
+
+CP2K is a <A HREF="download.html">freely available</A> (GPL) program, written in Fortran 95, 
+to perform atomistic and molecular simulations of solid state, liquid,
+molecular and biological systems. It provides a general framework for different methods such as e.g.
+density functional theory (DFT) using a mixed Gaussian and plane waves approach (GPW), 
+and classical pair and many-body potentials.
+
+<H3> Strengths ... </H3>
+
+CP2K provides state-of-the-art methods for efficient and accurate atomistic simulations,
+sources are freely available and actively improved. It is therefore easy to give the code a try, and
+to make modifications as needed.
+
+<H3> ... and limitations </H3> 
+
+However, you'll find all versions of the CP2K code on this server and they all come with little documentation and without any warranty. 
+No official release has been made.  You might therefore expect that substantial changes, improvements and bug fixes will
+be made at irregular intervals. Using the code for production quality simulations is possible but requires detailed 
+knowledge about the active development.
+
+<H3> Some key parts of CP2K </H3>
+
+<UL>
+<LI> Fist: classical molecular dynamics
+<LI> Kim-Gordon Models
+<LI> <A HREF="quickstep.html">Quickstep</A>: Density functional calculations
+</UL>
+
+<H3> Who is behind this project? </H3>
+
+A partial answer is given by the <A HREF="http://developer.berlios.de/project/memberlist.php?group_id=129">list of developers</A> 
+having CVS write permission.
+
+<h3>Links</h3>
+<ul>
+ <li><a href="download.html">Download and Install</a></li>
+ <li><a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/cp2k/src/?sortby=date">Browse the sources and follow development</a></li>
+ <li><a href="regtest.html">Automatic regression tester</a></li>
+ <li><a href="http://developer.berlios.de/projects/cp2k">The Berlios project page</a></li>
+ <li>Goedecker-Teter-Hutter (GTH) pseudopotential parameter sets
+ (<a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/potentials/Goedecker/cp2k">CP2K format</a>,
+  <a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/potentials/Goedecker/cpmd">CPMD format</a>)
+ </li>
+</ul>
+
+<H3>Hosted by</H3>
+<CENTER>
+<a href="http://developer.berlios.de" title="BerliOS Developer"> 
+<img src="http://developer.berlios.de/bslogo.php?group_id=129" width="124px" height="32px" border="0" alt="BerliOS Developer Logo"></a>
+</CENTER>
+
+<P><BR><HR></P>
+
+</BODY>
+</HTML>
+
