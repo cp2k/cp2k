39a57a1fdcff4c4483eafede1d0b44f182b81b31
Teodoro Laino <teodoro.laino@gmail.com>
2007-10-12 23:56:41 +0000

    OMP for full cp2k (Ben Levine). full regtest passes (with few instability problems for  regtests UKS-NMR and few of DDAPC). together with a weekly valgrind regtest there will be also another regtest to check that OMP version will be preserved through developments..
    svn-origin-rev: 6429

---
+++ src/call_collocate_omp.f90
@@ -0,0 +1,29 @@
+  SELECT CASE(lp)
+ CASE(0)
+ CALL collocate_core_0(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(1)
+ CALL collocate_core_1(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(2)
+ CALL collocate_core_2(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(3)
+ CALL collocate_core_3(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(4)
+ CALL collocate_core_4(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(5)
+ CALL collocate_core_5(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(6)
+ CALL collocate_core_6(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+ CASE(7)
+ CALL collocate_core_7(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax), pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),cmax,gridbounds(1,1))
+  CASE DEFAULT
+ CALL collocate_core_default(lgrid%r(ig),coef_xyz(1),pol_x(0,-cmax),pol_y(1,0,-cmax),pol_z(1,0,-cmax), &
+                        map(-cmax,1),sphere_bounds(1),lp,cmax,gridbounds(1,1))
+  END SELECT
