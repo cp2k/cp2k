a588ff481f8b0ea779c8d68a7c4a74fe124ecdc7
Teodoro Laino <teodoro.laino@gmail.com>
2008-02-25 16:11:24 +0000

    Patch for improving the CUDA FFT performance and added the infrastructure for managing the GPU's memory (Ben Levine).
    svn-origin-rev: 7059

---
+++ cuda_tools/cuda_memory_cu.cu
@@ -0,0 +1,69 @@
+#if defined ( __FFTCU ) || defined ( __CUDA )
+
+/* This file contains memory management routines for device memory.  The goal of these routines is eliminate the need to allocate device memory more than once during a run.  Basically, CP2K allocates a large chunk of memory at the beginning of the job using cuda_device_mem_init_cu_.  The size of this chunk is specified in the input file.  The below routines cuda_device_mem_alloc_cu_ and cuda_device_mem_free_cu_ are used to allocate and free memory from this chunk.  The bookkeeping is done by the static variables below.  Finally, the chunk can be freed at the end of the job using cuda_device_mem_release_cu_ */
+
+// Author:  Benjamin G Levine
+
+#include <cuda_runtime.h>
+#include <cufft.h>
+#include <stdio.h>
+
+static const int max_allocs=20;
+static int ioffset[max_allocs];
+static int isize[max_allocs];
+static int n_allocs;
+static int length;
+static float* device_memory;
+
+extern "C" void cuda_device_mem_init_cu_ (int *memory) {
+
+  
+  n_allocs=0;
+  length=(*memory)*256;
+  cudaMalloc((void**)&device_memory, sizeof(float)*(length));
+}
+
+extern "C" void cuda_device_mem_alloc_cu_ (float **ptr, int *n) {
+
+  if ( n_allocs==0 && (*n) <= length ) {
+    *ptr = device_memory;
+    n_allocs++;
+  }
+  else {
+    if ((*n) <= (length - (ioffset[n_allocs-1] + isize[n_allocs-1] ))) { 
+      isize[n_allocs] = (*n);
+      ioffset[n_allocs] = ioffset[n_allocs-1] + isize[n_allocs-1];
+      *ptr = device_memory + ioffset[n_allocs];
+      n_allocs++;
+    }
+    else {
+      *ptr=NULL;
+      printf("NOT ENOUGH GPU DEVICE MEMORY!\n");
+      fflush(stdout);
+    }
+  }
+}
+
+extern "C" void cuda_device_mem_free_cu_ (float **ptr) {
+  int ioffsettmp, zfound, i;
+  
+  zfound=0;
+  ioffsettmp = *ptr - device_memory;
+  for (i=0; i < n_allocs; i++) {
+    if (zfound) {
+      ioffset[i-1] = ioffset[i];
+      isize[i-1] = isize[i];
+    }
+    if (ioffsettmp==ioffset[i]) zfound=1;
+  }
+  n_allocs--;
+  *ptr=NULL;
+}
+
+extern "C" void cuda_device_mem_release_cu_ () {
+
+  cudaFree(device_memory);
+
+}
+
+#endif
--- cuda_tools/cufft.cu
+++ cuda_tools/cufft.cu
@@ -2,48 +2,108 @@
 
 #include <cuda_runtime.h>
 #include <cufft.h>
+#include <cublas.h>
 #include <stdio.h>
 
-extern "C" void fftcu3dc_ (int *ifft_in_place, int *fsign, float *scale, int *n, float *zin, float *zout) {
+#include "cuda_memory_cu.h"
 
+static const int max_plans=10;
+static int n_plans=0;
+static cufftHandle saved_plans[max_plans];
+static int iplandims[max_plans][3];
+
+void fftcu_plan3d(cufftHandle& plan, int* n, int ioverflow) {
+  int i;
+
+  ioverflow=0;
+  for (i=0; i<n_plans; i++) {
+    if ( n[0] == iplandims[i][0] && n[1] == iplandims[i][1] && 
+         n[2] == iplandims[i][2] ) {
+      plan = saved_plans[i];
+      return;
+    }
+  }
+  cufftPlan3d(&plan, n[2], n[1], n[0], CUFFT_C2C);
+  if ( n_plans < max_plans ) {
+    fflush(stdout);
+    saved_plans[n_plans] = plan;
+    iplandims[n_plans][0] = n[0];
+    iplandims[n_plans][1] = n[1];
+    iplandims[n_plans][2] = n[2];
+    n_plans++;
+    return;
+  }
+  ioverflow=1;
+}
+
+extern "C" void fftcu3d_cu_ (int *ifft_in_place, int *fsign, float *scale, int *n, float *zin, float *zout) {
+
+  float *ptr;
   cufftComplex *data;
   cufftHandle plan;
-  int i;
+  int lmem, ioverflow;
   
-  cudaMalloc((void**)&data, sizeof(cufftComplex)*n[0]*n[1]*n[2]);
+  lmem=2*n[0]*n[1]*n[2];
+  cuda_device_mem_alloc_cu_(&ptr, &lmem);
+
+  data=(cufftComplex*)ptr;
 
   cudaMemcpy(data, zin, sizeof(cufftComplex)*n[0]*n[1]*n[2], cudaMemcpyHostToDevice);
 
-  cufftPlan3d(&plan, n[2], n[1], n[0], CUFFT_C2C);
+  fftcu_plan3d(plan,n,ioverflow);
 
   if ( *fsign == 1 ) {
-    // this code does not yet use scale!
     cufftExecC2C(plan, data, data, CUFFT_INVERSE);
   }
   else {
-    // this code does not yet use scale!
     cufftExecC2C(plan, data, data, CUFFT_FORWARD);
+  }
+
+  if (*scale /= 1.0f) {
+    cublasSscal(lmem, *scale, ptr, 1);
   }
 
   if ( *ifft_in_place == 1 ) {
     cudaMemcpy(zin, data, sizeof(cufftComplex)*n[0]*n[1]*n[2], cudaMemcpyDeviceToHost);
-    if (*scale /= 1.0) {
-      for (i=0; i<2*n[0]*n[1]*n[2]; i++) {
-        zin[i]=zin[i]*(*scale);
-      }
-    }
   }
   else {
     cudaMemcpy(zout, data, sizeof(cufftComplex)*n[0]*n[1]*n[2], cudaMemcpyDeviceToHost);
-    if (*scale /= 1.0) {
-      for (i=0; i<2*n[0]*n[1]*n[2]; i++) {
-        zin[i]=zin[i]*(*scale);
-      }
-    }
+  }
+  cuda_device_mem_free_cu_(&ptr);
+  if (ioverflow) { cufftDestroy(plan); }
+}
+
+extern "C" void fftcu1dm_cu_ (int *fsign, int itrans, int *n, int *m, float *zin, float *zout, float *scale) {
+
+  float *ptr;
+  cufftComplex *data;
+  cufftHandle plan;
+  int lmem;
+  
+  lmem=2*(*n)*(*m);
+  cuda_device_mem_alloc_cu_(&ptr, &lmem);
+
+  data=(cufftComplex*)ptr;
+
+  cudaMemcpy(data, zin, sizeof(cufftComplex)*(*n)*(*m), cudaMemcpyHostToDevice);
+
+  cufftPlan1d(&plan, (*n), CUFFT_C2C, (*m));
+
+  if ( *fsign == 1 ) {
+    cufftExecC2C(plan, data, data, CUFFT_INVERSE);
+  }
+  else {
+    cufftExecC2C(plan, data, data, CUFFT_FORWARD);
   }
 
+  if (*scale /= 1.0f) {
+    cublasSscal(lmem, *scale, ptr, 1);
+  }
+
+  cudaMemcpy(zout, data, sizeof(cufftComplex)*(*n)*(*m), cudaMemcpyDeviceToHost);
+
+  cuda_device_mem_free_cu_(&ptr);
   cufftDestroy(plan);
-  cudaFree(data);
 }
 
 #endif
