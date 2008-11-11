5c73272f4b1eeb446f43321936a634204da228de
Matthias Krack <matthias.krack@psi.ch>
2008-11-11 07:30:46 +0000

    update links to pseudopotential and basis set database too
    svn-origin-rev: 7909

--- web/index.php
+++ web/index.php
@@ -81,10 +81,10 @@ having CVS write permission.
  <li><a href="http://developer.berlios.de/bugs/?group_id=129">Bug reporting</a> (please provide all necessary information to reproduce, including the needed inputs!)</li>
  <li><a href="http://developer.berlios.de/patch/?group_id=129">send patches</a></li>
  <li>Goedecker-Teter-Hutter (GTH) pseudopotential parameter sets
- (<a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/potentials/Goedecker/cp2k">CP2K format</a>,
-  <a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/potentials/Goedecker/cpmd">CPMD format</a>,
-  <a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/potentials/Goedecker/abinit">ABINIT format</a>)</li>
- <li><a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/cp2k/basis_sets">Gaussian basis sets generator for Quickstep</a></li>
+ (<a href="http://cvs.berlios.de/cgi-bin/viewvc.cgi/cp2k/potentials/Goedecker/cp2k">CP2K format</a>,
+  <a href="http://cvs.berlios.de/cgi-bin/viewvc.cgi/cp2k/potentials/Goedecker/cpmd">CPMD format</a>,
+  <a href="http://cvs.berlios.de/cgi-bin/viewvc.cgi/cp2k/potentials/Goedecker/abinit">ABINIT format</a>)</li>
+ <li><a href="http://cvs.berlios.de/cgi-bin/viewvc.cgi/cp2k/basis_sets">Gaussian basis sets generator for Quickstep</a></li>
 </ul>
 
 <H3>Hosted by</H3>
