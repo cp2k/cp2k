62038af71ff1db16b938234e1c33529842824c6f
Joost VandeVondele <joost.vandevondele@cscs.ch>
2008-12-15 07:56:10 +0000

    some refactoring, towards generalizing and improving the docs
    svn-origin-rev: 8050

--- tools/Fun2D/recurse.f90
+++ tools/Fun2D/recurse.f90
@@ -20,6 +20,9 @@
 ! * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ! * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 !
+!   Joost VandeVondele and Manuel Guidon, Nov 2008.
+!
+
 !   recursively bisect the rectangular domain of a bivariate function
 !   till the lagrange interpolation of specified degree through
 !   the Padua points for it converges to a preset threshold
@@ -44,24 +47,29 @@
 !   (which is accurate at least up to 1.0E-16)
 !   while for T smaller than the lower bound, the caller is instructed 
 !   to use the gamma function instead
-!   
-!   Joost VandeVondele and Manuel Guidon, Nov 2008.
-!
 !   This program requires the auxiliary program drvrec_mpfr.x
 !
     MODULE RECURSE      
       INTEGER, SAVE :: find_count=0
       INTEGER, PARAMETER :: dp=KIND(0.0D0)
       REAL(KIND=dp) ERR
+      integer :: functionid,digits
     CONTAINS
+
+!====================================================================================================================
+!
+!     a function specific wrapper to the bisection routines
+!
+!     this function must open a unit 13 (the coefficients of the interpolation)
+!     this function must open a unit 17 (some info on the bisected domains )
+!
+!====================================================================================================================
       SUBROUTINE GO()
          IMPLICIT NONE
          REAL(KIND=dp) A1,B1,A2,B2
          INTEGER :: DEG,LEVEL
          INTEGER :: nderiv
 
-         integer :: functionid,digits
-         COMMON/functiontype/functionid,digits
 
          ! these are the selected settings for CP2K
          ! some initial guess for the needed number of digits
@@ -73,33 +81,11 @@
          ! target error
          ERR=1.0E-9
 
-         IF (DEG<3) STOP "Not supported"
          OPEN(UNIT=13,FILE="T_C_G.dat")
          LEVEL=0
 
-         WRITE(6,'(A)') "!"
-         WRITE(6,'(A)') "! * Copyright (c) 2008, Joost VandeVondele and Manuel Guidon"
-         WRITE(6,'(A)') "! * All rights reserved."
-         WRITE(6,'(A)') "! *"
-         WRITE(6,'(A)') "! * Redistribution and use in source and binary forms, with or without"
-         WRITE(6,'(A)') "! * modification, are permitted provided that the following conditions are met:"
-         WRITE(6,'(A)') "! *     * Redistributions of source code must retain the above copyright"
-         WRITE(6,'(A)') "! *       notice, this list of conditions and the following disclaimer."
-         WRITE(6,'(A)') "! *     * Redistributions in binary form must reproduce the above copyright"
-         WRITE(6,'(A)') "! *       notice, this list of conditions and the following disclaimer in the"
-         WRITE(6,'(A)') "! *       documentation and/or other materials provided with the distribution."
-         WRITE(6,'(A)') "! *"
-         WRITE(6,'(A)') "! * THIS SOFTWARE IS PROVIDED BY Joost VandeVondele and Manuel Guidon AS IS AND ANY"
-         WRITE(6,'(A)') "! * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED"
-         WRITE(6,'(A)') "! * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE"
-         WRITE(6,'(A)') "! * DISCLAIMED. IN NO EVENT SHALL Joost VandeVondele or Manuel Guidon BE LIABLE FOR ANY"
-         WRITE(6,'(A)') "! * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES"
-         WRITE(6,'(A)') "! * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;"
-         WRITE(6,'(A)') "! * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND"
-         WRITE(6,'(A)') "! * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT"
-         WRITE(6,'(A)') "! * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS"
-         WRITE(6,'(A)') "! * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-         WRITE(6,'(A)') "!"
+         CALL write_copy_right()
+
          WRITE(6,'(A)') "!"
          WRITE(6,'(A)') "!   This module computes the basic integrals for the"
          WRITE(6,'(A)') "!   truncated coulomb operator"
@@ -121,42 +107,31 @@
          WRITE(6,'(A)') "!   while for T smaller than the lower bound, the caller is instructed"
          WRITE(6,'(A)') "!   to use the conventional gamma function instead"
          WRITE(6,'(A)') "!   (i.e. the limit of above expression for R to Infinity)"
-         WRITE(6,'(A)') "!"
-         WRITE(6,'(A)') "!   Joost VandeVondele and Manuel Guidon, Nov 2008."
          WRITE(6,'(A)') "! instead of a module one could use: INTEGER, PARAMETER ::  dp=KIND(0.0D0)"
-         WRITE(6,'(A,I0,A)') "MODULE T_C_G0"
-         WRITE(6,'(A)')      " USE kinds, ONLY: dp"
-         WRITE(6,'(A)')      " IMPLICIT NONE"
-         WRITE(6,'(A)')      " PUBLIC :: T_C_G0_n"
-         WRITE(6,'(A)')      " REAL(KIND=dp), DIMENSION(:,:), ALLOCATABLE,  SAVE :: C0"
-         WRITE(6,'(A,I0)')   " INTEGER, PARAMETER :: degree=",DEG
-         WRITE(6,'(A,E18.6)')   " REAL(KIND=dp), PARAMETER :: target_error=",err
-         WRITE(6,'(A,I0)')   " INTEGER, PARAMETER :: nderiv_max=",nderiv
-         WRITE(6,'(A,I0)')   " INTEGER            :: nderiv_init=",-1
-         WRITE(6,'(A)')      " INTEGER, SAVE      :: patches=-1"
-         WRITE(6,'(A,I0,A)') "CONTAINS"
+
+         CALL write_module_head("T_C_G0",DEG,nderiv,err)
 
          WRITE(6,'(A,I0,A)') "SUBROUTINE T_C_G0_n(RES,use_gamma,R,T,NDERIV)"
-         WRITE(6,'(A)') " IMPLICIT NONE"
-         WRITE(6,'(A)') " REAL(KIND=dp), INTENT(OUT) :: RES(*)"
-         WRITE(6,'(A)') " LOGICAL, INTENT(OUT) :: use_gamma"
-         WRITE(6,'(A)') " REAL(KIND=dp),INTENT(IN) :: R,T"
-         WRITE(6,'(A)') " INTEGER, INTENT(IN)      :: NDERIV"
-         WRITE(6,'(A)') " REAL(KIND=dp)            :: upper,lower,X1,X2,TG1,TG2"
+         WRITE(6,'(A)')      " IMPLICIT NONE"
+         WRITE(6,'(A)')      " REAL(KIND=dp), INTENT(OUT) :: RES(*)"
+         WRITE(6,'(A)')      " LOGICAL, INTENT(OUT) :: use_gamma"
+         WRITE(6,'(A)')      " REAL(KIND=dp),INTENT(IN) :: R,T"
+         WRITE(6,'(A)')      " INTEGER, INTENT(IN)      :: NDERIV"
+         WRITE(6,'(A)')      " REAL(KIND=dp)            :: upper,lower,X1,X2,TG1,TG2"
 
-         WRITE(6,'(A)') "  use_gamma=.FALSE."
-         WRITE(6,'(A)') "  upper=R**2 + 11.0_dp*R + 50.0_dp"
-         WRITE(6,'(A)') "  lower=R**2 - 11.0_dp*R + 0.0_dp"
-         WRITE(6,'(A)') "   IF (T>upper) THEN"
-         WRITE(6,'(A)') "      RES(1:NDERIV+1)=0.0_dp"
-         WRITE(6,'(A)') "      RETURN"
-         WRITE(6,'(A)') "   ENDIF"
+         WRITE(6,'(A)')      "  use_gamma=.FALSE."
+         WRITE(6,'(A)')      "  upper=R**2 + 11.0_dp*R + 50.0_dp"
+         WRITE(6,'(A)')      "  lower=R**2 - 11.0_dp*R + 0.0_dp"
+         WRITE(6,'(A)')      "   IF (T>upper) THEN"
+         WRITE(6,'(A)')      "      RES(1:NDERIV+1)=0.0_dp"
+         WRITE(6,'(A)')      "      RETURN"
+         WRITE(6,'(A)')      "   ENDIF"
 
-         WRITE(6,'(A)') " IF (R<=11.0_dp) THEN"
-         WRITE(6,'(A)') "  X2=R/11.0_dp"
-         WRITE(6,'(A)') "  upper=R**2 + 11.0_dp*R + 50.0_dp"
-         WRITE(6,'(A)') "  lower=0.0_dp"
-         WRITE(6,'(A)') "  X1=(T-lower)/(upper-lower)"
+         WRITE(6,'(A)')      " IF (R<=11.0_dp) THEN"
+         WRITE(6,'(A)')      "  X2=R/11.0_dp"
+         WRITE(6,'(A)')      "  upper=R**2 + 11.0_dp*R + 50.0_dp"
+         WRITE(6,'(A)')      "  lower=0.0_dp"
+         WRITE(6,'(A)')      "  X1=(T-lower)/(upper-lower)"
 
              functionid=2
              open(UNIT=17,FILE="nearfield_domains.dat")
@@ -187,7 +162,7 @@
 
          WRITE(6,'(A)') " ENDIF"
 
-         WRITE(6,'(A,I0,A)') "END SUBROUTINE T_C_G0_n"
+         WRITE(6,'(A)') "END SUBROUTINE T_C_G0_n"
            
          CLOSE(UNIT=13)
 
@@ -198,7 +173,67 @@
          WRITE(6,'(A)') "END MODULE T_C_G0"
 
       END SUBROUTINE GO
+!====================================================================================================================
+!
+!     write the top of the module 
+!
+!====================================================================================================================
+      SUBROUTINE WRITE_MODULE_HEAD(mod_name,DEG,nderiv,err)
+         CHARACTER(LEN=*) :: mod_name
+         INTEGER          :: nderiv,deg
+         REAL(KIND=dp)    :: err
 
+         WRITE(6,'(A,A)')    "MODULE ",mod_name
+         WRITE(6,'(A)')      " USE kinds, ONLY: dp"
+         WRITE(6,'(A)')      " IMPLICIT NONE"
+         WRITE(6,'(A)')      " REAL(KIND=dp), DIMENSION(:,:), ALLOCATABLE,  SAVE :: C0"
+         WRITE(6,'(A,I0)')   " INTEGER, PARAMETER :: degree=",DEG
+         WRITE(6,'(A,E18.6)')" REAL(KIND=dp), PARAMETER :: target_error=",err
+         WRITE(6,'(A,I0)')   " INTEGER, PARAMETER :: nderiv_max=",nderiv
+         WRITE(6,'(A)')      " INTEGER            :: nderiv_init=-1"
+         WRITE(6,'(A)')      " INTEGER, SAVE      :: patches=-1"
+         WRITE(6,'(A)')      "CONTAINS"
+
+      END SUBROUTINE WRITE_MODULE_HEAD
+!====================================================================================================================
+!
+!     writes a BSD style copy right header
+!
+!====================================================================================================================
+      SUBROUTINE WRITE_COPY_RIGHT()
+
+         WRITE(6,'(A)') "!"
+         WRITE(6,'(A)') "! * Copyright (c) 2008, Joost VandeVondele and Manuel Guidon"
+         WRITE(6,'(A)') "! * All rights reserved."
+         WRITE(6,'(A)') "! *"
+         WRITE(6,'(A)') "! * Redistribution and use in source and binary forms, with or without"
+         WRITE(6,'(A)') "! * modification, are permitted provided that the following conditions are met:"
+         WRITE(6,'(A)') "! *     * Redistributions of source code must retain the above copyright"
+         WRITE(6,'(A)') "! *       notice, this list of conditions and the following disclaimer."
+         WRITE(6,'(A)') "! *     * Redistributions in binary form must reproduce the above copyright"
+         WRITE(6,'(A)') "! *       notice, this list of conditions and the following disclaimer in the"
+         WRITE(6,'(A)') "! *       documentation and/or other materials provided with the distribution."
+         WRITE(6,'(A)') "! *"
+         WRITE(6,'(A)') "! * THIS SOFTWARE IS PROVIDED BY Joost VandeVondele and Manuel Guidon AS IS AND ANY"
+         WRITE(6,'(A)') "! * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED"
+         WRITE(6,'(A)') "! * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE"
+         WRITE(6,'(A)') "! * DISCLAIMED. IN NO EVENT SHALL Joost VandeVondele or Manuel Guidon BE LIABLE FOR ANY"
+         WRITE(6,'(A)') "! * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES"
+         WRITE(6,'(A)') "! * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;"
+         WRITE(6,'(A)') "! * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND"
+         WRITE(6,'(A)') "! * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT"
+         WRITE(6,'(A)') "! * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS"
+         WRITE(6,'(A)') "! * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+         WRITE(6,'(A)') "!"
+         WRITE(6,'(A)') "!"
+         WRITE(6,'(A)') "!   Joost VandeVondele and Manuel Guidon, Nov 2008."
+
+      END SUBROUTINE WRITE_COPY_RIGHT
+!====================================================================================================================
+!
+!     write the table initialization routines
+!
+!====================================================================================================================
       SUBROUTINE WRITE_INIT()
 
          WRITE(6,'(A)') "! iunit contains the data file to initialize the table"
@@ -209,7 +244,7 @@
          WRITE(6,'(A)') "  INTEGER :: I"
          WRITE(6,'(A)') "  REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: chunk"
          WRITE(6,'(A,I0)') "  patches=",find_count
-         WRITE(6,'(A)') '  IF (Nder>nderiv_max) STOP "T_C_G0 init failed"'
+         WRITE(6,'(A)') '  IF (Nder>nderiv_max) STOP "Reading data for initialization of C0 failed"'
          WRITE(6,'(A)') "  nderiv_init=Nder"
          WRITE(6,'(A)') "  IF(ALLOCATED(C0)) DEALLOCATE(C0)"
          WRITE(6,'(A)') "  ! round up to a multiple of 32 to give some generous alignment for each C0"
@@ -225,9 +260,12 @@
          WRITE(6,'(A)') "  IF(ALLOCATED(C0)) DEALLOCATE(C0)"
          WRITE(6,'(A)') "  nderiv_init=-1"
          WRITE(6,'(A)') "END SUBROUTINE FREE"
-
       END SUBROUTINE WRITE_INIT
-
+!====================================================================================================================
+!
+!     write the code to evaluate the interpolation
+!
+!====================================================================================================================
       SUBROUTINE WRITE_PD2VAL(DEG)
 
          INTEGER :: DEG
@@ -242,14 +280,14 @@
          WRITE(6,'(A,I0,A)') " REAL(KIND=dp) :: T1(0:",DEG,")"
          WRITE(6,'(A,I0,A)') " REAL(KIND=dp) :: T2(0:",DEG,")"
          WRITE(6,'(A,I0,A)') " REAL(KIND=dp), INTENT(IN) :: C0(",(DEG+1)*(DEG+2)/2,",*)"
-         WRITE(6,'(A,I0,A)') " INTEGER :: I,J,K"
-         WRITE(6,'(A,I0,A)') " REAL(KIND=dp), PARAMETER :: SQRT2=1.4142135623730950488016887242096980785696718753_dp"
-         WRITE(6,'(A,I0,A)') " T1(0)=1.0_dp"
-         WRITE(6,'(A,I0,A)') " T2(0)=1.0_dp"
-         WRITE(6,'(A,I0,A)') " T1(1)=SQRT2 * TG1"
-         WRITE(6,'(A,I0,A)') " T2(1)=SQRT2 * TG2"
-         WRITE(6,'(A,I0,A)') " T1(2)= 2 * TG1 * T1(1) - SQRT2 "
-         WRITE(6,'(A,I0,A)') " T2(2)= 2 * TG2 * T2(1) - SQRT2 "
+         WRITE(6,'(A)') " INTEGER :: I,J,K"
+         WRITE(6,'(A)') " REAL(KIND=dp), PARAMETER :: SQRT2=1.4142135623730950488016887242096980785696718753_dp"
+         WRITE(6,'(A)') " T1(0)=1.0_dp"
+         WRITE(6,'(A)') " T2(0)=1.0_dp"
+         WRITE(6,'(A)') " T1(1)=SQRT2 * TG1"
+         WRITE(6,'(A)') " T2(1)=SQRT2 * TG2"
+         WRITE(6,'(A)') " T1(2)= 2 * TG1 * T1(1) - SQRT2 "
+         WRITE(6,'(A)') " T2(2)= 2 * TG2 * T2(1) - SQRT2 "
          DO I=3,DEG
          WRITE(6,'(A,I0,A,I0,A,I0,A)') " T1(",I,") = 2 * TG1 * T1(",I-1,") - T1(",I-2,")" 
          WRITE(6,'(A,I0,A,I0,A,I0,A)') " T2(",I,") = 2 * TG2 * T2(",I-1,") - T2(",I-2,")" 
@@ -259,18 +297,20 @@
          WRITE(6,'(A)')      "    RES(K) = 0.0_dp"
            J=1
            DO I=DEG,0,-1
-              WRITE(6,'(A,I0,A,I0,A,I0,A,I0,A)') "  RES(K)=RES(K)+DOT_PRODUCT(T1(0:",I,"),C0(",J,":",J+I,",K))*T2(",DEG-I,")"
+              WRITE(6,'(A,I0,A,I0,A,I0,A,I0,A)') "  RES(K)=RES(K)+DOT_PRODUCT(T1(0:",I,"),C0(",&
+                                                           J,":",J+I,",K))*T2(",DEG-I,")"
               J=J+I+1
            ENDDO
          WRITE(6,'(A)')      " ENDDO"
 
          WRITE(6,'(A)') "END SUBROUTINE PD2VAL"
 
-      END SUBROUTINE
-
-      !
-      ! the basic piece of the code, does the bisection till convergence
-      !
+      END SUBROUTINE WRITE_PD2VAL
+!====================================================================================================================
+!
+!     The basic piece of the code, does the bisection till convergence
+!
+!====================================================================================================================
       RECURSIVE SUBROUTINE FIND_DOMAINS(A1,B1,A2,B2,DEG,NDERIV,ERR,LEVEL)
 
          IMPLICIT NONE
@@ -280,8 +320,9 @@
          REAL(KIND=dp) ESTIMATED_ERR,MIDDLE,ESTIMATED_ERR1,ESTIMATED_ERR2,ESTIMATED_ERR3,ESTIMATED_ERR4
          REAL(KIND=dp) :: C0(0:DEG,0:DEG,0:NDERIV)
 
+         IF (DEG<3) STOP "Not supported"
          IF (A1==B1 .AND. A2==B2) STOP "BUG 1"
-         IF (LEVEL>20) STOP "Too many bisections "
+         IF (LEVEL>20) STOP "Too many bisections: is the function sufficiently differentiable in the full domain?"
 
          ! compute the interpolating polynomial and its estimated error
 
@@ -351,16 +392,19 @@
          ENDIF
 
       END SUBROUTINE FIND_DOMAINS
-
-      ! evaluates C0 coefs increasing the number of digits, till the result becomes independent
+!====================================================================================================================
+!
+!     evaluates C0 coefs increasing the number of digits, till the resulting C0 is converged.
+!     This is a simple wrapper to the program drvrec_mpfr.x
+!
+!====================================================================================================================
       SUBROUTINE drvrec_wrap(A1,B1,A2,B2,DEG,NDERIV,C0,ESTIMATED_ERR)
 
          REAL(KIND=dp) :: a1,b1,a2,b2,estimated_err,estimated_err2,estimated_err3,esterr
          INTEGER :: deg,nderiv
          REAL(KIND=dp) :: C0(0:DEG,0:DEG,0:NDERIV), C02(0:DEG,0:DEG,0:NDERIV)
          REAL(KIND=dp) :: R,T,vals(0:nderiv)
-         integer :: functionid,digits,digits_local
-         COMMON/functiontype/functionid,digits
+         integer :: digits_local
 
          digits_local=digits
          C02 = 0.0_dp
@@ -397,15 +441,24 @@
          ENDDO
 
       END SUBROUTINE
-END MODULE
+END MODULE Recurse
 
+!====================================================================================================================
 !
-! The main program ... small and easy
+!     The main program ... small and easy
 !
+!     output files....
+!     std out          :  a program that can evaluate the function in a given domain
+!     history.dat      :  a list of function values, evaluated during the construction of the approximation,
+!                         that can be used as a reference data.
+!
+!====================================================================================================================
+PROGRAM Fun2D
 
- USE RECURSE
+ USE Recurse
+
  OPEN(UNIT=98,FILE="history.dat")
  CALL GO()
  CLOSE(98)
 
-END
+END PROGRAM Fun2D
