cf0dda02de6ea11a67239938461c057bf7ee27db
Teodoro Laino <teodoro.laino@gmail.com>
2009-03-17 16:28:25 +0000

    deleting the several RC_* keywords in SE and introducing several section for COULOMB, EXCHANGE and SCREENING. Added different taper functions for the several terms and reset 2 regtests for numerics.
    svn-origin-rev: 8322

--- src/semi_empirical_int_debug.h
+++ src/semi_empirical_int_debug.h
@@ -34,12 +34,13 @@ END INTERFACE check_rotmat_der
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_dssss_nucint_ana
-  SUBROUTINE check_dssss_nucint_ana (sepi,sepj,r,dssss,itype,se_int_control,taper,error)
+  SUBROUTINE check_dssss_nucint_ana (sepi,sepj,r,dssss,itype,se_int_control,&
+       se_taper,error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type  
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
@@ -47,7 +48,7 @@ INTERFACE check_dssss_nucint_ana
     REAL(dp), INTENT(IN)                     :: dssss
     INTEGER, INTENT(IN)                      :: itype
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     TYPE(cp_error_type), INTENT(inout)       :: error
   END SUBROUTINE check_dssss_nucint_ana
 END INTERFACE check_dssss_nucint_ana
@@ -61,12 +62,13 @@ END INTERFACE check_dssss_nucint_ana
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_dcore_nucint_ana
-  SUBROUTINE check_dcore_nucint_ana (sepi,sepj,r,dcore,itype,se_int_control,taper,error)
+  SUBROUTINE check_dcore_nucint_ana (sepi,sepj,r,dcore,itype,se_int_control,&
+       se_taper,error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
@@ -74,7 +76,7 @@ INTERFACE check_dcore_nucint_ana
     REAL(dp), DIMENSION(10, 2), INTENT(IN)   :: dcore
     INTEGER, INTENT(IN)                      :: itype
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     TYPE(cp_error_type), INTENT(inout)       :: error
   END SUBROUTINE check_dcore_nucint_ana
 END INTERFACE check_dcore_nucint_ana
@@ -88,19 +90,20 @@ END INTERFACE check_dcore_nucint_ana
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_drotnuc_ana
-   SUBROUTINE check_drotnuc_ana(sepi, sepj, rijv, itype, se_int_control, taper, e1b, e2a, de1b, de2a, error)
+   SUBROUTINE check_drotnuc_ana(sepi, sepj, rijv, itype, se_int_control, se_taper,&
+        e1b, e2a, de1b, de2a, error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
     REAL(dp), DIMENSION(3), INTENT(IN)       :: rijv
     INTEGER, INTENT(IN)                      :: itype
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     REAL(dp), DIMENSION(45), INTENT(IN), &
       OPTIONAL                               :: e1b, e2a
     REAL(dp), DIMENSION(45, 3), &
@@ -118,12 +121,13 @@ END INTERFACE check_drotnuc_ana
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_dcorecore_ana
-  SUBROUTINE check_dcorecore_ana(sepi, sepj, rijv, itype,se_int_control, taper, enuc, denuc, error)
+  SUBROUTINE check_dcorecore_ana(sepi, sepj, rijv, itype,se_int_control,&
+       se_taper, enuc, denuc, error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
@@ -133,7 +137,7 @@ INTERFACE check_dcorecore_ana
     REAL(dp), DIMENSION(3), INTENT(IN), &
          OPTIONAL                            :: denuc
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     TYPE(cp_error_type), INTENT(inout)       :: error
   END SUBROUTINE check_dcorecore_ana
 
@@ -148,19 +152,20 @@ END INTERFACE check_dcorecore_ana
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE rot_2el_2c_first_debug
-  SUBROUTINE rot_2el_2c_first_debug(sepi, sepj, rijv, se_int_control, taper, invert, ii, kk, v_d, error)
+  SUBROUTINE rot_2el_2c_first_debug(sepi, sepj, rijv, se_int_control, se_taper,&
+       invert, ii, kk, v_d, error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
     REAL(KIND=dp), DIMENSION(3), INTENT(IN)  :: rijv
     LOGICAL, INTENT(IN)                      :: invert
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     INTEGER, INTENT(IN)                      :: ii, kk
     REAL(KIND=dp), DIMENSION(45, 45, 3), &
       INTENT(IN)                             :: v_d
@@ -177,12 +182,12 @@ END INTERFACE rot_2el_2c_first_debug
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_dterep_ana
-  SUBROUTINE check_dterep_ana (sepi,sepj,r,ri,dri,se_int_control,taper,lgrad,error)
+  SUBROUTINE check_dterep_ana (sepi,sepj,r,ri,dri,se_int_control,se_taper,lgrad,error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
@@ -190,7 +195,7 @@ INTERFACE check_dterep_ana
     REAL(dp), DIMENSION(491), INTENT(IN)     :: ri, dri
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
     LOGICAL, INTENT(IN)                      :: lgrad
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     TYPE(cp_error_type), INTENT(inout)       :: error
   END SUBROUTINE check_dterep_ana
 END INTERFACE check_dterep_ana
@@ -204,12 +209,12 @@ END INTERFACE check_dterep_ana
 !> \author Teodoro Laino - Zurich University
 ! *****************************************************************************
 INTERFACE check_rotint_ana
-  SUBROUTINE check_rotint_ana(sepi,sepj,rijv,w,dw,se_int_control,taper,error)
+  SUBROUTINE check_rotint_ana(sepi,sepj,rijv,w,dw,se_int_control,se_taper,error)
     USE f77_blas
     USE kinds,                           ONLY: dp
     USE semi_empirical_types,            ONLY: semi_empirical_type,&
-                                               se_int_control_type
-    USE taper_types,                     ONLY: taper_type
+                                               se_int_control_type,&
+                                               se_taper_type
 #include "cp_common_uses.h"
     IMPLICIT NONE
     TYPE(semi_empirical_type), POINTER       :: sepi, sepj
@@ -219,7 +224,7 @@ INTERFACE check_rotint_ana
     REAL(dp), DIMENSION(2025, 3), &
       INTENT(IN), OPTIONAL                   :: dw
     TYPE(se_int_control_type), INTENT(IN)    :: se_int_control
-    TYPE(taper_type), POINTER                :: taper
+    TYPE(se_taper_type), POINTER             :: se_taper
     TYPE(cp_error_type), INTENT(inout)       :: error
   END SUBROUTINE check_rotint_ana
 END INTERFACE check_rotint_ana
