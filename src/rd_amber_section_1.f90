bded92b15745d19c93b3f86ca3058261b802f24e
Teodoro Laino <teodoro.laino@gmail.com>
2009-09-17 22:37:35 +0000

    removing more unused dummy arguments + cleaning
    svn-origin-rev: 8957

--- src/rd_amber_section_1.f90
+++ src/rd_amber_section_1.f90
@@ -4,7 +4,11 @@
        IF (parser_test_next_token(parser,error=error)=="EOL")&
             CALL parser_get_next_line(parser,1,at_end=my_end,error=error)
        IF (my_end) EXIT
+#if defined (LOWER_TO_UPPER)
        CALL parser_get_object  (parser,array1(i),lower_to_upper=.TRUE.,error=error)
+#else
+       CALL parser_get_object  (parser,array1(i),error=error)
+#endif
        i = i + 1
     END DO
     ! Trigger end of file aborting
