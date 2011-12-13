8b73be7b2bf834d4cd7fcd851cd9661faa5575c9
Matthias Krack <matthias.krack@psi.ch>
2011-12-13 09:41:33 +0000

    Prompt explicitly before removing possible new arch files (unless the -f flag is specified)
    svn-origin-rev: 12090

--- tools/clean_cwd.sh
+++ tools/clean_cwd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # clean_cwd: Cleans the current working directory and all its subdirectories from scratch files,
-#            i.e. files which are not committed to svn
+#            i.e. files which are not committed to SVN
 echo "The directory $PWD is cleaned from scratch files"
 if [[ $1 == "-f" ]]; then
   echo "NOTE: You requested to remove all files without being prompted"
@@ -13,7 +13,7 @@ for file in $(svn status | grep '? ' | awk '{print $2}'); do
     elif [[ $1 == "-i" ]]; then
       rm -ir ${file}
     else
-      rm -r $file
+      rm -r ${file}
       echo Directory ${file} removed
     fi
   elif [[ -f ${file} ]]; then
@@ -25,9 +25,17 @@ for file in $(svn status | grep '? ' | awk '{print $2}'); do
     else
       if [[ ${file##*.} == "inp" ]]; then
         echo "This files could be a new input file. Do you really want to delete it?"
-        rm -i $file
+        rm -i ${file}
+      elif [ ${file##*.} == "sopt" ] ||
+           [ ${file##*.} == "popt" ] ||
+           [ ${file##*.} == "sdbg" ] || 
+           [ ${file##*.} == "pdbg" ] ||
+           [ ${file##*.} == "ssmp" ] ||
+           [ ${file##*.} == "psmp" ]; then
+        echo "This files could be a new arch file. Do you really want to delete it?"
+        rm -i ${file}
       else
-        rm -f $file
+        rm -f ${file}
         echo File ${file} removed
       fi
     fi
