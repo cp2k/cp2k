e67f652e0faacc2e7cd01502b21f239727f50c75
Ole Schütt <ole.schuett@cp2k.org>
2014-03-14 09:28:22 +0000

    New Makefile (needed for create_dbcsr_tarball.sh)
    svn-origin-rev: 13688

---
+++ tools/create_dbcsr_tarball.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+#set -x
+
+TMPDIR=`mktemp -d`
+REV=`./get_revision_number | sed -e "s/://"`
+OUTNAME=libdbcsr_$REV
+OUTDIR=$TMPDIR/$OUTNAME
+
+#echo "Create tempdir: " $OUTDIR
+mkdir $OUTDIR
+
+mkdir $OUTDIR/src
+rsync -axq \
+   --exclude=".*" \
+   --exclude="**/preprettify/" \
+   --exclude="*.instantiation" \
+   --exclude="*.template" \
+   --exclude="*.pyc" \
+   --exclude="libcusmm.cu" \
+   ../src/dbcsr/ $OUTDIR/src/
+
+mkdir $OUTDIR/tools
+cp -a ../tools/makedep.py  $OUTDIR/tools/
+cp -a ../tools/get_arch_code  $OUTDIR/tools/
+cp -a ../tools/get_revision_number  $OUTDIR/tools/
+
+mkdir $OUTDIR/tools/dbcsr_test/
+rsync -axq --exclude=".*" ../tools/dbcsr_test/  $OUTDIR/tools/dbcsr_test/
+
+mkdir $OUTDIR/tools/build_libsmm/
+rsync -axq \
+   --exclude=".*" \
+   --exclude="**/run_*/" \
+   --exclude="**/lib/" \
+   --exclude="*.out" \
+   --exclude="*.o" \
+   --exclude="*.mod" \
+   ../tools/build_libsmm/  $OUTDIR/tools/build_libsmm/
+
+mkdir $OUTDIR/makefiles
+cp -a ../makefiles/Makefile  $OUTDIR/makefiles/
+
+mkdir $OUTDIR/arch/
+rsync -axq --exclude=".*" ../arch/  $OUTDIR/arch/
+
+# pack everything into a tar.gz-archive
+rm -f $OUTNAME.tgz
+tar -cz --directory=$TMPDIR --file=$OUTNAME.tgz $OUTNAME
+
+#clean up
+rm -r $TMPDIR
+
+echo "Assembled: " $OUTNAME.tgz
+#EOF
---
+++ tools/makedep.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re, sys
+from os import path
+from os.path import dirname, basename, normpath
+
+# pre-compiled regular expressions
+re_module = re.compile(r"(?:^|\n)\s*module\s+(\w+)\s.*\n\s*end\s*module",re.DOTALL)
+re_program = re.compile(r"\n\s*end\s*program")
+re_use    = re.compile(r"\n\s*use\s+(\w+)")
+re_incl1  = re.compile(r"\n\s*include\s+['\"](.+)['\"]")
+re_incl2  = re.compile(r"\n#include\s+['\"](.+)['\"]")
+
+
+#=============================================================================
+def main():
+    messages = []
+    #process command line arguments
+    out_fn  = sys.argv[1]
+    mod_format = sys.argv[2]
+    mode = sys.argv[3]
+    src_files = sys.argv[4:]
+    if(mod_format not in ('lower', 'upper', 'no')):
+        error('Module filename format must be eighter of "lower", "upper", or "no".')
+    if(mode not in ('normal', 'hackdep', 'mod_compiler')):
+        error('Mode must be eighter of "normal", "hackdep", or "mod_compiler".')
+    for fn in src_files:
+        if(not fn.startswith("/")):
+            error("Path of source-file not absolut: "+fn)
+    src_basenames = [basename(fn).rsplit(".", 1)[0] for fn in src_files]
+    for bfn in src_basenames:
+        if(src_basenames.count(bfn) > 1):
+            error("Multiple source files with the same basename: "+bfn)
+
+    # parse files
+    parsed_files = dict()
+    for fn in src_files:
+        parse_file(parsed_files, fn) #parses also included files
+    messages.append("Parsed %d files"%len(parsed_files))
+
+    # create table mapping fortan module-names to file-name
+    mod2fn = dict()
+    for fn in src_files:
+        for m in parsed_files[fn]['module']:
+            if(mod2fn.has_key(m)):
+                error('Multiple declarations of module "%s"'%m)
+            mod2fn[m] = fn
+    messages.append("Created mod2fn table, found %d modules."%len(mod2fn))
+
+    # check "one module per file"-convention
+    for m, fn in mod2fn.items():
+        if(basename(fn) != m+".F"):
+            error("Names of module and file do not match: "+fn)
+
+    # read package manifests
+    packages = dict()
+    for fn in src_files:
+        p = normpath(dirname(fn))
+        read_pkg_manifest(packages, p)
+    messages.append("Read %d package manifests"%len(packages))
+
+    # check dependencies against package manifests
+    n_deps = 0
+    for fn in src_files:
+        p = normpath(dirname(fn))
+        if(not parsed_files[fn]['program']):
+            packages[p]['objects'].append(src2obj(basename(fn)))
+        deps = collect_include_deps(parsed_files, fn)
+        deps += [ mod2fn[m] for m in collect_use_deps(parsed_files, fn) if mod2fn.has_key(m) ]
+        n_deps += len(deps)
+        for d in deps:
+            if(normpath(dirname(d)) not in packages[p]['allowed_deps']):
+                error("Dependency forbidden according to package manifest: %s -> %s"%(fn, d))
+    messages.append("Checked %d dependencies"%n_deps)
+
+    # check for circular dependencies
+    for fn in parsed_files.keys():
+        find_cycles(parsed_files, mod2fn, fn)
+
+    # write messages as comments
+    makefile = "".join(["#makedep: %s\n"%m for m in messages])
+    makefile += "\n"
+
+    # write rules for archives
+    for p in packages.keys():
+        if(len(packages[p]['objects']) > 0):
+            makefile += "# Package %s\n"%p
+            makefile += "$(LIBDIR)/%s.a : "%packages[p]['archive']
+            makefile += " ".join(packages[p]['objects']) + "\n\n"
+
+    # write rules for executables
+    for fn in src_files:
+        if(not parsed_files[fn]['program']):
+            continue
+        bfn = basename(fn).rsplit(".", 1)[0]
+        makefile += "# Program %s\n"%fn
+        makefile += "$(EXEDIR)/%s.$(ONEVERSION) : %s.o "%(bfn, bfn)
+        p = normpath(dirname(fn))
+        deps = collect_pkg_deps(packages, p)
+        makefile += " ".join(["$(LIBDIR)/%s.a"%a for a in deps]) + "\n"
+        makefile += "\t" + "$(LD) $(LDFLAGS) -L$(LIBDIR) -o $@ %s.o "%bfn
+        assert(all([a.startswith("lib") for a in deps]))
+        makefile += " ".join(["-l"+a[3:] for a in deps])
+        makefile += " $(LIBS)\n\n"
+
+    # write rules for objects
+    for fn in src_files:
+        deps = " ".join(collect_include_deps(parsed_files, fn))
+        mods = collect_use_deps(parsed_files, fn)
+        mods.sort(cmp_mods) # sort mods to speedup compilation
+        for m in mods:
+            if(mod2fn.has_key(m)):
+                deps += " " + mod2modfile(m, mod_format)
+        if(mode == "hackdep"):
+            deps = ""
+
+        bfn = basename(fn)
+        makefile += "# Object %s\n"%bfn
+        provides = [mod2modfile(m, mod_format) for m in parsed_files[fn]['module']]
+        for mfn in provides:
+            makefile += "%s : %s "%(mfn, bfn) + deps + "\n"
+        makefile += "%s : %s "%(src2obj(bfn), bfn) + deps
+        if(mode == "mod_compiler"):
+            makefile += " " + " ".join(provides)
+        makefile += "\n\n"
+
+    f = open(out_fn, "w")
+    f.write(makefile)
+    f.close()
+
+
+#=============================================================================
+def cmp_mods(a,b):
+    # list "type" modules first, they are probably on the critical path
+    if("type" in a):
+        return -1
+    if("type" in b):
+        return +1
+    return 0
+
+
+#=============================================================================
+def parse_file(parsed_files, fn):
+    if(fn in parsed_files): return
+
+    content = open(fn).read()
+
+    # re.IGNORECASE is horribly expensive. Converting to lower-case upfront
+    content_lower = content.lower()
+
+    mods   = re_module.findall(content_lower)
+    prog   = re_program.search(content_lower) != None
+    uses   = re_use.findall(content_lower)
+    incl1_iter = re_incl1.finditer(content_lower) # fortran includes
+    incls  = [ content[m.start(1):m.end(1)] for m in incl1_iter ]
+    incls += re_incl2.findall(content) #cpp includes, those are case-sensitiv
+
+    # exclude included files from outside the source tree
+    def incl_fn(i):
+        return normpath(path.join(dirname(fn), i))
+
+    existing_incl = [i for i in incls if path.exists(incl_fn(i))]
+
+    # store everything in parsed_files cache
+    parsed_files[fn] = {'module':mods, 'program': prog, 'use':uses, 'include':existing_incl}
+
+    # parse included files
+    for i in existing_incl:
+        parse_file(parsed_files, incl_fn(i))
+
+
+#=============================================================================
+def read_pkg_manifest(packages, p):
+    if(packages.has_key(p)): return
+
+    fn = p+"/PACKAGE"
+    if(not path.exists(fn)):
+        error("Could not open PACKAGE manifest: "+fn)
+    content = open(fn).read()
+
+    packages[p] = eval(content)
+    packages[p]['objects'] = []
+    if(not packages[p].has_key("archive")):
+        packages[p]['archive'] = "libcp2k"+basename(p)
+    packages[p]['allowed_deps'] = [normpath(p)]
+    packages[p]['allowed_deps'] += [normpath(path.join(p,r)) for r in packages[p]['requires']]
+
+    for r in packages[p]['requires']:
+        read_pkg_manifest(packages, normpath(path.join(p,r)))
+
+
+#=============================================================================
+def mod2modfile(m, mod_format):
+    if(mod_format == 'no'):
+        return("")
+    if(mod_format == 'lower'):
+        return(m.lower() + ".mod")
+    if(mod_format == 'upper'):
+        return(m.upper() + ".mod")
+    assert(False) # modeps unkown
+
+
+#=============================================================================
+def src2obj(src_fn):
+    return( basename(src_fn).rsplit(".",1)[0] + ".o" )
+
+
+#=============================================================================
+def collect_include_deps(parsed_files, fn):
+    pf = parsed_files[fn]
+    incs = []
+
+    for i in pf['include']:
+        fn_inc = normpath(path.join(dirname(fn), i))
+        if(parsed_files.has_key(fn_inc)):
+            incs.append(fn_inc)
+            incs += collect_include_deps(parsed_files, fn_inc)
+
+    return(list(set(incs)))
+
+
+#=============================================================================
+def collect_use_deps(parsed_files, fn):
+    pf = parsed_files[fn]
+    uses = pf['use']
+
+    for i in pf['include']:
+        fn_inc = normpath(path.join(dirname(fn), i))
+        if(parsed_files.has_key(fn_inc)):
+            uses += collect_use_deps(parsed_files, fn_inc)
+
+    return(list(set(uses)))
+
+
+#=============================================================================
+def find_cycles(parsed_files, mod2fn, fn, S=None):
+    pf = parsed_files[fn]
+    if(pf.has_key('visited')):
+        return
+
+    if(S==None):
+        S = []
+
+    for m in pf['module']:
+        if(m in S):
+            i = S.index(m)
+            error("Circular dependency: "+ " -> ".join(S[i:] + [m]))
+        S.append(m)
+
+    for m in collect_use_deps(parsed_files, fn):
+        if(mod2fn.has_key(m)):
+            find_cycles(parsed_files, mod2fn, mod2fn[m], S)
+
+    for m in pf['module']:
+        S.pop()
+
+    pf['visited'] = True
+
+
+#=============================================================================
+def collect_pkg_deps(packages, p, archives=None, S=None):
+    if(archives == None): archives = []
+    if(S == None): S = []
+
+    a = packages[p]['archive']
+    if(a in archives):
+        return(archives)
+
+    if(a in S):
+        i = S.index(a)
+        error("Circular package dependency: "+ " -> ".join(S[i:] + [a]))
+    S.append(a)
+
+    for r in packages[p]['requires']:
+        d = normpath(path.join(p,r))
+        collect_pkg_deps(packages, d, archives, S)
+
+    S.pop()
+
+    if(len(packages[p]['objects']) > 0):
+        archives.insert(0, packages[p]['archive'])
+
+    return(archives)
+
+
+#=============================================================================
+def error(msg):
+    sys.stderr.write("makedep error: %s\n"%msg)
+    sys.exit(1)
+
+
+#=============================================================================
+main()
+
+#EOF
