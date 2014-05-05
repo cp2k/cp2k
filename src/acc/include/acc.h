8cceb27e3a40d191aab124bf974afafc8fe6746c
Ole Schütt <ole.schuett@cp2k.org>
2014-05-05 15:09:23 +0000

    ACC: Make header files usable for C and C++.
    svn-origin-rev: 13841

--- src/acc/include/acc.h
+++ src/acc/include/acc.h
@@ -3,34 +3,41 @@
  *  Copyright (C) 2000 - 2014 the CP2K developers group                      *
  *****************************************************************************/
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 // devices
-extern "C" int acc_get_ndevices(int *n_devices);
-extern "C" int acc_set_active_device(int device_id);
+int acc_get_ndevices(int *n_devices);
+int acc_set_active_device(int device_id);
 
 // streams
-extern "C" int acc_stream_priority_range(int* least, int* greatest);
-extern "C" int acc_stream_create(void** stream_p, char* name, int priority);
-extern "C" int acc_stream_destroy(void* stream);
-extern "C" int acc_stream_sync(void* stream);
-extern "C" int acc_stream_wait_event(void* stream, void* event);
+int acc_stream_priority_range(int* least, int* greatest);
+int acc_stream_create(void** stream_p, char* name, int priority);
+int acc_stream_destroy(void* stream);
+int acc_stream_sync(void* stream);
+int acc_stream_wait_event(void* stream, void* event);
 
 // events
-extern "C" int acc_event_create(void** event_p);
-extern "C" int acc_event_destroy(void* event);
-extern "C" int acc_event_record(void* event, void* stream);
-extern "C" int acc_event_query(void* event, int* has_occured);
-extern "C" int acc_event_synchronize(void* event);
+int acc_event_create(void** event_p);
+int acc_event_destroy(void* event);
+int acc_event_record(void* event, void* stream);
+int acc_event_query(void* event, int* has_occured);
+int acc_event_synchronize(void* event);
 
 // memory
-extern "C" int acc_dev_mem_allocate(void **dev_mem, size_t n);
-extern "C" int acc_dev_mem_deallocate(void *dev_mem);
-extern "C" int acc_host_mem_allocate(void **host_mem, size_t n);
-extern "C" int acc_host_mem_deallocate(void *host_mem);
-extern "C" int acc_memcpy_h2d(const void *host_mem, void *dev_mem, size_t count, void* stream);
-extern "C" int acc_memcpy_d2h(const void *dev_mem, void *host_mem, size_t count, void* stream);
-extern "C" int acc_memcpy_d2d(const void *devmem_src, void *devmem_dst, size_t count, void* stream);
-extern "C" int acc_memset_zero(void *dev_mem, size_t offset, size_t length, void* stream);
-extern "C" int acc_dev_mem_info(size_t* free, size_t* avail);
+int acc_dev_mem_allocate(void **dev_mem, size_t n);
+int acc_dev_mem_deallocate(void *dev_mem);
+int acc_host_mem_allocate(void **host_mem, size_t n);
+int acc_host_mem_deallocate(void *host_mem);
+int acc_memcpy_h2d(const void *host_mem, void *dev_mem, size_t count, void* stream);
+int acc_memcpy_d2h(const void *dev_mem, void *host_mem, size_t count, void* stream);
+int acc_memcpy_d2d(const void *devmem_src, void *devmem_dst, size_t count, void* stream);
+int acc_memset_zero(void *dev_mem, size_t offset, size_t length, void* stream);
+int acc_dev_mem_info(size_t* free, size_t* avail);
 
+#ifdef __cplusplus
+ }
+#endif
 
 //EOF
--- src/dbcsr/libsmm_acc/include/libsmm_acc.h
+++ src/dbcsr/libsmm_acc/include/libsmm_acc.h
@@ -3,11 +3,19 @@
 *  Copyright (C) 2000 - 2013 the CP2K developers group                      *
 *****************************************************************************/
 
-extern "C" int libsmm_acc_process (int *param_stack, int stack_size,
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+int libsmm_acc_process (int *param_stack, int stack_size,
     int nparams, int datatype, void *a_data, void *b_data, void *c_data,
     int m_max, int n_max, int k_max, int def_mnk, void* stream);
 
-extern "C" int libsmm_acc_transpose (int *trs_stack, int offset, int nblks,
+int libsmm_acc_transpose (int *trs_stack, int offset, int nblks,
     void *buffer,int datatype, int m, int n, void* stream);
+
+#ifdef __cplusplus
+ }
+#endif
 
 //EOF
