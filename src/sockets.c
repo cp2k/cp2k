a25a316b32a846c2ce595f63d3004104ba7f8a35
Michele Ceriotti <michele.ceriotti@epfl.ch>
2014-06-16 06:32:43 +0000

    Re-factored the sockets interface to be easily replaced by F90 version
    Defined a module interface for write/readbuffer and some wrappers that 
    convert F90 types in such a form they can be passed as a "void *"
    
    
    
    svn-origin-rev: 14077

--- src/sockets.c
+++ src/sockets.c
@@ -1,4 +1,39 @@
-#ifndef __NO_IPI_DRIVER
+/* A minimal wrapper for socket communication.
+
+Copyright (C) 2013, Joshua More and Michele Ceriotti
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+Contains both the functions that transmit data to the socket and read the data
+back out again once finished, and the function which opens the socket initially.
+Can be linked to a FORTRAN code that does not support sockets natively.
+
+Functions:
+   error: Prints an error message and then exits.
+   open_socket_: Opens a socket with the required host server, socket type and
+      port number.
+   write_buffer_: Writes a string to the socket.
+   read_buffer_: Reads data from the socket.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,11 +42,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/un.h>
-#include <netdb.h> 
-
-void error(const char *msg)
-// Prints an error message and then exits.
-{   perror(msg);  exit(-1);   }
+#include <netdb.h>
 
 void open_socket(int *psockfd, int* inet, int* port, char* host)
 /* Opens a socket.
@@ -46,14 +77,14 @@ Args:
 
       sprintf(service,"%d",*port); // convert the port number to a string
       ai_err = getaddrinfo(host, service, &hints, &res); 
-      if (ai_err!=0) error("Error fetching host data. Wrong host name?");
+      if (ai_err!=0) { perror("Error fetching host data. Wrong host name?"); exit(-1); }
 
       // creates socket
       sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-      if (sockfd < 0)  error("Error opening socket");
+      if (sockfd < 0) { perror("Error opening socket"); exit(-1); }
     
       // makes connection
-      if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) error("Error opening INET socket: wrong port or server unreachable");
+      if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) { perror("Error opening INET socket: wrong port or server unreachable"); exit(-1); }
       freeaddrinfo(res);
    }
    else
@@ -70,29 +101,41 @@ Args:
       sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 
       // connects
-      if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("Error opening UNIX socket: path unavailable, or already existing");
+      if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { perror("Error opening UNIX socket: path unavailable, or already existing"); exit(-1); }
    }
+
 
    *psockfd=sockfd;
 }
 
 void writebuffer(int *psockfd, char *data, int* plen)
+/* Writes to a socket.
+
+Args:
+   psockfd: The id of the socket that will be written to.
+   data: The data to be written to the socket.
+   plen: The length of the data in bytes.
+*/
+
 {
-   int n;   
+   int n;
    int sockfd=*psockfd;
    int len=*plen;
 
    n = write(sockfd,data,len);
-   if (n < 0) error("Error writing to socket: server has quit or connection broke");
+   if (n < 0) { perror("Error writing to socket: server has quit or connection broke"); exit(-1); }
 }
 
-void writebuffer_i(int *psockfd, int *data, int* plen)
-{ int llen=(*plen)*4; writebuffer(psockfd, (char*) data, &llen); }
 
-void writebuffer_d(int *psockfd, double *data, int* plen)
-{ int llen=(*plen)*8; writebuffer(psockfd, (char*) data, &llen); }
+void readbuffer(int *psockfd, char *data, int* plen)
+/* Reads from a socket.
 
-int readbuffer(int *psockfd, char *data, int* plen)
+Args:
+   psockfd: The id of the socket that will be read from.
+   data: The storage array for data read from the socket.
+   plen: The length of the data in bytes.
+*/
+
 {
    int n, nr;
    int sockfd=*psockfd;
@@ -103,15 +146,7 @@ int readbuffer(int *psockfd, char *data, int* plen)
    while (nr>0 && n<len )
    {  nr=read(sockfd,&data[n],len-n); n+=nr; }
 
-   if (n <= 0) error("Error reading from socket: server has quit or connection broke");
-
-   return n;
+   if (n == 0) { perror("Error reading from socket: server has quit or connection broke"); exit(-1); }
 }
 
-int readbuffer_i(int *psockfd, int *data, int* plen)
-{ int llen=(*plen)*4; return readbuffer(psockfd, (char*) data, &llen); }
 
-int readbuffer_d(int *psockfd, double *data, int* plen)
-{ int llen=(*plen)*8; return readbuffer(psockfd, (char*) data, &llen); }
-
-#endif
