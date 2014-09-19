3d0c8e35683cde8d9eb21292fd8a621c6ca42b0d
Florian Schiffmann <florian.schiffmann@mat.ethz.ch>
2014-09-19 10:24:40 +0000

    change in the defaults gives significant speed up on GPUs
    svn-origin-rev: 14293

--- src/dbcsr/libsmm_acc/libcusmm/generate.py
+++ src/dbcsr/libsmm_acc/libcusmm/generate.py
@@ -17,9 +17,9 @@ from kernels.cusmm_dnt_tiny    import Kernel_dnt_tiny
 def main():
     triples  = combinations(23)                 # blocked H2O (benchmark)
     triples += combinations(6)                  # idem min basis
-    triples += combinations(14,16,29)           # RPA water
-    triples += combinations(5, 16, 13, 24, 26)
-    triples += combinations(9, 16, 22)
+    triples += combinations(14,32,29)           # RPA water
+    triples += combinations(5, 32, 13, 24, 26)
+    triples += combinations(9, 32, 22)
     triples += combinations(32)
     triples += combinations(64)
     triples += combinations(78)
