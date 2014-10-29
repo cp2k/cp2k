debbaeb55edfd1e4547dda1652a90c47ed027886
Ole Schütt <ole.schuett@cp2k.org>
2014-10-29 09:06:40 +0000

    analyze_gfortran_ast.py: Add detection of implicitly typed subroutines
    svn-origin-rev: 14495

--- tools/analyze_gfortran_ast.py
+++ tools/analyze_gfortran_ast.py
@@ -7,38 +7,12 @@ import sys
 import re
 
 BANNED_STM  = ('GOTO', 'OPEN', 'CLOSE', )
-
-#taken from f77_blas_poison.F
-BANNED_CALL  = ('SROTG',  'DROTG', 'CROTG',  'ZROTG', 'SROTMG', 'DROTMG', 'SROT',   'DROT',
-                'ZROT',   'CSROT', 'ZDROT',  'SROTM', 'DROTM',  'SSWAP',  'DSWAP',  'CSWAP',
-                'ZSWAP',  'SSCAL', 'DSCAL',  'CSCAL', 'ZSCAL',  'CSSCAL', 'ZDSCAL', 'SCOPY',
-                'DCOPY',  'CCOPY', 'ZCOPY',  'SAXPY', 'DAXPY',  'CAXPY',  'ZAXPY',  'SDOT',
-                'DDOT',   'CDOTU', 'ZDOTU',  'CDOTC', 'ZDOTC',  'SNRM2',  'DNRM2',  'SCNRM2',
-                'DZNRM2', 'SASUM', 'SCASUM', 'DASUM', 'DZASUM', 'ISAMAX', 'IDAMAX', 'ICAMAX',
-                'IZAMAX')
-
-BANNED_CALL += ('SGEMV', 'DGEMV', 'CGEMV', 'ZGEMV', 'SGBMV', 'DGBMV', 'CGBMV', 'ZGBMV',
-                'CHEMV', 'ZHEMV', 'CHBMV', 'ZHBMV', 'CHPMV', 'ZHPMV', 'SSYMV', 'DSYMV',
-                'SSBMV', 'DSBMV', 'SSPMV', 'DSPMV', 'STRMV', 'DTRMV', 'CTRMV', 'ZTRMV',
-                'STBMV', 'DTBMV', 'CTBMV', 'ZTBMV', 'STPMV', 'DTPMV', 'CTPMV', 'ZTPMV',
-                'STRSV', 'DTRSV', 'CTRSV', 'ZTRSV', 'STBSV', 'DTBSV', 'CTBSV', 'ZTBSV',
-                'STPSV', 'DTPSV', 'CTPSV', 'ZTPSV', 'SGER',  'DGER',  'CGERU', 'ZGERU',
-                'CGERC', 'ZGERC', 'CHER',  'ZHER',  'CHPR',  'ZHPR',  'CHER2', 'ZHER2',
-                'CHPR2', 'ZHPR2', 'SSYR',  'DSYR',  'SSPR',  'DSPR',  'SSYR2', 'DSYR2',
-                'SSPR2', 'DSPR2')
-
-BANNED_CALL += ('SGEMM',  'DGEMM',  'CGEMM',  'ZGEMM',  'SSYMM',  'DSYMM',  'CSYMM',  'ZSYMM',
-                'CHEMM',  'ZHEMM',  'SSYRK',  'DSYRK',  'CSYRK',  'ZSYRK',  'CHERK',  'ZHERK',
-                'SSYR2K', 'DSYR2K', 'CSYR2K', 'ZSYR2K', 'CHER2K', 'ZHER2K', 'STRMM',  'DTRMM',
-                'CTRMM',  'ZTRMM',  'STRSM',  'DTRSM',  'CTRSM',  'ZTRSM',  'SDSDOT', 'DSDOT',
-                'DCABS1', 'LSAME',  'SCABS1')
-
-BANNED_CALL += ('CP_FM_GEMM', )
+BANNED_CALL = ('CP_FM_GEMM', )
 
 # precompile regex
-re_procedure = re.compile(r"^\s*procedure name = (.*)$")
 re_symbol    = re.compile(r"^\s*symtree.* symbol: '([^']+)'.*$")
-re_attr      = re.compile(r"^\s*attributes: (.*)$")
+re_use       = re.compile(r" USE-ASSOC\(([^)]+)\)")
+
 
 #===============================================================================
 def main():
@@ -58,9 +32,16 @@ def main():
         suppress = [l for l in lines if not l.startswith("#")]
         log_files = sys.argv[2:]
 
+    public_symbols = set()
+    used_symbols = set()
     issues = []
     for fn in log_files:
-        issues += process_log_file(fn)
+        issues += process_log_file(fn, public_symbols, used_symbols)
+
+    #unused_public_symbols = public_symbols - used_symbols
+    #print unused_public_symbols
+    #if(len(unused_public_symbols)>0):
+    #    issues.append("Found %d unused public symbols"%len(unused_public_symbols))
 
     issues = sorted(set(issues))
     issues_shown = [i for i in issues if(i not in suppress)]
@@ -74,45 +55,90 @@ def main():
     print "Status: " + ("OK" if n==0 else "FAILED")
 
 #===============================================================================
-def process_log_file(fn):
+def process_log_file(fn, public_symbols, used_symbols):
     issues = []
     ast = open(fn).read()
     lines = ast.split("\n")
+    module_name = None
 
-    curr_symbol = curr_procedure = None
+    curr_symbol = curr_procedure = curr_symbol_defined = None
+
     for line in lines:
-        m = re_procedure.match(line)
-        if(m):
-            curr_procedure = m.group(1)
-            continue
+        line = line.strip()
+        tokens = line.split()
 
-        m = re_symbol.match(line)
-        if(m):
-            curr_symbol = m.group(1)
-            continue
+        if(line.startswith("procedure name =")):
+            curr_procedure = line.split("=")[1].strip()
+            if(not module_name):
+                module_name = curr_procedure
 
-        m = re_attr.match(line)
-        if(m and ("IMPLICIT-SAVE" in line) and ("PARAMETER" not in line)):
-            issues.append(fn+': Symbol "'+curr_symbol+'" in procedure "'+curr_procedure+'" is IMPLICIT-SAVE')
-            continue
+        elif(line.startswith("symtree: ") or len(line)==0):
+            if(curr_symbol):
+                if(not curr_symbol_defined):
+                    issues.append(fn+': Symbol "'+curr_symbol+'" in "'+curr_procedure+'" not defined')
 
-        m = re_attr.match(line)
-        if(m and ("IMPLICIT-TYPE" in line)):
-            issues.append(fn+': Symbol "'+curr_symbol+'" in procedure "'+curr_procedure+'" is IMPLICIT-TYPE')
-            continue
+            curr_symbol = curr_symbol_defined = None
+            if(len(line)==0): continue
 
-        tokens = line.upper().strip().split()
-        if(tokens and tokens[0] in BANNED_STM):
-            issues.append(fn+": Found "+tokens[0]+' statement in procedure "'+curr_procedure+'"')
-            continue
+            curr_symbol_defined = False
+            curr_symbol = re_symbol.match(line).group(1)
+            if("from namespace" in line):
+                curr_symbol_defined = True
 
-        if(tokens and tokens[0]=="CALL" and tokens[1] in BANNED_CALL):
-            issues.append(fn+": Found CALL "+tokens[1]+' in procedure "'+curr_procedure+'"')
-            continue
+        elif(line.startswith("attributes:")):
+            if("USE-ASSOC" in line):
+                mod = re_use.search(line).group(1)
+                used_symbols.add(mod+"::"+curr_symbol)
+                curr_symbol_defined = True
+            #if(("SAVE" in line) and ("PARAMETER" not in line) and ("PUBLIC" in line)):
+            #    issues.append(fn+': Symbol "'+curr_symbol+'" in procedure "'+curr_procedure+'" is PUBLIC-SAVE')
+            if(("IMPLICIT-SAVE" in line) and ("PARAMETER" not in line) and ("USE-ASSOC" not in line)):
+                issues.append(fn+': Symbol "'+curr_symbol+'" in procedure "'+curr_procedure+'" is IMPLICIT-SAVE')
+            if(("IMPLICIT-TYPE" in line) and ("USE-ASSOC" not in line) and ("FUNCTION" not in line)): #TODO sure about last clause?
+                issues.append(fn+': Symbol "'+curr_symbol+'" in procedure "'+curr_procedure+'" is IMPLICIT-TYPE')
+            if("INTRINSIC-PROC" in line):
+                curr_symbol_defined = True
+            if("INTRINSIC" in line):
+                curr_symbol_defined = True
+            if("INTERNAL-PROC" in line):
+                curr_symbol_defined = True
+            if("MODULE-PROC" in line):
+                curr_symbol_defined = True
+            if("EXTERNAL" in line):
+                curr_symbol_defined = True
+            if("MODULE" in line):
+                curr_symbol_defined = True
+            if("PROGRAM" in line):
+                curr_symbol_defined = True
+            if("LABEL" in line):
+                curr_symbol_defined = True
+            if("PUBLIC" in line):
+                public_symbols.add(module_name+"::"+curr_symbol)
 
-        if(tokens and tokens[0]=="!$OMP"):
-            if("OMP PARALLEL" in line and "DEFAULT(NONE)" not in line):
+        elif(line.startswith("type spec :")):
+            if("UNKNOWN" not in line):
+                curr_symbol_defined = True
+        elif(line.startswith("value:")):
+            curr_symbol_defined = True
+        elif(line.startswith("result:")):
+            curr_symbol_defined = True
+        elif(line.startswith("components:")):
+            curr_symbol_defined = True
+        elif(line.startswith("Generic interfaces:")):
+            curr_symbol_defined = True
+        elif(line.startswith("Formal arglist:")):
+            curr_symbol_defined = True
+
+        elif(line.startswith("!$OMP PARALLEL")):
+            if("DEFAULT(NONE)" not in line):
                 issues.append(fn+': OMP PARALLEL without DEFAULT(NONE) found in "'+curr_procedure+'"')
+
+        elif(line.startswith("CALL")):
+            if(tokens[1] in BANNED_CALL):
+                issues.append(fn+": Found CALL "+tokens[1]+' in procedure "'+curr_procedure+'"')
+
+        elif(tokens and tokens[0] in BANNED_STM):
+            issues.append(fn+": Found "+tokens[0]+' statement in procedure "'+curr_procedure+'"')
 
     return(issues)
 
