56d72b35a9d243ab7601cc79ccec9fbcaf039d09
Ole Schütt <ole.schuett@cp2k.org>
2015-01-27 19:09:28 +0000

    Toolchain: Add checksums for all archives, unset some variables.
    svn-origin-rev: 14959

--- tools/toolchain/install_cp2k_toolchain.sh
+++ tools/toolchain/install_cp2k_toolchain.sh
@@ -10,31 +10,37 @@
 # updating version could be easy, just change here:
 #
 
-binutilsver=2.24
+binutils_ver=2.24
+binutils_sha=4930b2886309112c00a279483eaef2f0f8e1b1b62010e0239c16b22af7c346d4
 
-valgrindver=3.10.0
+valgrind_ver=3.10.0
+valgrind_sha=03047f82dfc6985a4c7d9d2700e17bc05f5e1a0ca6ad902e5d6c81aeb720edc9
 
-lcovver=1.11
+lcov_ver=1.11
+lcov_sha=c282de8d678ecbfda32ce4b5c85fc02f77c2a39a062f068bd8e774d29ddc9bf8
 
-gccver=4.9.2
+gcc_ver=4.9.2
+gcc_sha=3e573826ec8b0d62d47821408fbc58721cd020df3e594cd492508de487a43b5e
 
-mpichver=3.1.2
+mpich_ver=3.1.2
+mpich_sha=37c3ba2d3cd3f4ea239497d9d34bd57a663a34e2ea25099c2cbef118c9156587
 
 # no version numbering used.
-scalapackver=XXXXXX
+scalapack_ver=XXXXXX
+scalapack_sha=8ecae0896a63c7980a71c22520afed6cfefb52b17c2496223026cc01caf07855
 
-libxcver=2.0.1
-libxcsha=c332f08648ec2bc7ccce83e45a84776215aa5dfebc64fae2a23f2ac546d41ea4
+libxc_ver=2.0.1
+libxc_sha=c332f08648ec2bc7ccce83e45a84776215aa5dfebc64fae2a23f2ac546d41ea4
 
-libintver=1.1.4
-libintsha=f67b13bdf1135ecc93b4cff961c1ff33614d9f8409726ddc8451803776885cff
+libint_ver=1.1.4
+libint_sha=f67b13bdf1135ecc93b4cff961c1ff33614d9f8409726ddc8451803776885cff
 
-fftwver=3.3.4
-fftwsha=8f0cde90929bc05587c3368d2f15cd0530a60b8a9912a8e2979a72dbe5af0982
+fftw_ver=3.3.4
+fftw_sha=8f0cde90929bc05587c3368d2f15cd0530a60b8a9912a8e2979a72dbe5af0982
 
 # will need hand-checking for non-standard dir name in tar
-elpaver=2013.11.008
-elpasha=d4a028fddb64a7c1454f08b930525cce0207893c6c770cb7bf92ab6f5d44bd78
+elpa_ver=2013.11.008
+elpa_sha=d4a028fddb64a7c1454f08b930525cce0207893c6c770cb7bf92ab6f5d44bd78
 
 #
 #
@@ -68,13 +74,18 @@ export FFLAGS="-O2 -g -Wno-error"
 export FCFLAGS="-O2 -g -Wno-error"
 export CXXFLAGS="-O2 -g -Wno-error"
 
+# F90 and F90FLAGS confuse mpich's configure.
+unset F90
+unset F90FLAGS
+
 echo "==================== Installing binutils ================="
-if [ -f binutils-${binutilsver}.tar.gz  ]; then
+if [ -f binutils-${binutils_ver}.tar.gz  ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://ftp.gnu.org/gnu/binutils/binutils-${binutilsver}.tar.gz
-  tar -xzf binutils-${binutilsver}.tar.gz
-  cd binutils-${binutilsver}
+  wget http://ftp.gnu.org/gnu/binutils/binutils-${binutils_ver}.tar.gz
+  echo "${binutils_sha} *binutils-${binutils_ver}.tar.gz" | sha256sum  --check
+  tar -xzf binutils-${binutils_ver}.tar.gz
+  cd binutils-${binutils_ver}
   ./configure --prefix=${INSTALLDIR} --enable-gold --enable-plugins >& config.log
   make -j $nprocs >& make.log
   make -j $nprocs install >& install.log
@@ -82,37 +93,40 @@ else
 fi
 
 echo "==================== Installing valgrind ================="
-if [ -f valgrind-${valgrindver}.tar.bz2 ]; then
+if [ -f valgrind-${valgrind_ver}.tar.bz2 ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://valgrind.org/downloads/valgrind-${valgrindver}.tar.bz2
-  tar -xjf valgrind-${valgrindver}.tar.bz2
-  cd valgrind-${valgrindver}
+  wget http://valgrind.org/downloads/valgrind-${valgrind_ver}.tar.bz2
+  echo "${valgrind_sha} *valgrind-${valgrind_ver}.tar.bz2" | sha256sum  --check
+  tar -xjf valgrind-${valgrind_ver}.tar.bz2
+  cd valgrind-${valgrind_ver}
   ./configure --prefix=${INSTALLDIR} >& config.log
   make -j $nprocs >& make.log
   make -j $nprocs install >& install.log
   cd ..
 fi
 
-echo "==================== Installing lcov ======================"
-if [ -f lcov-${lcovver}.tar.gz ]; then
+echo "==================== Installing lcov ====================="
+if [ -f lcov-${lcov_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://downloads.sourceforge.net/ltp/lcov-${lcovver}.tar.gz
-  tar -xzf lcov-${lcovver}.tar.gz
-  cd lcov-${lcovver}
+  wget http://downloads.sourceforge.net/ltp/lcov-${lcov_ver}.tar.gz
+  echo "${lcov_sha} *lcov-${lcov_ver}.tar.gz" | sha256sum  --check
+  tar -xzf lcov-${lcov_ver}.tar.gz
+  cd lcov-${lcov_ver}
   # note.... this installs in ${INSTALLDIR}/usr/bin
   make PREFIX=${INSTALLDIR} install >& make.log
   cd ..
 fi
 
 echo "==================== Installing gcc ======================"
-if [ -f gcc-${gccver}.tar.gz ]; then
+if [ -f gcc-${gcc_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget https://ftp.gnu.org/gnu/gcc/gcc-${gccver}/gcc-${gccver}.tar.gz
-  tar -xzf gcc-${gccver}.tar.gz
-  cd gcc-${gccver}
+  wget https://ftp.gnu.org/gnu/gcc/gcc-${gcc_ver}/gcc-${gcc_ver}.tar.gz
+  echo "${gcc_sha} *gcc-${gcc_ver}.tar.gz" | sha256sum  --check
+  tar -xzf gcc-${gcc_ver}.tar.gz
+  cd gcc-${gcc_ver}
   ./contrib/download_prerequisites >& prereq.log
   GCCROOT=${PWD}
   mkdir obj
@@ -155,24 +169,26 @@ export FFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native -ff
 export FCFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native -ffast-math"
 export CXXFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native -ffast-math"
 
-echo "==================== Installing mpich ======================"
-if [ -f mpich-${mpichver}.tar.gz ]; then
+echo "=================== Installing mpich ====================="
+if [ -f mpich-${mpich_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://www.mpich.org/static/downloads/${mpichver}/mpich-${mpichver}.tar.gz
-  tar -xzf mpich-${mpichver}.tar.gz
-  cd mpich-${mpichver}
+  wget http://www.mpich.org/static/downloads/${mpich_ver}/mpich-${mpich_ver}.tar.gz
+  echo "${mpich_sha} *mpich-${mpich_ver}.tar.gz" | sha256sum  --check
+  tar -xzf mpich-${mpich_ver}.tar.gz
+  cd mpich-${mpich_ver}
   ./configure --prefix=${INSTALLDIR} >& config.log
   make -j $nprocs >& make.log
   make -j $nprocs install >& install.log
   cd ..
 fi
 
-echo "==================== Installing scalapack ======================"
+echo "================= Installing scalapack ==================="
 if [ -f scalapack_installer.tgz ]; then
   echo "Installation already started, skipping it."
 else
   wget http://www.netlib.org/scalapack/scalapack_installer.tgz
+  echo "${scalapack_sha} *scalapack_installer.tgz" | sha256sum  --check
   tar -xzf scalapack_installer.tgz
   # we dont know the version
   cd scalapack_installer_*
@@ -186,58 +202,58 @@ else
 fi
 
 echo "==================== Installing libxc ===================="
-if [ -f libxc-${libxcver}.tar.gz ]; then
+if [ -f libxc-${libxc_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://www.cp2k.org/static/downloads/libxc-${libxcver}.tar.gz
-  echo "${libxcsha} *libxc-${libxcver}.tar.gz" | sha256sum  --check
-  tar -xzf libxc-${libxcver}.tar.gz
-  cd libxc-${libxcver}
+  wget http://www.cp2k.org/static/downloads/libxc-${libxc_ver}.tar.gz
+  echo "${libxc_sha} *libxc-${libxc_ver}.tar.gz" | sha256sum  --check
+  tar -xzf libxc-${libxc_ver}.tar.gz
+  cd libxc-${libxc_ver}
   ./configure  --prefix=${INSTALLDIR} >& config.log
   make -j $nprocs >& make.log
   make install >& install.log
   cd ..
 fi
 
-echo "==================== Installing libint ===================="
-if [ -f libint-${libintver}.tar.gz ]; then
+echo "=================== Installing libint ===================="
+if [ -f libint-${libint_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://www.cp2k.org/static/downloads/libint-${libintver}.tar.gz
-  echo "${libintsha} *libint-${libintver}.tar.gz" | sha256sum  --check
-  tar -xzf libint-${libintver}.tar.gz
-  cd libint-${libintver}
+  wget http://www.cp2k.org/static/downloads/libint-${libint_ver}.tar.gz
+  echo "${libint_sha} *libint-${libint_ver}.tar.gz" | sha256sum  --check
+  tar -xzf libint-${libint_ver}.tar.gz
+  cd libint-${libint_ver}
   ./configure  --prefix=${INSTALLDIR} --with-libint-max-am=5 --with-libderiv-max-am1=4 --with-cc-optflags="$CFLAGS" --with-cxx-optflags="$CXXFLAGS" >& config.log
   make -j $nprocs >&  make.log
   make install >& install.log
   cd ..
 fi
 
-echo "==================== Installing FFTW ===================="
-if [ -f fftw-${fftwver}.tar.gz ]; then
+echo "==================== Installing FFTW ====================="
+if [ -f fftw-${fftw_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://www.cp2k.org/static/downloads/fftw-${fftwver}.tar.gz
-  echo "${fftwsha} *fftw-${fftwver}.tar.gz" | sha256sum  --check 
-  tar -xzf fftw-${fftwver}.tar.gz
-  cd fftw-${fftwver}
+  wget http://www.cp2k.org/static/downloads/fftw-${fftw_ver}.tar.gz
+  echo "${fftw_sha} *fftw-${fftw_ver}.tar.gz" | sha256sum  --check
+  tar -xzf fftw-${fftw_ver}.tar.gz
+  cd fftw-${fftw_ver}
   ./configure  --prefix=${INSTALLDIR} --enable-openmp >& config.log
   make -j $nprocs >&  make.log
   make install >& install.log
   cd ..
 fi
 
-echo "==================== Installing ELPA ===================="
+echo "==================== Installing ELPA ====================="
 # elpa expect FC to be an mpi fortran compiler that's happy with long lines, and that a bunch of libs can be found
 export FC="mpif90 -ffree-line-length-none"
 export LDFLAGS="-L${INSTALLDIR}/lib"
 export LIBS="-lscalapack -lreflapack -lrefblas"
-if [ -f elpa-${elpaver}.tar.gz ]; then
+if [ -f elpa-${elpa_ver}.tar.gz ]; then
   echo "Installation already started, skipping it."
 else
-  wget http://www.cp2k.org/static/downloads/elpa-${elpaver}.tar.gz
-  echo "${elpasha} *elpa-${elpaver}.tar.gz" | sha256sum  --check
-  tar -xzf elpa-${elpaver}.tar.gz
+  wget http://www.cp2k.org/static/downloads/elpa-${elpa_ver}.tar.gz
+  echo "${elpa_sha} *elpa-${elpa_ver}.tar.gz" | sha256sum  --check
+  tar -xzf elpa-${elpa_ver}.tar.gz
 
   # need both flavors ?
   cp -rp ELPA_2013.11 ELPA_2013.11-mt
@@ -257,7 +273,7 @@ else
   cd ..
 fi
 
-echo "==================== generating arch files ===================="
+echo "================== generating arch files ================="
 echo "arch files can be found in the ${INSTALLDIR}/arch subdirectory"
 mkdir -p ${INSTALLDIR}/arch
 
@@ -417,12 +433,12 @@ CFLAGS   = ${CFLAGS}
 LIBS     = -lxc -lderiv -lint  -lreflapack -lrefblas -lstdc++ -lfftw3 -lfftw3_omp -lcudart -lcufft -lcublas -lrt
 EOF
 
-echo "==================== usage ===================="
+echo "========================== usage ========================="
 echo "done!"
 echo "now copy: cp ${INSTALLDIR}/arch/* to the cp2k/arch/ directory"
 echo "to use this toolchain or the cp2k version compiled with it you will first need to execute at the prompt:"
 echo "source ${SETUPFILE}"
 echo "to build CP2K you should change directory cd cp2k/makefiles/"
-echo "make -j${nprocs}" 'ARCH=local VERSION="sdbg sopt ssmp popt pdbg psmp"'
+echo "make -j${nprocs}" 'ARCH=local  VERSION="sdbg sopt ssmp popt pdbg psmp"'
 
 #EOF
