4707c372d9e2f7689462551793c156f1cb3eeb7b
Ole Schütt <ole.schuett@cp2k.org>
2015-02-12 17:46:02 +0000

    Conventions: link in all libraries
    svn-origin-rev: 15051

--- tools/conventions/analyze_gfortran_warnings.py
+++ tools/conventions/analyze_gfortran_warnings.py
@@ -23,6 +23,8 @@ lapack_re = re.compile("ILAENV|"
                        +"|SEN|SJA|SNA|SQR|SVP|SYL|TRD|TRF|TRI|TRS"
                        +"|SDD|EV|GV|SV|BS2D|BR2D))")
 
+warning_re = re.compile(".*[Ww]arning: (.*)")
+
 
 #===============================================================================
 def main():
@@ -49,30 +51,30 @@ def check_warnings(fn):
             loc = line.rsplit(":", 2)[0].strip()
             loc_short = path.basename(loc)
 
-        # we are only looking for warnings
-        if(not line.startswith("Warning:")): continue
+        m = warning_re.match(line)
+        if(not m): continue # we are only looking for warnings
+        warning = m.group(1)
 
         # we ignore these warnings
-        if("Creating array temporary" in line): continue
-        if("quality comparison" in line): continue
-        if("Unused" in line): 
-		if("error" in line or "routinep" in line):
-			continue
-        if("defined but not used" in line): continue
-        if("Removing call to function" in line): continue
-        if("Conversion from" in line): continue
-        if("CHARACTER expression" in line and "truncated" in line): continue
-        if("POINTER-valued function appears on right-hand side" in line): continue
+        if("-Wmaybe-uninitialized" in warning): continue
+        if("Creating array temporary" in warning): continue
+        if("quality comparison" in warning): continue
+        if("Unused" in warning and ("'error'" in warning or "'routinep'" in warning)): continue
+        if("defined but not used" in warning): continue
+        if("Removing call to function" in warning): continue
+        if("Conversion from" in warning): continue
+        if("CHARACTER expression" in warning and "truncated" in warning): continue
+        if("POINTER-valued function appears on right-hand side" in warning): continue
 
         # ok this warning we should handle
-        if("called with an implicit interface" in line):
-            parts = line.split()
-            assert(parts[1] == "Procedure")
-            routine = parts[2].strip("'").upper()
+        if("called with an implicit interface" in warning):
+            parts = warning.split()
+            assert(parts[0] == "Procedure")
+            routine = parts[1].strip("'").upper()
             if(may_call_implicit(loc, routine)): continue
             print "%s: Routine %s called with an implicit interface."%(loc_short, routine)
         else:
-            print "%s: %s"%(loc_short, line) # unkown warning, just output
+            print "%s: %s"%(loc_short, warning) # unkown warning, just output
 
 #===============================================================================
 def may_call_implicit(loc, routine):
