a29d3f12e7e7d9c70d18d44f31cacd46b4e96dba
Ole Schütt <ole.schuett@cp2k.org>
2015-09-04 12:41:13 +0000

    Dashboard: Fix runtimes, convert to minutes
    svn-origin-rev: 15813

--- tools/dashboard/generate_dashboard.py
+++ tools/dashboard/generate_dashboard.py
@@ -455,8 +455,9 @@ def parse_regtest_report(report_txt):
     if(memory_leaks > 0):
         report['summary'] += "; memleaks: %d"%memory_leaks
 
-    runtime = float(re.search("\nRegtest took (.+) seconds.\n", report_txt).group(1))
-    report['summary'] += "; time: %.0fs"%runtime
+    runtimes = [float(m) for m in re.findall("\nRegtest took (.+) seconds.\n", report_txt)]
+    report['summary'] += "; time: %.0fmin"%(sum(runtimes)/60.0)
+    print "; time: %.0fmin"%(sum(runtimes)/60.0)
 
     if(wrong_results>0 or runtime_errors>0 or memory_leaks>0):
         report['status'] = "FAILED"
