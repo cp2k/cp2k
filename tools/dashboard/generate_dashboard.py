7ed776e4923d5a571563788eebac57f29d861737
Ole Schütt <ole.schuett@cp2k.org>
2015-09-04 13:08:31 +0000

    Dashboard: one more fix
    svn-origin-rev: 15814

--- tools/dashboard/generate_dashboard.py
+++ tools/dashboard/generate_dashboard.py
@@ -456,8 +456,7 @@ def parse_regtest_report(report_txt):
         report['summary'] += "; memleaks: %d"%memory_leaks
 
     runtimes = [float(m) for m in re.findall("\nRegtest took (.+) seconds.\n", report_txt)]
-    report['summary'] += "; time: %.0fmin"%(sum(runtimes)/60.0)
-    print "; time: %.0fmin"%(sum(runtimes)/60.0)
+    report['summary'] += "; %.0fmin"%(sum(runtimes)/60.0)
 
     if(wrong_results>0 or runtime_errors>0 or memory_leaks>0):
         report['status'] = "FAILED"
