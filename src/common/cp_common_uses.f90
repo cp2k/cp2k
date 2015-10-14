563c4aaee9c4f1209f7b0892d28f9b74a96dcd36
Joost VandeVondele <joost.vandevondele@cscs.ch>
2015-10-14 12:43:47 +0000

    introduce 'MARK_USED' macro, and employ for the elpa wrapper.
    svn-origin-rev: 16132

--- src/common/cp_common_uses.f90
+++ src/common/cp_common_uses.f90
@@ -39,3 +39,10 @@
 #define CPWARN(msg) CALL cp__w(__SHORT_FILE__,__LINE__,msg)
 #define CPABORT(msg) CALL cp__b(__SHORT_FILE__,__LINE__,msg)
 #define CPASSERT(cond) IF(.NOT.(cond))CALL cp__a(__SHORT_FILE__,__LINE__)
+
+! The MARK_USED macro can be used to mark an argument/variable as used.
+! It is intended to make it possible to switch on -Werror=unused-dummy-argument,
+! but deal elegantly with e.g. library wrapper routines that take arguments only used if the library is linked in. 
+! This code should be valid for any Fortran variable, is always standard conforming,
+! and will be optimized away completely by the compiler
+#define MARK_USED(foo) IF(.FALSE.)THEN; DO ; IF(SIZE(SHAPE(foo))==-1) EXIT ;  END DO ; ENDIF
