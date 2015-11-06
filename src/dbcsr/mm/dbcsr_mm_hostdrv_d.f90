27eaea8bfd0312939057b422bae12cdbccc7a403
Ole Schütt <ole.schuett@cp2k.org>
2015-11-06 15:47:02 +0000

    DBCSR: Add skeleton for novel XSMM driver
    svn-origin-rev: 16278

--- src/dbcsr/mm/dbcsr_mm_hostdrv_d.f90
+++ src/dbcsr/mm/dbcsr_mm_hostdrv_d.f90
@@ -132,6 +132,71 @@
   END SUBROUTINE smm_process_mm_stack_d
 
 
+#if 1
+
+! *****************************************************************************
+!> \brief Processes MM stack and issues libxsmm calls
+!>
+!> \param stack_descr ...
+!> \param[in] params           Stack of MM parameters
+!> \param[in] stack_size       Number of parameters
+!> \param[in] a_data           Left-matrix data
+!> \param[in] b_data           Right-matrix data
+!> \param[in,out] c_data       Product data
+! *****************************************************************************
+  SUBROUTINE xsmm_process_mm_stack_d(stack_descr, params,&
+       stack_size, a_data, b_data, c_data)
+
+#if defined(__LIBXSMM)
+    USE libxsmm, ONLY: libxsmm_dmm
+#endif
+
+    INTEGER, INTENT(IN)                       :: stack_size
+    TYPE(stack_descriptor_type), INTENT(IN)   :: stack_descr
+    INTEGER, DIMENSION(dbcsr_ps_width,1:stack_size), &
+      INTENT(IN)                              :: params
+    REAL(kind=real_8), DIMENSION(*), TARGET, INTENT(IN) :: a_data, &
+                                                 b_data
+    REAL(kind=real_8), DIMENSION(*), TARGET, &
+      INTENT(INOUT)                           :: c_data
+
+    CHARACTER(len=*), PARAMETER :: routineN = 'libxsmm_process_mm_stack_d', &
+      routineP = moduleN//':'//routineN
+
+#if defined(__LIBXSMM)
+    REAL(kind=real_8), DIMENSION(:,:), POINTER          :: a_ptr, &
+                                                 b_ptr, &
+                                                 c_ptr
+    INTEGER                                   :: fa, fb, fc, m, n, k
+    INTEGER                                   :: sp
+
+    DO sp = 1, stack_size
+       fa = params(p_a_first,sp)
+       fb = params(p_b_first,sp)
+       fc = params(p_c_first,sp)
+       m = params(p_m,sp)
+       n = params(p_n,sp)
+       k = params(p_k,sp)
+       a_ptr(1:m,1:k) => a_data(fa:fa+(m*k))
+       b_ptr(1:k,1:n) => b_data(fb:fb+(k*n))
+       c_ptr(1:m,1:n) => c_data(fc:fc+(m*n))
+
+       CALL libxsmm_dmm(m, n, k, a_ptr, b_ptr, c_ptr)
+    ENDDO
+#else
+   CPABORT("libxsmm not compiled in")
+   MARK_USED(params)
+   MARK_USED(stack_size)
+   !MARK_USED does not work with assumed size arguments
+   IF(.FALSE.)THEN; DO; IF(ABS(a_data(1))>ABS(b_data(1)))EXIT; ENDDO; ENDIF
+   IF(.FALSE.) c_data(1)=0
+#endif
+
+    MARK_USED(stack_descr)
+  END SUBROUTINE xsmm_process_mm_stack_d
+
+#endif
+
 ! *****************************************************************************
 !> \brief ...
 !> \param M ...
