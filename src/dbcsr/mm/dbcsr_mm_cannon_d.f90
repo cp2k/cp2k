5da18c4bed062fc3b6252bb4691e3c4d8b025dfd
Alfio Lazzaro <alfio.lazzaro@chem.uzh.ch>
2016-08-08 11:15:43 +0000

    DBCSR: make local and list indexing as default
    svn-origin-rev: 17201

--- src/dbcsr/mm/dbcsr_mm_cannon_d.f90
+++ src/dbcsr/mm/dbcsr_mm_cannon_d.f90
@@ -1,65 +1,6 @@
 ! **************************************************************************************************
 !> \brief Calculates norms of the entire matrix with minimal overhead.
 !> \param norms ...
-!> \param nrows ...
-!> \param row_p ...
-!> \param col_i ...
-!> \param blk_p ...
-!> \param rbs ...
-!> \param cbs ...
-!> \param DATA ...
-!> \param local ...
-!> \param local2global ...
-! **************************************************************************************************
-  SUBROUTINE calc_norms_d(norms, nrows,&
-       row_p, col_i, blk_p, rbs, cbs, DATA, local, local2global)
-    REAL(kind=sp), DIMENSION(:), INTENT(OUT) :: norms
-    INTEGER, INTENT(IN)                      :: nrows
-    INTEGER, DIMENSION(1:nrows+1), &
-      INTENT(IN)                             :: row_p
-    INTEGER, DIMENSION(*), INTENT(IN)        :: col_i, blk_p, rbs, cbs
-    REAL(kind=real_8), DIMENSION(*), &
-      INTENT(IN)                             :: DATA
-    LOGICAL, INTENT(IN)                      :: local
-    INTEGER, DIMENSION(*), INTENT(IN)        :: local2global
-
-    INTEGER                                  :: blk, bp, bpe, row, row_i, &
-                                                row_size
-    REAL(kind=sp)                            :: val
-
-!   ---------------------------------------------------------------------------
-
-    !$omp parallel default(none) &
-    !$omp          private (row_i, row, row_size, blk, bp, bpe, val) &
-    !$omp          shared (nrows, local) &
-    !$omp          shared (local2global, rbs, cbs, row_p, col_i, blk_p, &
-    !$omp                  data, norms)
-    !$omp do
-    DO row_i = 1, nrows
-       IF (local) THEN
-          row = local2global(row_i)
-       ELSE
-          row = row_i
-       ENDIF
-       row_size = rbs(row)
-       DO blk = row_p(row_i)+1, row_p(row_i+1)
-          IF (blk_p(blk) .NE. 0) THEN
-             bp = ABS(blk_p(blk))
-             bpe = bp + row_size * cbs(col_i(blk)) - 1
-             val = SQRT (REAL (SUM(ABS(DATA(bp:bpe))**2), KIND=sp))
-          ELSE
-             val = 0.0_sp
-          ENDIF
-          norms(blk) = val
-       ENDDO
-    ENDDO
-    !$omp end do
-    !$omp end parallel
-  END SUBROUTINE calc_norms_d
-
-! **************************************************************************************************
-!> \brief Calculates norms of the entire matrix with minimal overhead.
-!> \param norms ...
 !> \param nblks ...
 !> \param blki ...
 !> \param rbs ...
@@ -70,14 +11,13 @@
 !> \param local2global_cols ...
 ! **************************************************************************************************
   SUBROUTINE calc_norms_list_d(norms, nblks,&
-       blki, rbs, cbs, DATA, local, local2global_rows, local2global_cols)
+       blki, rbs, cbs, DATA, local2global_rows, local2global_cols)
     REAL(kind=sp), DIMENSION(:), INTENT(OUT) :: norms
     INTEGER, INTENT(IN)                      :: nblks
     INTEGER, DIMENSION(3,nblks), INTENT(IN)  :: blki
     INTEGER, DIMENSION(:), INTENT(IN)        :: rbs, cbs
     REAL(kind=real_8), DIMENSION(:), &
       INTENT(IN)                             :: DATA
-    LOGICAL, INTENT(IN)                      :: local
     INTEGER, DIMENSION(:), INTENT(IN)        :: local2global_rows
     INTEGER, DIMENSION(:), INTENT(IN)        :: local2global_cols
 
@@ -88,20 +28,15 @@
 
     !$omp parallel default(none) &
     !$omp          private (row, col, blk, bp, bpe, val) &
-    !$omp          shared (local, nblks) &
+    !$omp          shared (nblks) &
     !$omp          shared (rbs, cbs, blki, &
     !$omp                  data, norms, local2global_rows, local2global_cols)
     !$omp do
     DO blk = 1, nblks
        IF (blki(3,blk) .NE. 0) THEN
           bp = blki(3,blk)
-          IF (local) THEN
-             row = local2global_rows(blki(1,blk))
-             col = local2global_cols(blki(2,blk))
-          ELSE
-             row = blki(1,blk)
-             col = blki(2,blk)
-          ENDIF
+          row = local2global_rows(blki(1,blk))
+          col = local2global_cols(blki(2,blk))
           bpe = bp + rbs(row) * cbs(col) - 1
           val = SQRT (REAL (SUM(ABS(DATA(bp:bpe))**2), KIND=sp))
        ELSE
