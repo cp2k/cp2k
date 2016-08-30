96a3409aa1686b1a7379410b1c10e606820233a0
Patrick Seewald <patrick.seewald@chem.uzh.ch>
2016-08-30 15:14:15 +0000

    fix python selftest
    svn-origin-rev: 17333

--- tools/prettify/setup.py
+++ tools/prettify/setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+import sys
+if(len(sys.argv)==2 and sys.argv[-1]=="--selftest"):
+       sys.exit(0)
 
 from setuptools import setup
 
 setup(name='fprettify',
       description='auto-formatter for modern fortran source code',
-      author='Mohamed Fawzi, Patrick Seewald',
-      license = "GPL",
+      author='Mohamed Fawzi, Patrick Seewald, Ole Schuett',
+      license = 'GPL',
       entry_points={'console_scripts': ['fprettify = fprettify:main']},
       py_modules=['fprettify',
           'formatting.normalizeFortranFile',
