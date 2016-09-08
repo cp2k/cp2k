339b0a376934ab7d0d1639cb72ccca1b96e42a9d
Hossein Banihashemian <hossein.banihashemian@mat.ethz.ch>
2016-09-08 11:37:25 +0000

    (transport) add an extra input keyword.
    svn-origin-rev: 17375

--- src/start/libcp2k.h
+++ src/start/libcp2k.h
@@ -172,6 +172,7 @@ typedef struct {
     int     num_contacts;
     int     stride_contacts;
     int     tasks_per_energy_point;
+    int     tasks_per_pole;
     int     gpus_per_point;
     int     n_points_beyn;
     int     ncrc_beyn;
