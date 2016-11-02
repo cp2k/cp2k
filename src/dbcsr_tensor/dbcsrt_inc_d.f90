0b616c873a61f075fb4d8553897eb50188500677
Patrick Seewald <patrick.seewald@chem.uzh.ch>
2016-11-02 14:28:19 +0000

    DBCSR tensor framework representation of block-sparse n-rank tensors (3 <= n <= 6) as DBCSR matrices conversion / redistribution between different tensor distributions and different matrix representations unit test Tensor contraction not yet implemented
    svn-origin-rev: 17514

---
+++ src/dbcsr_tensor/dbcsrt_inc_d.f90
@@ -0,0 +1,317 @@
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_put_block for tensor rank 3.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to put
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_put_3d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3)), &
+         INTENT(IN)                                               :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d, dims_2d
+      REAL(kind=real_8), ALLOCATABLE, DIMENSION(:, :)                       :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+
+      ! reshape block
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL get_mapping_info(map_blk, dims_2d=dims_2d)
+      CALL allocate_any(dims_2d, block_2d)
+      CALL reshape_nd_to_2d_block(map_blk, block_2d, block)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+
+      CALL dbcsr_put_block(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_put_block for tensor rank 4.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to put
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_put_4d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), &
+         INTENT(IN)                                               :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4)), &
+         INTENT(IN)                                               :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d, dims_2d
+      REAL(kind=real_8), ALLOCATABLE, DIMENSION(:, :)                       :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+
+      ! reshape block
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL get_mapping_info(map_blk, dims_2d=dims_2d)
+      CALL allocate_any(dims_2d, block_2d)
+      CALL reshape_nd_to_2d_block(map_blk, block_2d, block)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+
+      CALL dbcsr_put_block(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_put_block for tensor rank 5.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to put
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_put_5d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), &
+         INTENT(IN)                                               :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4), sizes(5)), &
+         INTENT(IN)                                               :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d, dims_2d
+      REAL(kind=real_8), ALLOCATABLE, DIMENSION(:, :)                       :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+
+      ! reshape block
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL get_mapping_info(map_blk, dims_2d=dims_2d)
+      CALL allocate_any(dims_2d, block_2d)
+      CALL reshape_nd_to_2d_block(map_blk, block_2d, block)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+
+      CALL dbcsr_put_block(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_put_block for tensor rank 6.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to put
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_put_6d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), &
+         INTENT(IN)                                               :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4), sizes(5), sizes(6)), &
+         INTENT(IN)                                               :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d, dims_2d
+      REAL(kind=real_8), ALLOCATABLE, DIMENSION(:, :)                       :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+
+      ! reshape block
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL get_mapping_info(map_blk, dims_2d=dims_2d)
+      CALL allocate_any(dims_2d, block_2d)
+      CALL reshape_nd_to_2d_block(map_blk, block_2d, block)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+
+      CALL dbcsr_put_block(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d)
+   END SUBROUTINE
+
+
+! **************************************************************************************************
+!> \brief Generic implementation of dbcsrt_put_block (arbitrary tensor rank)
+!> \param tensor ...
+!> \param ind block index
+!> \param block block to put
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_put_anyd_block_d(tensor, ind, block)
+      TYPE(block_nd_d), INTENT(IN)                      :: block
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), &
+         INTENT(IN)                                               :: ind
+
+      SELECT CASE(dbcsrt_ndims(tensor))
+      CASE(3)
+         CALL dbcsrt_put_3d_block_d(tensor, ind, block%sizes, block%blk)
+      CASE(4)
+         CALL dbcsrt_put_4d_block_d(tensor, ind, block%sizes, block%blk)
+      END SELECT
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_get_block for tensor rank 3.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to get
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_get_3d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3)), &
+         INTENT(OUT)                                              :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d
+      REAL(kind=real_8), DIMENSION(:,:), POINTER                            :: block_2d_ptr
+      REAL(kind=real_8), DIMENSION(:,:), ALLOCATABLE                        :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+      LOGICAL :: tr, found
+
+      NULLIFY (block_2d_ptr)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+      CALL dbcsr_get_block_p(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d_ptr, tr, found)
+      CPASSERT(found)
+      ! convert pointer to allocatable
+      CALL allocate_any(SHAPE(block_2d_ptr), block_2d, source=block_2d_ptr)
+
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL reshape_2d_to_nd_block(map_blk, block_2d, block)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_get_block for tensor rank 4.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to get
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_get_4d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4)), &
+         INTENT(OUT)                                              :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d
+      REAL(kind=real_8), DIMENSION(:,:), POINTER                            :: block_2d_ptr
+      REAL(kind=real_8), DIMENSION(:,:), ALLOCATABLE                        :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+      LOGICAL                                                     :: tr, found
+
+      NULLIFY (block_2d_ptr)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+      CALL dbcsr_get_block_p(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d_ptr, tr, found)
+
+      ! convert pointer to allocatable
+      CALL allocate_any(SHAPE(block_2d_ptr), block_2d, source=block_2d_ptr)
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL reshape_2d_to_nd_block(map_blk, block_2d, block)
+      DEALLOCATE(block_2d_ptr)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_get_block for tensor rank 5.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to get
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_get_5d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                 :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)      :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)      :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4), sizes(5)), &
+         INTENT(OUT)                                            :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                        :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                     :: ind_2d
+      REAL(kind=real_8), DIMENSION(:,:), POINTER                          :: block_2d_ptr
+      REAL(kind=real_8), DIMENSION(:,:), ALLOCATABLE                      :: block_2d
+      TYPE(nd_to_2d_mapping)                                    :: map_blk
+      LOGICAL                                                   :: tr, found
+
+      NULLIFY (block_2d_ptr)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+      CALL dbcsr_get_block_p(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d_ptr, tr, found)
+
+      ! convert pointer to allocatable
+      CALL allocate_any(SHAPE(block_2d_ptr), block_2d, source=block_2d_ptr)
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL reshape_2d_to_nd_block(map_blk, block_2d, block)
+      DEALLOCATE(block_2d_ptr)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Implementation of dbcsrt_get_block for tensor rank 5.
+!> \param tensor ...
+!> \param ind block index
+!> \param sizes block size
+!> \param block block to get
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_get_6d_block_d(tensor, ind, sizes, block)
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), INTENT(IN)        :: sizes
+      REAL(kind=real_8), DIMENSION(sizes(1), sizes(2), sizes(3), sizes(4), sizes(5), sizes(6)), &
+         INTENT(OUT)                                              :: block
+
+      INTEGER, ALLOCATABLE, DIMENSION(:)                          :: map1_2d, map2_2d
+      INTEGER, DIMENSION(2)                                       :: ind_2d
+      REAL(kind=real_8), DIMENSION(:,:), POINTER                            :: block_2d_ptr
+      REAL(kind=real_8), DIMENSION(:,:), ALLOCATABLE                        :: block_2d
+      TYPE(nd_to_2d_mapping)                                      :: map_blk
+      LOGICAL                                                     :: tr, found
+
+      NULLIFY (block_2d_ptr)
+
+      ! convert block index
+      ind_2d(:) = get_2d_indices(tensor%nd_index_blk, ind)
+      CALL dbcsr_get_block_p(tensor%matrix_rep, ind_2d(1), ind_2d(2), block_2d_ptr, tr, found)
+
+      ! convert pointer to allocatable
+      CALL allocate_any(SHAPE(block_2d_ptr), block_2d, source=block_2d_ptr)
+      CALL get_mapping_info(tensor%nd_index_blk, map1_2d=map1_2d, map2_2d=map2_2d)
+      CALL create_nd_to_2d_mapping(map_blk, sizes, map1_2d, map2_2d)
+      CALL reshape_2d_to_nd_block(map_blk, block_2d, block)
+      DEALLOCATE(block_2d_ptr)
+   END SUBROUTINE
+
+! **************************************************************************************************
+!> \brief Generic implementation of dbcsrt_get_block (arbitrary tensor rank)
+!> \param tensor ...
+!> \param ind block index
+!> \param block block to get
+! **************************************************************************************************
+   SUBROUTINE dbcsrt_get_anyd_block_d(tensor, ind, block)
+      TYPE(block_nd), INTENT(OUT)                                 :: block
+      TYPE(dbcsrt_type), INTENT(INOUT)                            :: tensor
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor)), &
+         INTENT(IN)                                               :: ind
+      INTEGER, DIMENSION(dbcsrt_ndims(tensor))                    :: blk_size
+      REAL(kind=real_8), DIMENSION(:), ALLOCATABLE                          :: block_arr
+
+      CALL dbcsrt_blk_sizes(tensor, ind, blk_size)
+      ALLOCATE(block_arr(PRODUCT(blk_size)))
+
+      SELECT CASE(dbcsrt_ndims(tensor))
+      CASE (3)
+        CALL dbcsrt_get_3d_block_d(tensor, ind, blk_size, block_arr)
+      CASE (4)
+        CALL dbcsrt_get_4d_block_d(tensor, ind, blk_size, block_arr)
+      END SELECT
+      CALL create_block(block, blk_size, block_arr)
+   END SUBROUTINE
