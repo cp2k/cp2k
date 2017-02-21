9c37a19615df725df60aeb84bd0dbd1f61a33d92
Alfio Lazzaro <alfio.lazzaro@chem.uzh.ch>
2017-02-21 11:54:13 +0000

    DBCSR-RMA: more clean-up
    svn-origin-rev: 17768

--- src/dbcsr/mm/dbcsr_mm_3d_c.f90
+++ src/dbcsr/mm/dbcsr_mm_3d_c.f90
@@ -1,44 +1,41 @@
 ! **************************************************************************************************
-!> \brief Calculates norms of each image with minimal overhead.
+!> \brief Calculates norms with minimal overhead.
 !> \param buffer ...
 !> \param norms ...
 ! **************************************************************************************************
-  SUBROUTINE calc_image_norms_c(images,norms,uf,ul)
-  TYPE(dbcsr_1d_array_type), INTENT(IN)    :: images
-  REAL(kind=sp), DIMENSION(:, :), INTENT(INOUT) :: norms
-  INTEGER, INTENT(IN)                      :: uf, ul
+  SUBROUTINE calc_norms_c(matrix,norms)
+  TYPE(dbcsr_type), INTENT(IN)               :: matrix
+  REAL(kind=sp), DIMENSION(:), INTENT(INOUT) :: norms
 
   INTEGER, DIMENSION(:), POINTER    :: row, col, bps, rbs, cbs, &
                                        local_rows, local_cols
   COMPLEX(kind=real_4), DIMENSION(:), POINTER    :: data
-  INTEGER                           :: ui, blk, bpe
+  INTEGER                           :: blk, bpe
 
+  row => matrix%coo_l(1::3)
+  col => matrix%coo_l(2::3)
+  bps => matrix%coo_l(3::3)
+  rbs => array_data(matrix%row_blk_size)
+  cbs => array_data(matrix%col_blk_size)
+  local_rows => array_data(matrix%local_rows)
+  local_cols => array_data(matrix%local_cols)
+  data => dbcsr_get_data_p_c (matrix%data_area)
   !$omp parallel default(none) &
-  !$omp private(ui,row,col,bps,blk,bpe,data,&
-  !$omp         rbs,cbs,local_rows,local_cols) &
-  !$omp shared(norms,images,uf,ul)
-  DO ui=uf,ul
-     IF (images%mats(ui)%nblks.EQ.0) CYCLE
-     row => images%mats(ui)%coo_l(1::3)
-     col => images%mats(ui)%coo_l(2::3)
-     bps => images%mats(ui)%coo_l(3::3)
-     rbs => array_data(images%mats(ui)%row_blk_size)
-     cbs => array_data(images%mats(ui)%col_blk_size)
-     local_rows => array_data(images%mats(ui)%local_rows)
-     local_cols => array_data(images%mats(ui)%local_cols)
-     data => dbcsr_get_data_p_c (images%mats(ui)%data_area)
-     !$omp do
-     DO blk = 1, images%mats(ui)%nblks
-        IF (bps(blk).NE.0) THEN
-           bpe = bps(blk) + rbs(local_rows(row(blk))) * cbs(local_cols(col(blk))) - 1
-           norms(blk,ui) = SQRT (REAL (SUM(ABS(data(bps(blk):bpe))**2), KIND=sp))
-        ELSE
-           norms(blk,ui) = 0.0_sp
-        ENDIF
-     ENDDO
-     !$omp end do
+  !$omp shared(row,col,bps,data,&
+  !$omp        rbs,cbs,local_rows,local_cols, &
+  !$omp        norms,matrix) &
+  !$omp private(blk,bpe)
+  !$omp do
+  DO blk = 1, matrix%nblks
+     IF (bps(blk).NE.0) THEN
+        bpe = bps(blk) + rbs(local_rows(row(blk))) * cbs(local_cols(col(blk))) - 1
+        norms(blk) = SQRT (REAL (SUM(ABS(data(bps(blk):bpe))**2), KIND=sp))
+     ELSE
+        norms(blk) = 0.0_sp
+     ENDIF
   ENDDO
+  !$omp end do
   !$omp end parallel
 
-END SUBROUTINE calc_image_norms_c
+END SUBROUTINE calc_norms_c
 
