aa5154dac008d4aed635ee290e1175a7fc9f96a8
Patrick Seewald <patrick.seewald@chem.uzh.ch>
2017-03-22 14:40:10 +0000

    remove templates from formatting summary
    svn-origin-rev: 17822

--- tools/formatting/test_formatting.sh
+++ tools/formatting/test_formatting.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -e 
+#!/bin/bash -e
 
-# Test if all templates are instantiated and prettify was run
+# Test if prettify was run
 # author: Ole Schuett
 
 echo -n "Date: "
@@ -41,7 +41,7 @@ fi
 
 echo "Comparing MD5-sums ..."
 if ! md5sum --quiet --check checksums.md5 ; then
-  summary='Code not invariant under "make templates pretty; make pretty"'
+  summary='Code not invariant under "make pretty"'
   status="FAILED"
 fi
 
