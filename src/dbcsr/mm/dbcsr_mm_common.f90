2327412bb73731de9c99fcc845a1c40462439b5e
Alfio Lazzaro <alfio.lazzaro@chem.uzh.ch>
2017-05-19 11:23:32 +0000

    DBCSR: use local sizes in norms
    svn-origin-rev: 17913

--- src/dbcsr/mm/dbcsr_mm_common.f90
+++ src/dbcsr/mm/dbcsr_mm_common.f90
@@ -13,15 +13,13 @@
 !> \param local2global_cols ...
 ! **************************************************************************************************
   SUBROUTINE calc_norms_${nametype1}$(norms, nblks,&
-       blki, rbs, cbs, DATA, local2global_rows, local2global_cols)
+       blki, rbs, cbs, DATA)
     REAL(kind=sp), DIMENSION(:), INTENT(OUT) :: norms
     INTEGER, INTENT(IN)                      :: nblks
     INTEGER, DIMENSION(3,nblks), INTENT(IN)  :: blki
     INTEGER, DIMENSION(:), INTENT(IN)        :: rbs, cbs
     ${type1}$, DIMENSION(:), &
       INTENT(IN)                             :: DATA
-    INTEGER, DIMENSION(:), INTENT(IN)        :: local2global_rows
-    INTEGER, DIMENSION(:), INTENT(IN)        :: local2global_cols
 
     INTEGER                                  :: blk, bp, bpe, row, col
     REAL(kind=sp)                            :: val
@@ -32,13 +30,13 @@
     !$omp          private (row, col, blk, bp, bpe, val) &
     !$omp          shared (nblks) &
     !$omp          shared (rbs, cbs, blki, &
-    !$omp                  data, norms, local2global_rows, local2global_cols)
+    !$omp                  data, norms)
     !$omp do
     DO blk = 1, nblks
        IF (blki(3,blk) .NE. 0) THEN
+          row = blki(1,blk)
+          col = blki(2,blk)
           bp = blki(3,blk)
-          row = local2global_rows(blki(1,blk))
-          col = local2global_cols(blki(2,blk))
           bpe = bp + rbs(row) * cbs(col) - 1
           val = SQRT (REAL (SUM(ABS(DATA(bp:bpe))**2), KIND=sp))
        ELSE
