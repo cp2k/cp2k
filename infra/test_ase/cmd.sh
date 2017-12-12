e95d4f1ea2028e050267427bfe4b3a434ee5dcb4
Ole Schütt <ole.schuett@cp2k.org>
2017-12-12 19:08:53 +0000

    infra: fix ase test
    svn-origin-rev: 18165

--- infra/test_ase/cmd.sh
+++ infra/test_ase/cmd.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 
-date --utc
-
 echo "========== running ASE tests =========="
-export ASE_CP2K_COMMAND="mpiexec -np 2 /cp2k/exe/local/cp2k_shell.popt"
+export OMP_NUM_THREADS=1
+export ASE_CP2K_COMMAND="mpiexec -np 2 cp2k_shell.psmp"
 
 (
 for i in ./ase/test/cp2k/cp2k_*.py
@@ -20,12 +19,10 @@ echo ""
 ASE_REVISION=`git rev-parse --short HEAD`
 if (( $EXIT_CODE )); then
     echo "Summary: Something is wrong with ASE commit ${ASE_REVISION}."
-    echo "Status: FAILED" >> $REPORT
+    echo "Status: FAILED"
 else
     echo "Summary: ASE commit ${ASE_REVISION} works fine."
     echo "Status: OK"
 fi
-
-date --utc
 
 #EOF
