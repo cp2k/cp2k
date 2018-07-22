6ed4d28468183ffd6d5dcba78610242161b171db
Ole Schütt <ole.schuett@cp2k.org>
2018-07-22 17:22:18 +0000

    docker: Update toolchain before running tests.
    svn-origin-rev: 18542

--- tools/docker/regtest/cmd.sh
+++ tools/docker/regtest/cmd.sh
@@ -17,6 +17,20 @@ rsync --exclude="*~"          \
       --checksum              \
       /opt/cp2k-local/  /opt/cp2k-master/
 
+rsync --exclude="*~"                              \
+      --exclude=".*/"                             \
+      --exclude="*.pyc"                           \
+      --ignore-times                              \
+      --update                                    \
+      --verbose                                   \
+      --recursive                                 \
+      --checksum                                  \
+      /opt/cp2k-local/cp2k/tools/toolchain/  /opt/cp2k-toolchain/
+
+echo -e "\n========== Updating Toolchain =========="
+cd /opt/cp2k-toolchain/
+./install_cp2k_toolchain.sh --install-all --with-make=no
+
 echo -e "\n========== Running Regtests =========="
 source /opt/cp2k-toolchain/install/setup
 cd /opt/cp2k-master/cp2k/makefiles
