87ad222bad54de3f59a694880d43a1a941d9b726
Ole Schütt <ole.schuett@cp2k.org>
2018-07-30 18:52:04 +0000

    Fix aiida-cp2k test
    svn-origin-rev: 18562

--- tools/docker/test_aiida/cmd.sh
+++ tools/docker/test_aiida/cmd.sh
@@ -36,7 +36,14 @@ echo -e "\n========== Compiling CP2K =========="
 source /opt/cp2k-toolchain/install/setup
 cd /opt/cp2k-master/cp2k/makefiles
 make -j VERSION=pdbg cp2k
-ln -fs /opt/cp2k-master/cp2k/exe/local/cp2k.pdbg /usr/bin/cp2k
+
+echo -e "\n========== Installing CP2K =========="
+cat > /usr/bin/cp2k << EndOfMessage
+#!/bin/bash -e
+source /opt/cp2k-toolchain/install/setup
+/opt/cp2k-master/cp2k/exe/local/cp2k.pdbg "\$@"
+EndOfMessage
+chmod +x /usr/bin/cp2k
 
 echo -e "\n========== Installing AiiDA-CP2K plugin =========="
 cd /opt/
