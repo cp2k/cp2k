3186bd4469524f3ff7e5ddab4e5eaf4a37f2cef9
Ole Schütt <ole.schuett@cp2k.org>
2018-08-05 14:57:11 +0000

    cuda: fix parameters_K20X.txt
    svn-origin-rev: 18578

--- src/dbcsr/libsmm_acc/libcusmm/generate.py
+++ src/dbcsr/libsmm_acc/libcusmm/generate.py
@@ -34,8 +34,8 @@ def main(argv):
     #triples += combinations(12,25)
     #triples += combinations(9,12)
     #triples += combinations(5,13,16,32)
-    triples += combinations(6,7,8)
-    triples += combinations(13,14,25,26,32)
+    #triples += combinations(6,7,8)
+    #triples += combinations(13,14,25,26,32)
 
     usage = "Generator of LibCuSMM. The Library for Cuda Small Matrix Multiplications."
     parser = OptionParser(usage)
