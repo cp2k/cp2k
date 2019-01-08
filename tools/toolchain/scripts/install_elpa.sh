e387141db84a9c94d3f6d38b130a9d32fe3a2a09
Robert Schade <robert.schade@uni-paderborn.de>
2019-01-08 14:48:47 +0100

    toolchain: fix broken pexsi, quip and superlu build with OpenBLAS

--- tools/toolchain/scripts/install_elpa.sh
+++ tools/toolchain/scripts/install_elpa.sh
@@ -92,7 +92,7 @@ case "$with_elpa" in
                           CFLAGS="${CFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS}" \
                           CXXFLAGS="${CXXFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS}" \
                           LDFLAGS="-Wl,--enable-new-dtags ${MATH_LDFLAGS} ${SCALAPACK_LDFLAGS} ${cray_ldflags}" \
-                          LIBS="${SCALAPACK_LIBS} $(resolve_string "${MATH_LIBS}")" \
+                          LIBS="${SCALAPACK_LIBS} ${MATH_LIBS}" \
                           > configure.log 2>&1
             make -j $NPROCS >  make.log 2>&1
             make install > install.log 2>&1
@@ -116,7 +116,7 @@ case "$with_elpa" in
                               CFLAGS="${CFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS}" \
                               CXXFLAGS="${CXXFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS}" \
                               LDFLAGS="-Wl,--enable-new-dtags ${MATH_LDFLAGS} ${SCALAPACK_LDFLAGS} ${cray_ldflags}" \
-                              LIBS="${SCALAPACK_LIBS} $(resolve_string "${MATH_LIBS}" OMP)" \
+                              LIBS="${SCALAPACK_LIBS} ${MATH_LIBS}" \
                               > configure.log 2>&1
                 make -j $NPROCS >  make.log 2>&1
                 make install > install.log 2>&1
