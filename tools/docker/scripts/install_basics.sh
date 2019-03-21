5b4e1ae0379b619c18d34132e5c9a21e2bfe625f
Ole Schütt <ole.schuett@cp2k.org>
2019-03-21 12:10:34 +0100

    Docker: Move installation of Ubuntu packages into toolchain image

--- tools/docker/scripts/install_basics.sh
+++ tools/docker/scripts/install_basics.sh
@@ -2,21 +2,6 @@
 
 # author: Ole Schuett
 
-# install Ubuntu packages
-apt-get update
-apt-get install -y --no-install-recommends \
-    ca-certificates                        \
-    python                                 \
-    git                                    \
-    nano                                   \
-    wget                                   \
-    unzip                                  \
-    less                                   \
-    make                                   \
-    cmake                                  \
-    rsync
-rm -rf /var/lib/apt/lists/*
-
 # clone cp2k repository
 git clone --recursive --depth=1 --single-branch -b master https://github.com/cp2k/cp2k.git /workspace/cp2k
 
