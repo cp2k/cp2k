1159b825b844a6989c8e8900d3d13332e41d4d67
Ole Schütt <ole.schuett@cp2k.org>
2019-04-24 15:41:39 +0200

    Docker: Ignore errors in install_regtest.sh

--- tools/docker/scripts/install_regtest.sh
+++ tools/docker/scripts/install_regtest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # author: Ole Schuett
 
@@ -19,12 +19,25 @@ source /opt/cp2k-toolchain/install/setup
 
 # pre-build cp2k
 cd /workspace/cp2k
-make -j ARCH="${ARCH}" VERSION="${VERSION}"
+echo -n "Trying to compile... "
+if make -j ARCH="${ARCH}" VERSION="${VERSION}" &> /dev/null ; then
+    echo "done."
+else
+    echo "failed."
+fi
+
 
 # run regtests which lack fixed reference value
 # Disable LeakSanitizer during docker build as it requires ptrace capabilities.
 export LSAN_OPTIONS="detect_leaks=0"
-make test ARCH="${ARCH}" VERSION="${VERSION}" TESTOPTS="-restrictdir QS/regtest-almo-md -restrictdir QS/regtest-almo-1 -restrictdir SE/regtest-3-4 -restrictdir QS/regtest-ot-1-vib -restrictdir Fist/regtest-5-vib -restrictdir QS/regtest-optbas -restrictdir TMC/regtest_ana_post_proc"
+echo -n "Trying to run regtests which lack reference values... "
+if make test ARCH="${ARCH}" VERSION="${VERSION}" TESTOPTS="-restrictdir QS/regtest-almo-md -restrictdir QS/regtest-almo-1 -restrictdir SE/regtest-3-4 -restrictdir QS/regtest-ot-1-vib -restrictdir Fist/regtest-5-vib -restrictdir QS/regtest-optbas -restrictdir TMC/regtest_ana_post_proc" &> /dev/null ; then
+   echo "done."
+else
+   echo "failed."
+fi
+
+# remove binaries to reduce image size
 rm -rf lib exe "regtesting/${ARCH}/${VERSION}"/TEST-*
 
 #EOF
