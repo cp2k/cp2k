eb268f84c8d5c2009e51f81f44988268d5f86358
Hans Pabst <hans.pabst@intel.com>
2019-04-30 10:00:06 +0200

    Implement m_cpuid_static in C since there is no way to determine target flags in Fortran code.
    * Similar to other Fortran compilers, "gfortran -E -dM -mavx - < /dev/null | grep AVX" defines a variety of predefined macros (also similar to C).
    * However, with a Fortran translation unit only a subset of these definitions disappears ("gfortran -E -dM -mavx my.F | grep AVX").
    
    To demonstrate and use m_cpuid_static() and m_cpuid(), a new CPWARN is introduced: "the compiler target flags used to build this binary are insufficiently exploiting the extensions which are available for this CPU model". The warning is shown after presenting "CPU model name" (global environment; CP2K startup output).

---
+++ src/base/machine_cpuid.c
@@ -0,0 +1,37 @@
+/*------------------------------------------------------------------------------------------------*
+ * CP2K: A general program to perform molecular dynamics simulations                              *
+ * Copyright (C) 2019 - 2019  CP2K developers group                                               *
+ *------------------------------------------------------------------------------------------------*/
+
+/* shared between C and Fortran */
+#include "machine_cpuid.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/* This routine determines the CPUID according to the given compiler flags (expected to be similar to Fortran).
+ * Similar to other Fortran compilers, "gfortran -E -dM -mavx - < /dev/null | grep AVX" defines a variety of
+ * predefined macros (also similar to C). However, with a Fortran translation unit only a subset of these
+ * definitions disappears ("gfortran -E -dM -mavx my.F | grep AVX") hence an implementation in C is used.
+ */
+int m_cpuid_static(void); /* avoid pedantic warning about missing prototype */
+int m_cpuid_static(void)
+{
+#if (__AVX512F__ && __AVX512CD__ && __AVX2__ && __FMA__ && __AVX__ && __SSE4_2__ && __SSE4_1__ && __SSE3__)
+  return CP_MACHINE_X86_AVX512;
+#elif (__AVX2__ && __FMA__ && __AVX__ && __SSE4_2__ && __SSE4_1__ && __SSE3__)
+  return CP_MACHINE_X86_AVX2;
+#elif (__AVX__ && __SSE4_2__ && __SSE4_1__ && __SSE3__)
+  return CP_MACHINE_X86_AVX;
+#elif (__SSE4_2__ && __SSE4_1__ && __SSE3__)
+  return CP_MACHINE_X86_SSE4;
+#else
+  return CP_MACHINE_CPU_GENERIC;
+#endif  
+}
+
+#if defined(__cplusplus)
+}
+#endif
+
