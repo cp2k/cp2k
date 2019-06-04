2214abea8e74b66b87cf567792ed9d1068eb1f26
Ole Schütt <ole.schuett@cp2k.org>
2019-06-04 11:05:45 +0200

    Dashboard: Fix label matching of github issues

--- tools/dashboard/generate_dashboard.py
+++ tools/dashboard/generate_dashboard.py
@@ -154,7 +154,7 @@ def gen_frontpage(config, log, status_fn, outdir):
         matching_issues = []
         for issue in issues:
             link_pattern = "dashboard.cp2k.org/archive/{}/".format(s)
-            matching_labels = [label for label in issue['labels'] if s in label]
+            matching_labels = [label for label in issue['labels'] if s in label['name']]
             if link_pattern in issue['body'] or any(matching_labels):
                 issue_tmpl = '<a href="{}">#{}</a>'
                 matching_issues.append(issue_tmpl.format(issue['html_url'], issue['number']))
