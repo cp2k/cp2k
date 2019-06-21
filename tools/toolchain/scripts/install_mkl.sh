d7090bd2842357b8c2d0a7c1576464d7ff8231a3
Ondrej Marsalek <ondrej.marsalek@gmail.com>
2019-06-21 21:35:05 +0200

    Update also mkl_blacs_lib

--- tools/toolchain/scripts/install_mkl.sh
+++ tools/toolchain/scripts/install_mkl.sh
@@ -73,17 +73,16 @@ if [ "$with_mkl" != "__DONTUSE__" ] ; then
         mkl_optional_libs="libmkl_scalapack_lp64.a"
         case $MPI_MODE in
             mpich)
-                mkl_optional_libs="$mkl_optional_libs libmkl_blacs_intelmpi_lp64.a"
-                mkl_blacs_lib="libmkl_blacs_lp64.a"
+                mkl_blacs_lib="libmkl_blacs_intelmpi_lp64.a"
                 ;;
             openmpi)
-                mkl_optional_libs="$mkl_optional_libs libmkl_blacs_openmpi_lp64.a"
                 mkl_blacs_lib="libmkl_blacs_openmpi_lp64.a"
                 ;;
             *)
                 enable_mkl_scalapack="__FALSE__"
                 ;;
         esac
+        mkl_optional_libs="$mkl_optional_libs $mkl_blacs_lib"
         for ii in $mkl_optional_libs ; do
             if ! [ -f "${mkl_lib_dir}/${ii}" ] ; then
                 enable_mkl_scalapack="__FALSE__"
