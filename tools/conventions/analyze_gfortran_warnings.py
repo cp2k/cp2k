bb880275d271cbf3fb80f0de5ddeb249c1698bf5
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-09-03 14:53:46 +0200

    tools/conventions: make all scripts py3 compatible
    this includes:
    
    * use print as a function
    * properly use iterators and checks for keys in dicts
    * reformat everything according to PEP 8 using Black
    * use `argparse` instead of custom argument line parsing
    * add option to `analyse_src` to call it with a single file for
      `pre-commit` inclusion
    * fix issues mentioned by `flake8-bugbear`
    * use Python's string auto-concat or multiline strings inside of string
      summation

--- tools/conventions/analyze_gfortran_warnings.py
+++ tools/conventions/analyze_gfortran_warnings.py
@@ -3,115 +3,150 @@
 
 # author: Ole Schuett
 
+from __future__ import print_function
+
+import argparse
 import re
-import sys
+import ast
 from os import path
 
-blas_re = re.compile("[SDCZ]"
-                     +"(ROTG|ROTMG|ROT|ROTM|SWAP|SCAL|COPY|AXPY|DOT|DOTU|DOTC" #level 1
-                     +"|GEMV|GBMV|HEMV|HBMV|HPMV|SYMV|SBMV|SPMV|TRMV|TBMV|TPMV|TRSV" # level 2
-                     +"|TBSV|TPSV|GER|GERU|GERC|HER|HPR|HER2|HPR2|SYR|SPR|SYR2|SPR2"
-                     +"|GEMM|SYMM|HEMM|SYRK|HERK|SYR2K|HER2K|TRMM|TRSM"  # level 3
-                     +"|LANGE|LARNV|LAMCH|NRM2|CNRM2|ZNRM2|DSCAL)") # aux
+blas_re = re.compile(
+    r"[SDCZ]"
+    r"(ROTG|ROTMG|ROT|ROTM|SWAP|SCAL|COPY|AXPY|DOT|DOTU|DOTC"  # level 1
+    r"|GEMV|GBMV|HEMV|HBMV|HPMV|SYMV|SBMV|SPMV|TRMV|TBMV|TPMV|TRSV"  # level 2
+    r"|TBSV|TPSV|GER|GERU|GERC|HER|HPR|HER2|HPR2|SYR|SPR|SYR2|SPR2"
+    r"|GEMM|SYMM|HEMM|SYRK|HERK|SYR2K|HER2K|TRMM|TRSM"  # level 3
+    r"|LANGE|LARNV|LAMCH|NRM2|CNRM2|ZNRM2|DSCAL)"
+)  # aux
 
-lapack_re = re.compile("ILAENV|"
-                       +"([SDCZ]" + "(BD|DI|GB|GE|GG|GT|HB|HE|HG|HP|HS|OP"
-                       +"|OR|PB|PO|PP|PT|SB|SP|ST|SY|TB|TG|TP|TR|TZ|UN|UP)"
-                       +"(BAK|BAL|BRD|CON|EBZ|EDC|EIN|EQR|EGR|EQU|EQZ|ERF|EVC"
-                       +"|EXC|GBR|GHR|GLQ|GQL|GQR|GRQ|GST|GTR|HRD|LQF|MBR|MHR"
-                       +"|MLQ|MQL|MQR|MRQ|MRZ|MTR|QLF|QPF|QRF|RFS|RQF|RZF|SDC"
-                       +"|SEN|SJA|SNA|SQR|SVP|SYL|TRD|TRF|TRI|TRS"
-                       +"|SDD|EV|GV|SV|BS2D|BR2D|LS))")
+lapack_re = re.compile(
+    r"ILAENV|"
+    r"([SDCZ]" + "(BD|DI|GB|GE|GG|GT|HB|HE|HG|HP|HS|OP"
+    r"|OR|PB|PO|PP|PT|SB|SP|ST|SY|TB|TG|TP|TR|TZ|UN|UP)"
+    r"(BAK|BAL|BRD|CON|EBZ|EDC|EIN|EQR|EGR|EQU|EQZ|ERF|EVC"
+    r"|EXC|GBR|GHR|GLQ|GQL|GQR|GRQ|GST|GTR|HRD|LQF|MBR|MHR"
+    r"|MLQ|MQL|MQR|MRQ|MRZ|MTR|QLF|QPF|QRF|RFS|RQF|RZF|SDC"
+    r"|SEN|SJA|SNA|SQR|SVP|SYL|TRD|TRF|TRI|TRS"
+    r"|SDD|EV|GV|SV|BS2D|BR2D|LS))"
+)
 
-warning_re = re.compile(".*[Ww]arning: (.*)")
+warning_re = re.compile(r".*[Ww]arning: (.*)")
+
+IGNORED_WARNINGS = (
+    "-Wrealloc-lhs",
+    "-Wdo-subscript",
+    "-Wmaybe-uninitialized",
+    "Creating array temporary",
+    "quality comparison",
+    "defined but not used",
+    "Removing call to function",
+    "Conversion from",
+    "Non-significant digits in 'REAL(8)'",
+    "POINTER-valued function appears on right-hand side",
+    "style of line directive is a GCC extension",
+)
 
 
-#===============================================================================
-def main():
-    if(len(sys.argv) < 2):
-        print("Usage: analyse_gfortran_warnings.py <warn-file-1> ... <warn-file-N>")
-        print("       This tool checks the given stderr output from gfortran for violations of the coding conventions.")
-        print("       For generating the warn-files run gfortran with as all warning flags and redirect the output to a file.")
-        print('       This can be achieved by putting "FCLOGPIPE = 2>$(notdir $<).warn" in the cp2k arch-file.')
-        sys.exit(1)
-
-    files = sys.argv[1:]
-    for fn in files:
-        check_warnings(fn)
-
-#===============================================================================
-def check_warnings(fn):
-    content = open(fn).read()
-
-    lines = content.split("\n")
+def check_warnings(fhandle):
     loc = loc_short = ""
-    for i, line in enumerate(lines):
-        if(len(line)==0): continue
+
+    for line in fhandle:
+        line = line.rstrip("\n")
+
+        if not line:
+            continue
 
         # line directives issues by gcc directly
         # we ignore non-absolut paths, because they point to indermediate fypp output
-        if(line[0]=="/" and line[-1]==":"):
+        if line.startswith("/") and line.endswith(":"):
             loc = line.rsplit(":")[0].strip()
             loc_short = path.basename(loc)
             if not path.exists(loc):
-                return # source file gone - skipping
+                return  # source file gone - skipping
             continue
 
         # fypp line directives that leaked through as part of warning messages
-        if(line.startswith(' # 1 "')):
+        if line.startswith(' # 1 "'):
             loc = line.split()[2].strip('"')
             loc_short = path.basename(loc)
             if not path.exists(loc):
-                return # source file gone - skipping
+                return  # source file gone - skipping
             continue
 
-        if(loc.endswith("include/fftw3.f")): continue # an external file
+        if loc.endswith("include/fftw3.f"):
+            continue  # an external file
 
         m = warning_re.match(line)
-        if(not m): continue # we are only looking for warnings
+        if not m:
+            continue  # we are only looking for warnings
         warning = m.group(1)
 
-        # we ignore these warnings
-        if("-Wrealloc-lhs" in warning): continue
-        if("-Wdo-subscript" in warning): continue
-        if("-Wmaybe-uninitialized" in warning): continue
-        if("Creating array temporary" in warning): continue
-        if("quality comparison" in warning): continue
-        if("Unused" in warning):
-            if("'error'" in warning): continue
-            if("'routinep'" in warning): continue
-            if(loc_short == "cp_common_uses.f90"): continue
-        if("defined but not used" in warning): continue
-        if("Removing call to function" in warning): continue
-        if("Conversion from" in warning): continue
-        if("Non-significant digits in 'REAL(8)'" in warning): continue
-        if("CHARACTER expression" in warning and "truncated" in warning): continue
-        if("POINTER-valued function appears on right-hand side" in warning): continue
-        if("style of line directive is a GCC extension" in warning): continue
+        if any(iw in warning for iw in IGNORED_WARNINGS):
+            continue
+
+        if "Unused" in warning:
+            if "'error'" in warning:
+                continue
+            if "'routinep'" in warning:
+                continue
+            if loc_short == "cp_common_uses.f90":
+                continue
+
+        if ("CHARACTER expression" in warning) and ("truncated" in warning):
+            continue
 
         # ok this warning we should handle
-        if("called with an implicit interface" in warning):
+        if "called with an implicit interface" in warning:
             parts = warning.split()
-            assert(parts[0] == "Procedure")
+            assert parts[0] == "Procedure"
             routine = parts[1].strip("'").upper()
-            if(may_call_implicit(loc, routine)): continue
-            print("%s: Routine %s called with an implicit interface."%(loc_short, routine))
+            if may_call_implicit(loc, routine):
+                continue
+            print(
+                "%s: Routine %s called with an implicit interface."
+                % (loc_short, routine)
+            )
         else:
-            print("%s: %s"%(loc_short, warning)) # unknown warning, just output
+            print("%s: %s" % (loc_short, warning))  # unknown warning, just output
 
-#===============================================================================
+
 def may_call_implicit(loc, routine):
-    if(blas_re.match(routine)):
-        return(True)  # BLAS calls are allowed everywhere
-    if(lapack_re.match(routine)):
-        return(True) # Lapack calls are allowed everywhere
+    if blas_re.match(routine):
+        return True  # BLAS calls are allowed everywhere
+
+    if lapack_re.match(routine):
+        return True  # Lapack calls are allowed everywhere
 
     pkg = path.dirname(loc)
-    manifest_fn = pkg+"/PACKAGE"
-    manifest = eval(open(manifest_fn).read())
-    if(not manifest.has_key("implicit")): return(False)
+    manifest_fn = pkg + "/PACKAGE"
+    with open(manifest_fn) as fhandle:
+        manifest = ast.literal_eval(fhandle.read())
 
-    return(re.match(manifest["implicit"], routine))
+    if "implicit" not in manifest:
+        return False
 
-#===============================================================================
-main()
+    return re.match(manifest["implicit"], routine)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Checks given stderr output files from gfortran for violations of the coding conventions",
+        epilog="""\
+For generating the warn-files run gfortran with all warning flags and redirect the output to a file.
+This can be achieved by putting
+    FCLOGPIPE = 2>$(notdir $<).warn
+in the cp2k arch-file.
+""",
+    )
+    parser.add_argument(
+        "files",
+        metavar="<warn-file>",
+        type=str,
+        nargs="+",
+        help="files containing the compiler warnings",
+    )
+    args = parser.parse_args()
+
+    for fn in args.files:
+        with open(fn) as fhandle:
+            check_warnings(fhandle)
