2913f4fde9a28e2f390672f419fb1ca50e1521e4
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-09-25 17:57:29 +0200

    doxify: cleanup scripts

--- tools/doxify/remove_double_ampersands.pl
+++ tools/doxify/remove_double_ampersands.pl
@@ -1,68 +1,81 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
-# Want to locate any lines where we have an ampersand at the end of a line followed
-# by an ampersand at the start of the next line as these causes the fixcomments.pl 
-# script to break as it uses the ampersand to decide whether there is more data to 
-# read. 
-# An example would be (from cp_lbfgs.F): 
-#      SUBROUTINE setulb(n, m, x, l, u, nbd, f, g, factr, pgtol, wa, iwa,&
-#     &                 task, iprint, csave, lsave, isave, dsave)
+=head1 NAME
 
-$DEBUG=$ARGV[2];
-if ($DEBUG eq 1) {
-    print "DEBUG = YES \n";
-    print "remove_double_ampersands.pl running with INPUT & OUTPUT = ";
-    print $ARGV[0];
-    print " ";
-    print $ARGV[1];
-    print "\n";
+remove_double_ampersands.pl
+
+=head1 SYNOPSIS
+
+remove_double_ampersands.pl [options] [infile]
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<infile>
+
+Input file to process (uses stdin if not specified)
+
+=item B<--help|-h>
+
+A little help
+
+=item B<--verbose|-v>
+
+Output debug messages (to stderr), repeat for even more output
+
+=back
+
+=head1 DESCRIPTION
+
+Want to locate any lines where we have an ampersand at the end of a line followed
+by an ampersand at the start of the next line as these causes the fixcomments.pl
+script to break as it uses the ampersand to decide whether there is more data to
+read.
+An example would be (from cp_lbfgs.F):
+     SUBROUTINE setulb(n, m, x, l, u, nbd, f, g, factr, pgtol, wa, iwa,&
+    &                 task, iprint, csave, lsave, isave, dsave)
+
+=cut
+
+use strict;
+use warnings;
+use Pod::Usage qw(pod2usage);
+use Getopt::Long;
+
+my $verbose = 0;
+my $help = 0;
+
+GetOptions(
+    'verbose+' => \$verbose,
+    'help|?' => \$help) or pod2usage(2);
+pod2usage(1) if $help;
+
+sub print_debug {
+    print(STDERR "DEBUG: @_\n") if ($verbose > 0);
 }
-open ($INPUT , "<" , $ARGV[0]) or die "Cant open $ARGV[0] $!";
-open ($OUTPUT , ">" , $ARGV[1]) or die "Cant create $ARGV[1] $!";
 
-$count = 0;
-$linesread = 0;
-$countampend = 0;
-$countampstart = 0;
-$firsttime = 1;
-while (<$INPUT>) # While there are still lines to read
-{
 
-# First time we enter the loop we need to read in two lines
-    if ($firsttime eq 1) {
-	$currline = $_; # currline = the line we've just read in  
-	$linesread = $linesread + 1;
-	$nextline = <$INPUT>; # read in the next line too 
-	$linesread = $linesread + 1;
-	$firsttime = 0;
-    } else {
-# If not first iteration need to copy nextline into currline and read currline from $_
-	$currline = $nextline;
-	$nextline = $_;
-	$linesread = $linesread + 1;
-    }
+my $count = 0;
+my $countampend = 0;
+my $countampstart = 0;
+my $currline = <>; # First time we enter the loop we need to read in two lines, read the first now
+my $linesread = 1;
+
+while (my $nextline=<>) { # While there are still lines to read, either from STDIN or the files specified
+    $linesread += 1;
 
     if ( ($currline =~ m/&\s*$/i) && ($nextline =~ m/^\s*&\s*\w+/i) ) {
-	$nextline =~ s/&/ /;
-	if ($DEBUG eq 1) { # Only print when debugging the script
-	    print "remove_double_ampersands.pl:: CURRLINE is ",$currline;
-	    print "remove_double_ampersands.pl:: NEXTLINE is ",$nextline;
-	}
-	    $count = $count + 1;
+        $nextline =~ s/&/ /;
+        print_debug("remove_double_ampersands.pl:: CURRLINE is ", $currline);
+        print_debug("remove_double_ampersands.pl:: NEXTLINE is ", $nextline);
+        $count += 1;
     }
-    print $OUTPUT $currline; # Write all lines except the last one to file - initial & replaced with space
 
+    print $currline; # Write all lines except the last one - initial & replaced with space
+    $currline = $nextline
 }
 
-print $OUTPUT $nextline;  # Write the last line out to file
+print $currline;  # Write the last line (unaltered)
 
-
-if ( $DEBUG eq 1 ) { # Only print when debugging the script 
-    print "remove_double_ampersand.pl:: ", $linesread, " lines read in, found a total of ", $count, " lines with the double ampersand issue in file = ", $ARGV[0], " \n";
-}
-
-
-# Close the files
-close $OUTPUT;
-close $INPUT;
-
+print_debug("remove_double_ampersand.pl:: ", $linesread, " lines read in, found a total of ", $count, " lines with the double ampersand issue in file", " \n");
