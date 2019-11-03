abbbcaa7c0da9f4d63cb22c84a2868e28c3806a0
Ole Schütt <ole.schuett@cp2k.org>
2019-11-03 17:44:22 +0100

    Docker: Update AiiDA test to Python3

--- tools/docker/scripts/install_basics.sh
+++ tools/docker/scripts/install_basics.sh
@@ -6,6 +6,7 @@
 apt-get update -qq
 apt-get install -qq --no-install-recommends \
     ca-certificates                         \
+    locales                                 \
     git                                     \
     less                                    \
     nano                                    \
@@ -13,6 +14,8 @@ apt-get install -qq --no-install-recommends \
     wget
 
 rm -rf /var/lib/apt/lists/*
+
+locale-gen "en_US.UTF-8"
 
 # clone cp2k repository
 git clone --quiet --recursive --single-branch -b master https://github.com/cp2k/cp2k.git /workspace/cp2k
