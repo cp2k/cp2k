f937b4d21ed68f1e1c4a1f2a1e4e11e31f46dc48
Ole Schütt <ole.schuett@cp2k.org>
2019-11-15 17:38:00 +0100

    Toolchain: Add install_requirements.sh

---
+++ tools/toolchain/install_requirements.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# author: Ole Schuett
+
+# Install Ubuntu packages required for the toolchain.
+
+apt-get update -qq
+
+apt-get install -qq --no-install-recommends \
+    autoconf                                \
+    autogen                                 \
+    automake                                \
+    autotools-dev                           \
+    ca-certificates                         \
+    g++                                     \
+    git                                     \
+    less                                    \
+    libtool                                 \
+    make                                    \
+    nano                                    \
+    pkg-config                              \
+    python                                  \
+    python3                                 \
+    unzip                                   \
+    wget                                    \
+    xxd                                     \
+    zlib1g-dev
+
+rm -rf /var/lib/apt/lists/*
+
+#EOF
