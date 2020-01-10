cb3df64bbfb67b02f3ed6d2bb60a674856e5d1a6
Ole Schütt <ole.schuett@cp2k.org>
2020-01-11 00:26:21 +0100

    Docker: Upgrade lcov to make it GCC9 compatible

--- tools/docker/scripts/install_coverage.sh
+++ tools/docker/scripts/install_coverage.sh
@@ -2,8 +2,13 @@
 
 # author: Ole Schuett
 
-lcov_version="1.14"
-lcov_sha256="14995699187440e0ae4da57fe3a64adc0a3c5cf14feab971f8db38fb7d8f071a"
+# GCC9 changed the output format of gcov to json.
+# While lcov has been updated there isn't a new release yet.
+# Hence, we're using https://github.com/linux-test-project/lcov/commit/75fbae1cf
+# For details see https://github.com/linux-test-project/lcov/issues/58
+
+lcov_version="75fbae1cfc5027f818a0bb865bf6f96fab3202da"
+lcov_sha256="f536c31d517d54b36b8a15fceb9e697c05f920bb31ac6fb1a43d19eafa077fa6"
 
 cd /tmp
 wget -q https://www.cp2k.org/static/downloads/lcov-${lcov_version}.tar.gz
@@ -13,5 +18,10 @@ cd lcov-${lcov_version}
 make install > make.log 2>&1
 cd ..
 rm -rf lcov-${lcov_version}.tar.gz lcov-${lcov_version}
+
+# LCOV dependencies
+apt-get update -qq
+apt-get install -qq --no-install-recommends libperlio-gzip-perl libjson-perl
+rm -rf /var/lib/apt/lists/*
 
 #EOF
