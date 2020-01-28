75e8e09cb753734f7c6236000d729c8b56265712
Matthias Krack <matthias.krack@psi.ch>
2020-01-28 11:42:23 +0100

    Add -mtune flag

--- tools/toolchain/scripts/setup_buildtools.sh
+++ tools/toolchain/scripts/setup_buildtools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
-SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")" && pwd -P)"
+SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_NAME}")" && pwd -P)"
 
 source "${SCRIPT_DIR}"/common_vars.sh
 source "${SCRIPT_DIR}"/tool_kit.sh
@@ -19,20 +19,20 @@ done
 
 # setup compiler flags, leading to nice stack traces on crashes but
 # still optimised
-CFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
-FFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
-F77FLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
-F90FLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
-FCFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
-CXXFLAGS="-O2 -ftree-vectorize -g -fno-omit-frame-pointer -march=native $TSANFLAGS"
+CFLAGS="-O2 -fno-omit-frame-pointer -ftree-vectorize -g -march=native -mtune=native ${TSANFLAGS}"
+CXXFLAGS="${CFLAGS}"
+FFLAGS="-O2 -fbacktrace -fno-omit-frame-pointer -ftree-vectorize -g -march=native -mtune=native ${TSANFLAGS}"
+F77FLAGS="${FFLAGS}"
+F90FLAGS="${FFLAGS}"
+FCFLAGS="${FFLAGS}"
 
-export CFLAGS=$(allowed_gcc_flags $CFLAGS)
-export FFLAGS=$(allowed_gfortran_flags $FFLAGS)
-export F77FLAGS=$(allowed_gfortran_flags $F77FLAGS)
-export F90FLAGS=$(allowed_gfortran_flags $F90FLAGS)
-export FCFLAGS=$(allowed_gfortran_flags $FCFLAGS)
-export CXXFLAGS=$(allowed_gxx_flags $CXXFLAGS)
-export LDFLAGS="$TSANFLAGS"
+export CFLAGS=$(allowed_gcc_flags ${CFLAGS})
+export FFLAGS=$(allowed_gfortran_flags ${FFLAGS})
+export F77FLAGS=$(allowed_gfortran_flags ${F77FLAGS})
+export F90FLAGS=$(allowed_gfortran_flags ${F90FLAGS})
+export FCFLAGS=$(allowed_gfortran_flags ${FCFLAGS})
+export CXXFLAGS=$(allowed_gxx_flags ${CXXFLAGS})
+export LDFLAGS="${TSANFLAGS}"
 
 # get system arch information using OpenBLAS prebuild
 "${SCRIPTDIR}"/get_openblas_arch.sh; load "${BUILDDIR}/openblas_arch"
