1e0a2e59745062bd027c53c8d858552018be30b2
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-04-15 15:22:35 +0200

    Simplify aiida test.
    This PR removes unnecessary setup that are taken care by the
    pytest. In particular, those things are removed:
    1) Postgres database setup
    2) AiiDa profile setup
    3) Computer and code setup

--- tools/docker/scripts/test_aiida.sh
+++ tools/docker/scripts/test_aiida.sh
@@ -33,43 +33,6 @@ service rabbitmq-server start
 
 # start and configure PostgreSQL
 service postgresql start
-sudo -u postgres psql -d template1 -c "CREATE USER aiida WITH PASSWORD 'aiida_db_passwd';"
-sudo -u postgres psql -d template1 -c "CREATE DATABASE aiidadb OWNER aiida;"
-sudo -u postgres psql -d template1 -c "GRANT ALL PRIVILEGES ON DATABASE aiidadb to aiida;"
-
-# setup aiida user
-$AS_UBUNTU_USER verdi setup                       \
-      --non-interactive                           \
-      --email aiida@localhost                     \
-      --first-name Some                           \
-      --last-name Body                            \
-      --institution XYZ                           \
-      --db-backend django                         \
-      --db-username aiida                         \
-      --db-password aiida_db_passwd               \
-      --db-name aiidadb                           \
-      --db-host localhost                         \
-      --db-port 5432                              \
-      --repository /home/ubuntu/aiida_repository  \
-      --profile default
-
-# start aiida daemon
-$AS_UBUNTU_USER verdi profile setdefault default
-$AS_UBUNTU_USER verdi daemon start
-
-# setup local computer
-$AS_UBUNTU_USER mkdir -p /home/ubuntu/aiida_run
-
-$AS_UBUNTU_USER verdi computer setup    \
-      --non-interactive                 \
-      --label localhost                 \
-      --hostname localhost              \
-      --transport local                 \
-      --scheduler direct                \
-      --work-dir /home/ubuntu/aiida_run
-
-$AS_UBUNTU_USER verdi computer configure local localhost --non-interactive --safe-interval 0.0
-$AS_UBUNTU_USER verdi computer test localhost
 
 # setup code
 cat > /usr/bin/cp2k << EndOfMessage
@@ -78,13 +41,6 @@ source /opt/cp2k-toolchain/install/setup
 /workspace/cp2k/exe/local/cp2k.pdbg "\$@"
 EndOfMessage
 chmod +x /usr/bin/cp2k
-
-$AS_UBUNTU_USER verdi code setup        \
-      --non-interactive                 \
-      --label cp2k                      \
-      --computer localhost              \
-      --remote-abs-path /usr/bin/cp2k   \
-      --input-plugin cp2k
 
 echo -e "\n========== Running AiiDA-CP2K Tests =========="
 cd  /opt/aiida-cp2k/
