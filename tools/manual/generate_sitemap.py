ce965591122f72e460389f9600aae351c6c318d7
Ole Schütt <ole.schuett@cp2k.org>
2020-05-27 23:02:08 +0200

    Manual: Fix generate_sitemap.py

--- tools/manual/generate_sitemap.py
+++ tools/manual/generate_sitemap.py
@@ -18,7 +18,6 @@ def main():
     entries = list()
     for dirname, subdirs, files in os.walk(rootdir, followlinks=True):
         reldir = dirname[len(rootdir) :]
-        changefreq = "never" if ("branch" in reldir.split("/")[0]) else "daily"
         # print('Found directory: %s' % reldir)
         for fn in files:
             if not fn.lower().endswith(".html"):
@@ -31,14 +30,13 @@ def main():
             entry = "<url>"
             entry += "<loc>https://manual.cp2k.org/%s</loc>" % relfn
             entry += "<lastmod>%s</lastmod>" % lastmod
-            entry += "<changefreq>%s</changefreq>" % changefreq
             entry += "</url>"
             entries.append(entry)
 
     print("Found %d html-files." % len(entries))
 
     fn_out = "sitemap.xml.gz"
-    f = gzip.open("sitemap.xml.gz", "wb")
+    f = gzip.open("sitemap.xml.gz", "wt", encoding="utf8")
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
     f.write("\n".join(entries))
