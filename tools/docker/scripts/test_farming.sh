e88cda828f1362b9f8488e1a7263639fe5201d98
Ole Schütt <ole.schuett@cp2k.org>
2020-06-12 15:08:21 +0200

    Docker: Switch test_farming to use pdbg

--- tools/docker/scripts/test_farming.sh
+++ tools/docker/scripts/test_farming.sh
@@ -7,6 +7,6 @@ source /opt/cp2k-toolchain/install/setup
 
 echo -e "\n========== Running Regtests =========="
 cd /workspace/cp2k
-make VERSION=popt test TESTOPTS="-farming -skipunittest -skipdir TMC/regtest_ana_on_the_fly -skipdir TMC/regtest_ana_post_proc -skipdir TMC/regtest ${TESTOPTS}"
+make VERSION=pdbg test TESTOPTS="-farming -skipunittest -skipdir TMC/regtest_ana_on_the_fly -skipdir TMC/regtest_ana_post_proc -skipdir TMC/regtest ${TESTOPTS}"
 
 #EOF
