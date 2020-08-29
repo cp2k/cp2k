a9e0f95442f2fce685219d03be42fb8c6ff570e0
Ole Schütt <ole.schuett@cp2k.org>
2020-08-30 00:25:13 +0200

    precommit: Improve entrypoint.sh and quiet pip

--- tools/precommit/entrypoint.sh
+++ tools/precommit/entrypoint.sh
@@ -10,19 +10,22 @@ if [ -z "${GIT_REF}" ]; then
     wait  # Can be interrupted with ctrl-C.
 else
     # CI mode.
-    echo -e "\n========== Starting Precommit Server =========="
-    gunicorn --bind=:8080 --workers=1 --threads=8 --timeout=0 precommit_server:app &> server.logs &
-    sleep 3
-    cat server.logs
     echo -e "\n========== Fetching Git Commit =========="
     cd /workspace/cp2k
     git fetch --quiet origin "${GIT_BRANCH}"
     git reset --quiet --hard "${GIT_REF}"
     git submodule update --init --recursive
     git --no-pager log -1 --pretty='%nCommitSHA: %H%nCommitTime: %ci%nCommitAuthor: %an%nCommitSubject: %s%n'
+
+    echo -e "\n========== Starting Precommit Server =========="
+    cd ./tools/precommit/
+    gunicorn --bind=:8080 --workers=1 --threads=8 --timeout=0 precommit_server:app &> server.logs &
+    sleep 3
+    cat server.logs
+
     echo -e "\n========== Running Precommit Checks =========="
     export CP2K_PRECOMMIT_SERVER="http://127.0.0.1:8080"
-    ./tools/precommit/precommit.py --no-cache --progressbar-wait=10 || true
+    ./precommit.py --no-cache --progressbar-wait=10 || true
 fi
 
 #EOF
