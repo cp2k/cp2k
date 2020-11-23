22941e7a4c634d2a843d333b22cb40aa665ddda7
Ole Schütt <ole.schuett@cp2k.org>
2020-11-23 12:14:25 +0100

    Docker: Pin DBCSR version for pdbg test

--- tools/docker/scripts/test_regtest.sh
+++ tools/docker/scripts/test_regtest.sh
@@ -19,6 +19,17 @@ if command -v ompi_info &> /dev/null ; then
     export OMPI_MCA_plm_rsh_agent=/bin/false
 fi
 
+# Switch to stable DBCSR version if requested.
+if [ -n "${USE_STABLE_DBCSR}" ] ; then
+    echo "Switching to stable DBCSR version..."
+    if ! git -C cp2k/exts/dbcsr checkout -b v2.1.0-rc16 6c52382 ; then
+        echo -e "\nSummary: Could not checkout stable DBCSR version."
+        echo -e "Status: FAILED\n"
+        exit 0
+    fi
+fi
+
+# Compile cp2k.
 echo -en "\nCompiling cp2k... "
 cd /workspace/cp2k || exit 1
 if make -j ARCH="${ARCH}" VERSION="${VERSION}" &> make.out ; then
@@ -31,7 +42,7 @@ else
     exit 0
 fi
 
-
+# Check benchmark files for input errors.
 if [[ "${ARCH}" == "local" ]] ; then
     echo -en "\nChecking benchmarks... "
     if ! ./tools/regtesting/check_inputs.py "./exe/${ARCH}/cp2k.${VERSION}" "./benchmarks/" ; then
@@ -41,7 +52,7 @@ if [[ "${ARCH}" == "local" ]] ; then
     fi
 fi
 
-
+# Run regtests.
 echo -e "\n========== Running Regtests =========="
 make ARCH="${ARCH}" VERSION="${VERSION}" TESTOPTS="${TESTOPTS}" test
 
