f213cf715560f5470e4f348ef8ead4ee96f52919
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-12-15 15:21:18 +0100

    docker: update lcov to 1.15
    fixes #1217

--- tools/docker/scripts/install_coverage.sh
+++ tools/docker/scripts/install_coverage.sh
@@ -2,26 +2,21 @@
 
 # author: Ole Schuett
 
-# GCC9 changed the output format of gcov to json.
-# While lcov has been updated there isn't a new release yet.
-# Hence, we're using https://github.com/linux-test-project/lcov/commit/75fbae1cf
-# For details see https://github.com/linux-test-project/lcov/issues/58
-
-lcov_version="75fbae1cfc5027f818a0bb865bf6f96fab3202da"
-lcov_sha256="f536c31d517d54b36b8a15fceb9e697c05f920bb31ac6fb1a43d19eafa077fa6"
-
-cd /tmp
-wget -q https://www.cp2k.org/static/downloads/lcov-${lcov_version}.tar.gz
-echo "${lcov_sha256} lcov-${lcov_version}.tar.gz" | sha256sum --check
-tar -xzf lcov-${lcov_version}.tar.gz
-cd lcov-${lcov_version}
-make install > make.log 2>&1
-cd ..
-rm -rf lcov-${lcov_version}.tar.gz lcov-${lcov_version}
+lcov_version="1.15"
+lcov_sha256="c1cda2fa33bec9aa2c2c73c87226cfe97de0831887176b45ee523c5e30f8053a"
 
 # LCOV dependencies
 apt-get update -qq
-apt-get install -qq --no-install-recommends libperlio-gzip-perl libjson-perl
+apt-get install -qq --no-install-recommends \
+    libperlio-gzip-perl \
+    libjson-perl
 rm -rf /var/lib/apt/lists/*
 
-#EOF
+cd /tmp
+wget -q "https://www.cp2k.org/static/downloads/lcov-${lcov_version}.tar.gz"
+echo "${lcov_sha256} lcov-${lcov_version}.tar.gz" | sha256sum --check
+tar -xzf "lcov-${lcov_version}.tar.gz"
+cd "lcov-${lcov_version}"
+make install > make.log 2>&1
+cd ..
+rm -rf "lcov-${lcov_version}.tar.gz" "lcov-${lcov_version}"
