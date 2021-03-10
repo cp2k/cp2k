50911849e3d8a948a9404aaf3f29e23ab6eaa86d
Ole Schütt <ole.schuett@cp2k.org>
2021-03-10 11:14:52 +0100

    Toolchain: Ignore shellcheck errors for now

--- tools/toolchain/scripts/stage0/install_stage0.sh
+++ tools/toolchain/scripts/stage0/install_stage0.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# TODO: Review and if possible fix shellcheck errors.
+# shellcheck disable=SC1003,SC1035,SC1083,SC1090
+# shellcheck disable=SC2001,SC2002,SC2005,SC2016,SC2091,SC2034,SC2046,SC2086,SC2089,SC2090
+# shellcheck disable=SC2124,SC2129,SC2144,SC2153,SC2154,SC2155,SC2163,SC2164,SC2166
+# shellcheck disable=SC2235,SC2237
+
 ./scripts/stage0/install_gcc.sh
 ./scripts/stage0/setup_buildtools.sh
 ./scripts/stage0/install_cmake.sh
