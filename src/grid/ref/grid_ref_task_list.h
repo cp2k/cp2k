aec20905932837ea718cb7b6b66629d8ebc9edcb
Ole Schütt <ole.schuett@cp2k.org>
2021-04-05 23:04:12 +0200

    grid: Store grid layouts in REF task list

--- src/grid/ref/grid_ref_task_list.h
+++ src/grid/ref/grid_ref_task_list.h
@@ -32,10 +32,24 @@ typedef struct {
 } grid_ref_task;
 
 /*******************************************************************************
+ * \brief Internal representation of a grid layout.
+ * \author Ole Schuett
+ ******************************************************************************/
+typedef struct {
+  int npts_global[3];
+  int npts_local[3];
+  int shift_local[3];
+  int border_width[3];
+  double dh[3][3];
+  double dh_inv[3][3];
+} grid_ref_layout;
+
+/*******************************************************************************
  * \brief Internal representation of a task list.
  * \author Ole Schuett
  ******************************************************************************/
 typedef struct {
+  bool orthorhombic;
   int ntasks;
   int nlevels;
   int natoms;
@@ -46,6 +60,7 @@ typedef struct {
   int *atom_kinds;
   grid_basis_set **basis_sets;
   grid_ref_task *tasks;
+  grid_ref_layout *layouts;
   int *first_level_block_task;
   int *last_level_block_task;
   int maxco;
@@ -59,16 +74,19 @@ typedef struct {
  * \author Ole Schuett
  ******************************************************************************/
 void grid_ref_create_task_list(
-    const int ntasks, const int nlevels, const int natoms, const int nkinds,
-    const int nblocks, const int block_offsets[nblocks],
-    const double atom_positions[natoms][3], const int atom_kinds[natoms],
-    const grid_basis_set *basis_sets[nkinds], const int level_list[ntasks],
-    const int iatom_list[ntasks], const int jatom_list[ntasks],
-    const int iset_list[ntasks], const int jset_list[ntasks],
-    const int ipgf_list[ntasks], const int jpgf_list[ntasks],
-    const int border_mask_list[ntasks], const int block_num_list[ntasks],
-    const double radius_list[ntasks], const double rab_list[ntasks][3],
-    grid_ref_task_list **task_list);
+    const bool orthorhombic, const int ntasks, const int nlevels,
+    const int natoms, const int nkinds, const int nblocks,
+    const int block_offsets[nblocks], const double atom_positions[natoms][3],
+    const int atom_kinds[natoms], const grid_basis_set *basis_sets[nkinds],
+    const int level_list[ntasks], const int iatom_list[ntasks],
+    const int jatom_list[ntasks], const int iset_list[ntasks],
+    const int jset_list[ntasks], const int ipgf_list[ntasks],
+    const int jpgf_list[ntasks], const int border_mask_list[ntasks],
+    const int block_num_list[ntasks], const double radius_list[ntasks],
+    const double rab_list[ntasks][3], const int npts_global[nlevels][3],
+    const int npts_local[nlevels][3], const int shift_local[nlevels][3],
+    const int border_width[nlevels][3], const double dh[nlevels][3][3],
+    const double dh_inv[nlevels][3][3], grid_ref_task_list **task_list);
 
 /*******************************************************************************
  * \brief Deallocates given task list, basis_sets have to be freed separately.
@@ -81,13 +99,10 @@ void grid_ref_free_task_list(grid_ref_task_list *task_list);
  *        See grid_task_list.h for details.
  * \author Ole Schuett
  ******************************************************************************/
-void grid_ref_collocate_task_list(
-    const grid_ref_task_list *task_list, const bool orthorhombic,
-    const enum grid_func func, const int nlevels,
-    const int npts_global[nlevels][3], const int npts_local[nlevels][3],
-    const int shift_local[nlevels][3], const int border_width[nlevels][3],
-    const double dh[nlevels][3][3], const double dh_inv[nlevels][3][3],
-    const grid_buffer *pab_blocks, double *grid[nlevels]);
+void grid_ref_collocate_task_list(const grid_ref_task_list *task_list,
+                                  const enum grid_func func, const int nlevels,
+                                  const grid_buffer *pab_blocks,
+                                  double *grid[nlevels]);
 
 /*******************************************************************************
  * \brief Integrate all tasks of in given list from given grids.
@@ -95,13 +110,10 @@ void grid_ref_collocate_task_list(
  * \author Ole Schuett
  ******************************************************************************/
 void grid_ref_integrate_task_list(
-    const grid_ref_task_list *task_list, const bool orthorhombic,
-    const bool compute_tau, const int natoms, const int nlevels,
-    const int npts_global[nlevels][3], const int npts_local[nlevels][3],
-    const int shift_local[nlevels][3], const int border_width[nlevels][3],
-    const double dh[nlevels][3][3], const double dh_inv[nlevels][3][3],
-    const grid_buffer *pab_blocks, const double *grid[nlevels],
-    grid_buffer *hab_blocks, double forces[natoms][3], double virial[3][3]);
+    const grid_ref_task_list *task_list, const bool compute_tau,
+    const int natoms, const int nlevels, const grid_buffer *pab_blocks,
+    const double *grid[nlevels], grid_buffer *hab_blocks,
+    double forces[natoms][3], double virial[3][3]);
 
 #endif
 
