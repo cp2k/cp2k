50ea0842239de20b1226ec654ae4c92e3c9de121
Ole Schütt <ole.schuett@cp2k.org>
2021-05-30 23:15:47 +0200

    Toolchain: Add -g to nvcc flags

--- tools/toolchain/scripts/generate_arch_files.sh
+++ tools/toolchain/scripts/generate_arch_files.sh
@@ -110,7 +110,7 @@ CUDA_DFLAGS="-D__GRID_CUDA -D__DBCSR_ACC -D__PW_CUDA IF_DEBUG(-D__CUDA_PROFILING
 if [ "${ENABLE_CUDA}" = __TRUE__ ] && [ "${GPUVER}" != no ]; then
   LIBS="${LIBS} IF_CUDA(${CUDA_LIBS}|)"
   DFLAGS="IF_CUDA(${CUDA_DFLAGS}|) ${DFLAGS}"
-  NVFLAGS="-arch sm_${ARCH_NUM} -O3 -Xcompiler='-fopenmp' --std=c++11 \$(DFLAGS)"
+  NVFLAGS="-g -arch sm_${ARCH_NUM} -O3 -Xcompiler='-fopenmp' --std=c++11 \$(DFLAGS)"
   check_command nvcc "cuda"
   check_lib -lcudart "cuda"
   check_lib -lnvrtc "cuda"
