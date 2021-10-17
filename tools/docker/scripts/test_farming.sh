4e2d5bf436c7b9cf1467a5118cdf99093a3baea0
Ole Schütt <ole.schuett@cp2k.org>
2021-10-17 17:27:07 +0200

    Docker: Exclude SIRIUS from farming test

--- tools/docker/scripts/test_farming.sh
+++ tools/docker/scripts/test_farming.sh
@@ -7,6 +7,6 @@ source /opt/cp2k-toolchain/install/setup
 
 echo -e "\n========== Running Regtests =========="
 cd /workspace/cp2k
-make VERSION=psmp test TESTOPTS="-farming -ompthreads 1 -skipunittest -skipdir TMC/regtest_ana_on_the_fly -skipdir TMC/regtest_ana_post_proc -skipdir TMC/regtest -skipdir LIBTEST/libvori -skipdir LIBTEST/libbqb ${TESTOPTS}"
+make VERSION=psmp test TESTOPTS="-farming -ompthreads 1 -skipunittest -skipdir TMC/regtest_ana_on_the_fly -skipdir TMC/regtest_ana_post_proc -skipdir TMC/regtest -skipdir LIBTEST/libvori -skipdir LIBTEST/libbqb -skipdir SIRIUS/regtest-1 ${TESTOPTS}"
 
 #EOF
