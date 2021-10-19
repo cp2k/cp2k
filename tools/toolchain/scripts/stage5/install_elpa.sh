aa83d9594892e46c2194ff2a0c811ed3c473fb07
Fabian Ducry <f2@ducry.ch>
2021-10-19 17:45:41 +0200

    gpu target for elpa and cray math-mode for cosma

--- tools/toolchain/scripts/stage5/install_elpa.sh
+++ tools/toolchain/scripts/stage5/install_elpa.sh
@@ -115,6 +115,7 @@ case "$with_elpa" in
           ${config_flags} \
           --enable-nvidia-gpu=$([ "$TARGET" == "nvidia" ] && echo "yes" || echo "no") \
           --with-cuda-path=${CUDA_PATH} \
+          --with-NVIDIA-GPU-compute-capability=$([ "$TARGET" == "nvidia" ] && echo "sm_$ARCH_NUM" || echo "sm_35") \
           OMPI_MCA_plm_rsh_agent=/bin/false \
           FC=${MPIFC} \
           CC=${MPICC} \
