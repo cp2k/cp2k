449e3c04f448d82106dbbec906e9e9914f988a31
Hans Pabst <hans.pabst@intel.com>
2021-11-10 13:37:39 +0100

    Prettify.

--- tools/toolchain/install_cp2k_toolchain.sh
+++ tools/toolchain/install_cp2k_toolchain.sh
@@ -716,15 +716,14 @@ else
 fi
 
 # several packages require cmake.
-if [ "$with_spglib" = "__INSTALL__" ] \
-|| [ "$with_libvori" = "__INSTALL__" ] \
-|| [ "$with_scalapack" = "__INSTALL__" ] \
-|| [ "$with_superlu" = "__INSTALL__" ] \
-|| [ "$with_sirius" = "__INSTALL__" ] \
-|| [ "$with_cosma" = "__INSTALL__" ] \
-|| [ "$with_spfft" = "__INSTALL__" ] \
-|| [ "$with_spla" = "__INSTALL__" ];
-then
+if [ "$with_spglib" = "__INSTALL__" ] ||
+  [ "$with_libvori" = "__INSTALL__" ] ||
+  [ "$with_scalapack" = "__INSTALL__" ] ||
+  [ "$with_superlu" = "__INSTALL__" ] ||
+  [ "$with_sirius" = "__INSTALL__" ] ||
+  [ "$with_cosma" = "__INSTALL__" ] ||
+  [ "$with_spfft" = "__INSTALL__" ] ||
+  [ "$with_spla" = "__INSTALL__" ]; then
   [ "$with_cmake" = "__DONTUSE__" ] && with_cmake="__INSTALL__"
 fi
 
