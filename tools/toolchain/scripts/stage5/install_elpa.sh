27118a64d1748213c8d11f447b15714097be86ef
Matthias Krack <matthias.krack@psi.ch>
2021-12-16 11:24:02 +0100

    Workaround for multiple definition error (ELPA-2021.11.001.rc1)

--- tools/toolchain/scripts/stage5/install_elpa.sh
+++ tools/toolchain/scripts/stage5/install_elpa.sh
@@ -124,7 +124,7 @@ case "$with_elpa" in
           FCFLAGS="${FCFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS} -ffree-line-length-none ${AVX_flag} ${FMA_flag} ${SSE4_flag} ${AVX512_flags}" \
           CFLAGS="${CFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS} ${AVX_flag} ${FMA_flag} ${SSE4_flag} ${AVX512_flags}" \
           CXXFLAGS="${CXXFLAGS} ${MATH_CFLAGS} ${SCALAPACK_CFLAGS} ${AVX_flag} ${FMA_flag} ${SSE4_flag} ${AVX512_flags}" \
-          LDFLAGS="-Wl,--enable-new-dtags ${MATH_LDFLAGS} ${SCALAPACK_LDFLAGS} ${cray_ldflags}" \
+          LDFLAGS="-Wl,--allow-multiple-definition -Wl,--enable-new-dtags ${MATH_LDFLAGS} ${SCALAPACK_LDFLAGS} ${cray_ldflags}" \
           LIBS="${SCALAPACK_LIBS} $(resolve_string "${MATH_LIBS}" "MPI")" \
           > configure.log 2>&1
         make -j $(get_nprocs) ${ELPA_MAKEOPTS} > make.log 2>&1
