cd2c5c7a9acfda11d2ddd617d2fa176b9e0f5209
Matthias Krack <matthias.krack@psi.ch>
2021-12-30 00:29:10 +0100

    Restrict fix #1840 to Intel

--- tools/toolchain/scripts/stage3/install_libint.sh
+++ tools/toolchain/scripts/stage3/install_libint.sh
@@ -86,8 +86,10 @@ case "$with_libint" in
         --libdir="${pkg_install_dir}/lib" \
         > configure.log 2>&1
 
-      # Fix bug in makefile for Fortran module
-      sed -i "s/\$(CXX) \$(CXXFLAGS)/\$(FC) \$(FCFLAGS)/g" fortran/Makefile
+      if [ ${MPI_MODE} == "intelmpi" ]; then
+        # Fix bug in makefile for Fortran module
+        sed -i "s/\$(CXX) \$(CXXFLAGS)/\$(FC) \$(FCFLAGS)/g" fortran/Makefile
+      fi
 
       make -j $(get_nprocs) > make.log 2>&1
       make install > install.log 2>&1
