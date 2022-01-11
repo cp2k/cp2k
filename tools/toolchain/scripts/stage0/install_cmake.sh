561df16441c9984ef905d11a0814d81090a101f4
Matthias Krack <matthias.krack@psi.ch>
2022-01-11 14:44:13 +0100

    Dump log file on non-zero exit code

--- tools/toolchain/scripts/stage0/install_cmake.sh
+++ tools/toolchain/scripts/stage0/install_cmake.sh
@@ -38,7 +38,7 @@ case "$with_cmake" in
       fi
       echo "Installing from scratch into ${pkg_install_dir}"
       mkdir -p ${pkg_install_dir}
-      /bin/sh cmake-${cmake_ver}-Linux-x86_64.sh --prefix=${pkg_install_dir} --skip-license > install.log 2>&1
+      /bin/sh cmake-${cmake_ver}-Linux-x86_64.sh --prefix=${pkg_install_dir} --skip-license > install.log 2>&1 || cat install.log
       write_checksums "${install_lock_file}" "${SCRIPT_DIR}/stage0/$(basename ${SCRIPT_NAME})"
     fi
     ;;
