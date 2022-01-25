5993512e1fa08b14842e7c92e93eaede930e58eb
Alfio Lazzaro <alfio.lazzaro@gmail.com>
2022-01-25 17:31:08 +0100

    pretty

--- src/pw/gpu/pw_gpu_internal.cc
+++ src/pw/gpu/pw_gpu_internal.cc
@@ -5,7 +5,7 @@
 /*  SPDX-License-Identifier: GPL-2.0-or-later                                 */
 /*----------------------------------------------------------------------------*/
 
-#if defined (__PW_GPU)
+#if defined(__PW_GPU)
 
 #include <array>
 #include <cstdio>
