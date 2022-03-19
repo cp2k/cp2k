a8ecf2a9c65627c9cc731edbe4b4a13d528f2fa9
Ole Schütt <ole.schuett@cp2k.org>
2022-03-19 08:09:44 +0100

    grid: Migrate GPU backend to offload_runtime.h

--- src/grid/gpu/grid_gpu_integrate.h
+++ src/grid/gpu/grid_gpu_integrate.h
@@ -9,8 +9,8 @@
 
 #ifdef __GRID_CUDA
 
+#include "../../offload/offload_runtime.h"
 #include "grid_gpu_task_list.h"
-#include <cuda_runtime.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -23,7 +23,7 @@ extern "C" {
 void grid_gpu_integrate_one_grid_level(
     const grid_gpu_task_list *task_list, const int first_task,
     const int last_task, const bool compute_tau, const grid_gpu_layout *layout,
-    const cudaStream_t stream, const double *pab_blocks_dev,
+    const offloadStream_t stream, const double *pab_blocks_dev,
     const double *grid_dev, double *hab_blocks_dev, double *forces_dev,
     double *virial_dev, int *lp_diff);
 
