9032f91d3aa99217699d9052951277d088d3de72
Matthias Krack <matthias.krack@psi.ch>
2022-05-18 09:42:54 +0200

    Adaptations for Darwin

--- tools/toolchain/scripts/stage7/install_spglib.sh
+++ tools/toolchain/scripts/stage7/install_spglib.sh
@@ -51,7 +51,7 @@ case "$with_spglib" in
       make -j $(get_nprocs) symspg > make.log 2>&1 || tail -n ${LOG_LINES} make.log
       make install >> install.log 2>&1 || tail -n ${LOG_LINES} install.log
       # Despite -DBUILD_SHARED_LIBS=NO the shared library gets build and installed.
-      rm "${pkg_install_dir}"/lib*/*.so*
+      rm -f "${pkg_install_dir}"/lib*/*.so*
       write_checksums "${install_lock_file}" "${SCRIPT_DIR}/stage7/$(basename ${SCRIPT_NAME})"
     fi
 
