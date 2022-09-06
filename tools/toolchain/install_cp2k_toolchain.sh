938b8b2cb579551b5d5f24310ec41c1ab5e54f0c
Dr. Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2022-09-06 15:12:52 +0200

    Force use of libvdwxc (and cosma) for sirius if not explicitly specified

--- tools/toolchain/install_cp2k_toolchain.sh
+++ tools/toolchain/install_cp2k_toolchain.sh
@@ -782,8 +782,8 @@ if [ "${with_sirius}" = "__INSTALL__" ]; then
   [ "${with_fftw}" = "__DONTUSE__" ] && with_fftw="__INSTALL__"
   [ "${with_spglib}" = "__DONTUSE__" ] && with_spglib="__INSTALL__"
   [ "${with_hdf5}" = "__DONTUSE__" ] && with_hdf5="__INSTALL__"
-#  [ "${with_libvdwxc}" = "__DONTUSE__" ] && with_libvdwxc="__INSTALL__"
-#  [ "${with_cosma}" = "__DONTUSE__" ] && with_cosma="__INSTALL__"
+  [ "${with_libvdwxc}" = "__DONTUSE__" ] && with_libvdwxc="__INSTALL__"
+  [ "${with_cosma}" = "__DONTUSE__" ] && with_cosma="__INSTALL__"
 fi
 
 if [ "${with_plumed}" = "__INSTALL__" ]; then
