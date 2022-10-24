5103be39df6f760b5308dcf5f5b5ead28fe4755a
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2022-10-24 10:49:10 +0200

    Modify FindElpa to consider the openmp / without openmp version

--- cmake/FindElpa.cmake
+++ cmake/FindElpa.cmake
@@ -17,31 +17,33 @@ cp2k_set_default_paths(ELPA "Elpa")
 find_package(PkgConfig)
 
 if(PKG_CONFIG_FOUND)
-  pkg_search_module(CP2K_ELPA elpa elpa_openmp elpa-openmp-2021.11.002
-                    elpa-2021.11.002)
+  if (CP2K_ENABLE_ELPA_OPENMP_SUPPORT)
+    pkg_search_module(CP2K_ELPA REQUIRED IMPORTED_TARGET GLOBAL elpa_openmp)
+  else ()
+    pkg_search_module(CP2K_ELPA REQUIRED IMPORTED_TARGET GLOBAL elpa)
+  endif ()
 else()
   message(
     "ELPA : We need pkg-config to get all information about the elpa library")
 endif()
 
-if(NOT CP2K_ELPA_INCLUDE_DIRS)
-  cp2k_include_dirs(ELPA "elpa.h")
-endif()
-
 find_package_handle_standard_args(Elpa "DEFAULT_MSG" CP2K_ELPA_LINK_LIBRARIES
                                   CP2K_ELPA_INCLUDE_DIRS)
 
-if(CP2K_ELPA_FOUND AND NOT TARGET CP2K_elpa::elpa)
-  add_library(CP2K_ELPA::elpa INTERFACE IMPORTED)
+if(CP2K_ELPA_FOUND)
+  if (NOT TARGET CP2K_elpa::elpa)
+    add_library(CP2K_ELPA::elpa INTERFACE IMPORTED)
+  endif ()
   set_target_properties(
     CP2K_ELPA::elpa PROPERTIES INTERFACE_LINK_LIBRARIES
-                               "${CP2K_ELPA_LINK_LIBRARIES}")
+    "${CP2K_ELPA_LINK_LIBRARIES}")
   if(CP2K_ELPA_INCLUDE_DIRS)
     set_target_properties(
       CP2K_ELPA::elpa PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                 "${CP2K_ELPA_INCLUDE_DIRS}")
+      "${CP2K_ELPA_INCLUDE_DIRS}" "${CP2K_ELPA_INCLUDE_DIRS}/modules")
   endif()
 endif()
 
-mark_as_advanced(CP2K_ELPA_FOUND CP2K_ELPA_LINK_LIBRARIES
-                 CP2K_ELPA_INCLUDE_DIRS)
+mark_as_advanced(CP2K_ELPA_FOUND
+  CP2K_ELPA_LINK_LIBRARIES
+  CP2K_ELPA_INCLUDE_DIRS)
