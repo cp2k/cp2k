8740a2b5ef880761974ea9f67412878ea9f1269f
Rocco Meli <rocco.meli@cscs.ch>
2022-12-12 09:09:04 +0100

    fix parameter expansion in cp2k_usils.cmake

--- cmake/cp2k_utils.cmake
+++ cmake/cp2k_utils.cmake
@@ -71,7 +71,7 @@ endfunction()
 function(cp2k_include_dirs _package_name _library_include_file)  
   find_path(
     CP2K_${_package_name}_INCLUDE_DIRS_TMP
-    NAMES "${_library_include_file}"
+    NAMES ${_library_include_file}
     PATHS "${CP2K_${_package_name}_PREFIX}"
     HINTS "${CP2K_${_package_name}_PREFIX}"
     PATH_SUFFIXES "include" "include/${_pacakge_name}" "${_package_name}")
