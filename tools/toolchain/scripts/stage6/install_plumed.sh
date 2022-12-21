a8fdff0941595c0291b786a6072490551e3ad375
Matthias Krack <matthias.krack@psi.ch>
2022-12-21 11:40:17 +0100

    plumed 2.8.0 -> 2.8.1

--- tools/toolchain/scripts/stage6/install_plumed.sh
+++ tools/toolchain/scripts/stage6/install_plumed.sh
@@ -6,9 +6,9 @@
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
-plumed_ver="2.8.0"
+plumed_ver="2.8.1"
 plumed_pkg="plumed-src-${plumed_ver}.tgz"
-plumed_sha256="c1fc97d60b8acb2583071e247d13307c89ea5c1be1fe630bcbeb33302f588d91"
+plumed_sha256="fd15fb46132c9ac430a228bd18147ca16ef070a8609f6eb9d7ba6c767be46882"
 
 source "${SCRIPT_DIR}"/common_vars.sh
 source "${SCRIPT_DIR}"/tool_kit.sh
