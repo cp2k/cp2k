0e722df91b89847b24d3e2fa39485a0bdfbfba12
Frederick Stein <frederick.stein@chem.uzh.ch>
2023-01-11 10:56:00 +0100

    Bump LibXC to version 6.1.0
    We drop the patch because it was fixed in the given release.

--- tools/toolchain/scripts/stage3/install_libxc.sh
+++ tools/toolchain/scripts/stage3/install_libxc.sh
@@ -6,8 +6,8 @@
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
-libxc_ver="6.0.0"
-libxc_sha256="c2ca205a762200dfba2e6c9e8ca2061aaddc6b7cf42048859fe717a7aa07de7c"
+libxc_ver="6.1.0"
+libxc_sha256="a3aa16915942543031a5d9c4a92c439ce54249bdcda8c91c4e69e65329dc9a54"
 source "${SCRIPT_DIR}"/common_vars.sh
 source "${SCRIPT_DIR}"/tool_kit.sh
 source "${SCRIPT_DIR}"/signal_trap.sh
@@ -39,9 +39,6 @@ case "$with_libxc" in
       [ -d libxc-${libxc_ver} ] && rm -rf libxc-${libxc_ver}
       tar -xzf libxc-${libxc_ver}.tar.gz
       cd libxc-${libxc_ver}
-
-      # Fix missing comma at end of line in src/mgga_xc_b97mv.c
-      patch -p1 < "${SCRIPT_DIR}/stage3/libxc-${libxc_ver}_mgga_xc_b97mv.patch"
 
       # CP2K does not make use of fourth derivatives, so skip their compilation with --disable-lxc
       ./configure --prefix="${pkg_install_dir}" --libdir="${pkg_install_dir}/lib" --disable-lxc \
