946d13bdcf675fa27f4ec2ca26c927edbde6febb
Dr. Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2023-02-22 10:36:51 +0100

    Use one CMakeLists.txt file for each C dependency when it makes sense
    - dbm, grid, offload now have their own CMakeLists.txt
    - list of compiled files is generated by cmake instead of relying on a external program
    - restored the initial src/CMakeLists.txt
    - temporarily removed SIRIUS versioning
    - added cp2k config for cmake package

--- tools/docker/scripts/test_python.sh
+++ tools/docker/scripts/test_python.sh
@@ -28,7 +28,6 @@ unzip -q -d ./tools/minimax_tools/1_xData 1_xData.zip
 run_test ./tools/prettify/prettify_test.py
 run_test ./tools/minimax_tools/minimax_to_fortran_source.py --check
 run_test ./tools/docker/generate_dockerfiles.py --check
-run_test ./cmake/generate_cmakelists.py --check
 
 run_test mypy --strict ./tools/minimax_tools/minimax_to_fortran_source.py
 run_test mypy --strict ./tools/dashboard/generate_dashboard.py
@@ -40,7 +39,6 @@ run_test mypy --strict ./tools/precommit/check_file_properties.py
 run_test mypy --strict ./tools/precommit/format_makefile.py
 run_test mypy --strict ./tools/docker/generate_dockerfiles.py
 run_test mypy --strict ./tools/conventions/analyze_gfortran_ast.py
-run_test mypy --strict ./cmake/generate_cmakelists.py
 
 # TODO: Find a way to test generate_dashboard.py without git repository.
 #
