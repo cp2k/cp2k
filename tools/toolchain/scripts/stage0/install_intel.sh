45c7a2e3f936e9ef7d2b66dbd3ec549e14d14bd8
Matthias Krack <matthias.krack@psi.ch>
2023-03-03 11:10:13 +0100

    Avoid double slashes in compiler path

--- tools/toolchain/scripts/stage0/install_intel.sh
+++ tools/toolchain/scripts/stage0/install_intel.sh
@@ -28,9 +28,9 @@ case "${with_intel}" in
     ;;
   __SYSTEM__)
     echo "==================== Finding Intel compiler from system paths ===================="
-    check_command icc "intel" && CC="$(command -v icc)" || exit 1
-    check_command icpc "intel" && CXX="$(command -v icpc)" || exit 1
-    check_command ifort "intel" && FC="$(command -v ifort)" || exit 1
+    check_command icc "intel" && CC="$(realpath $(command -v icx))" || exit 1
+    check_command icpc "intel" && CXX="$(realpath $(command -v icpx))" || exit 1
+    check_command ifort "intel" && FC="$(realpath $(command -v ifort))" || exit 1
     F90="${FC}"
     F77="${FC}"
     ;;
