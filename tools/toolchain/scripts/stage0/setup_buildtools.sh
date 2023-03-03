fff3d3dd54368f068b41014d6af1567bdee4dc2b
Matthias Krack <matthias.krack@psi.ch>
2023-03-03 12:20:16 +0100

    Revise Intel compiler flags
    Use Intel compiler specific flags

--- tools/toolchain/scripts/stage0/setup_buildtools.sh
+++ tools/toolchain/scripts/stage0/setup_buildtools.sh
@@ -20,24 +20,47 @@ done
 # Install or compile packages using newly installed tools
 # ------------------------------------------------------------------------
 
-# setup compiler flags, leading to nice stack traces on crashes but still optimised
-if [ "${TARGET_CPU}" = "generic" ]; then
-  CFLAGS="-O2 -fPIC -fno-omit-frame-pointer -fopenmp -g -mtune=generic ${TSANFLAGS}"
+# Setup compiler flags, leading to nice stack traces on crashes but still optimised
+if [ "${with_intel}" != "__DONTUSE__" ]; then
+  CFLAGS="-O2 -fPIC -fp-model precise -funroll-loops -g -qopenmp -qopenmp-simd -traceback"
+  if [ "${TARGET_CPU}" = "native" ]; then
+    CFLAGS="${CFLAGS} -xHost"
+  elif [ "${TARGET_CPU}" = "generic" ]; then
+    CFLAGS="${CFLAGS} -mtune=${TARGET_CPU}"
+  else
+    CFLAGS="${CFLAGS} -march=${TARGET_CPU} -mtune=${TARGET_CPU}"
+  fi
+  FFLAGS="${CFLAGS}"
 else
-  CFLAGS="-O2 -fPIC -fno-omit-frame-pointer -fopenmp -g -march=${TARGET_CPU} -mtune=${TARGET_CPU} ${TSANFLAGS}"
+  CFLAGS="-O2 -fPIC -fno-omit-frame-pointer -fopenmp -g"
+  if [ "${TARGET_CPU}" = "generic" ]; then
+    CFLAGS="${CFLAGS} -mtune=generic ${TSANFLAGS}"
+  else
+    CFLAGS="${CFLAGS} -march=${TARGET_CPU} -mtune=${TARGET_CPU} ${TSANFLAGS}"
+  fi
+  FFLAGS="${CFLAGS} -fbacktrace"
 fi
-FFLAGS="${CFLAGS} -fbacktrace"
 CXXFLAGS="${CFLAGS}"
 F77FLAGS="${FFLAGS}"
 F90FLAGS="${FFLAGS}"
 FCFLAGS="${FFLAGS}"
 
-export CFLAGS="$(allowed_gcc_flags ${CFLAGS})"
-export FFLAGS="$(allowed_gfortran_flags ${FFLAGS})"
-export F77FLAGS="$(allowed_gfortran_flags ${F77FLAGS})"
-export F90FLAGS="$(allowed_gfortran_flags ${F90FLAGS})"
-export FCFLAGS="$(allowed_gfortran_flags ${FCFLAGS})"
-export CXXFLAGS="$(allowed_gxx_flags ${CXXFLAGS})"
+if [ "${with_intel}" == "__DONTUSE__" ]; then
+  export CFLAGS="$(allowed_gcc_flags ${CFLAGS})"
+  export FFLAGS="$(allowed_gfortran_flags ${FFLAGS})"
+  export F77FLAGS="$(allowed_gfortran_flags ${F77FLAGS})"
+  export F90FLAGS="$(allowed_gfortran_flags ${F90FLAGS})"
+  export FCFLAGS="$(allowed_gfortran_flags ${FCFLAGS})"
+  export CXXFLAGS="$(allowed_gxx_flags ${CXXFLAGS})"
+else
+  # TODO Check functions for allowed Intel compiler flags
+  export CFLAGS
+  export FFLAGS
+  export F77FLAGS
+  export F90FLAGS
+  export FCFLAGS
+  export CXXFLAGS
+fi
 export LDFLAGS="${TSANFLAGS}"
 
 # get system arch information using OpenBLAS prebuild
