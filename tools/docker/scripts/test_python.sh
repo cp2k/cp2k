fac1893f6ec53c00a2bff811530ef2a8edddc954
Ole Schuett <ole.schuett@cp2k.org>
2023-03-24 16:27:50 +0100

    Add generate_apptainer_def_files.py to test_python.sh

--- tools/docker/scripts/test_python.sh
+++ tools/docker/scripts/test_python.sh
@@ -28,6 +28,7 @@ unzip -q -d ./tools/minimax_tools/1_xData 1_xData.zip
 run_test ./tools/prettify/prettify_test.py
 run_test ./tools/minimax_tools/minimax_to_fortran_source.py --check
 run_test ./tools/docker/generate_dockerfiles.py --check
+run_test ./tools/apptainer/generate_apptainer_def_files.py --check
 
 run_test mypy --strict ./tools/minimax_tools/minimax_to_fortran_source.py
 run_test mypy --strict ./tools/dashboard/generate_dashboard.py
@@ -38,6 +39,7 @@ run_test mypy --strict ./tools/precommit/precommit.py
 run_test mypy --strict ./tools/precommit/check_file_properties.py
 run_test mypy --strict ./tools/precommit/format_makefile.py
 run_test mypy --strict ./tools/docker/generate_dockerfiles.py
+run_test mypy --strict ./tools/apptainer/generate_apptainer_def_files.py
 run_test mypy --strict ./tools/conventions/analyze_gfortran_ast.py
 
 # TODO: Find a way to test generate_dashboard.py without git repository.
