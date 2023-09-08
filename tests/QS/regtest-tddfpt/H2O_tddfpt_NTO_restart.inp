cp2k/cp2k

commit 18d5c6eecb1583739a723fdc877dd6e49c5a3a43

SOC absorption for TDDFPT

tests/QS/regtest-tddfpt-soc/TEST_FILES
@@ -1,0 +1,9 @@
+# runs are executed in the same order as in this file
+# the second field tells which test should be run in order to compare with the last available output
+# e.g. 0 means do not compare anything, running is enough
+#      1 compares the last total energy in the file
+#      for details see cp2k/tools/do_regtest
+regtest-Ar-S.inp              104    4.0E-06           20.8357607201
+regtest-Ar-T.inp              104    4.0E-06           20.8357607201
+regtest-Ne-S.inp              104    4.0E-06           45.3245164440
+#EOF

src/CMakeLists.txt
@@ -653,6 +653,9 @@ list(
   qs_tddfpt2_operators.F
   qs_tddfpt2_properties.F
   qs_tddfpt2_restart.F
+  qs_tddfpt2_soc.F
+  qs_tddfpt2_soc_utils.F
+  qs_tddfpt2_soc_types.F
   qs_tddfpt2_stda_types.F
   qs_tddfpt2_stda_utils.F
   qs_tddfpt2_subgroups.F

src/atom_utils.F
@@ -1089,6 +1089,7 @@ CONTAINS
       int1 = fourpi*integrate_grid(density, grid)
       int2 = 0._dp
       cpot(nc:) = int1/r(nc:)
+      !   IF (log_unit>0) WRITE(log_unit,"(A,2F10.8)") "r", int1, cpot(nc:)
 
       ! test that grid is decreasing
       CPASSERT(r(1) > r(nc))

src/cp_control_types.F
@@ -512,6 +512,9 @@ MODULE cp_control_types
       !> user-defined reference point
       REAL(kind=dp), DIMENSION(:), POINTER :: dipole_ref_point
       !
+      ! SOC subsection
+      LOGICAL                              :: do_soc
+      !
       ! MGRID subsection
       !
       !> number of plain-wave grids
@@ -920,6 +923,7 @@ CONTAINS
       CALL timeset(routineN, handle)
 
       ALLOCATE (tddfpt_control)
+      tddfpt_control%do_soc = .FALSE.
 
       CALL timestop(handle)
    END SUBROUTINE tddfpt2_control_create

src/cp_control_utils.F
@@ -1289,7 +1289,8 @@ CONTAINS
                                                             multigrid_set
       REAL(KIND=dp)                                      :: filter, fval, hfx
       TYPE(section_vals_type), POINTER                   :: dipole_section, mgrid_section, &
-                                                            stda_section, xc_func, xc_section
+                                                            soc_section, stda_section, xc_func, &
+                                                            xc_section
 
       CALL timeset(routineN, handle)
 
@@ -1369,6 +1370,13 @@ CONTAINS
          IF (t_control%dipole_form == tddfpt_dipole_length .AND. t_control%dipole_reference == use_mom_ref_user) THEN
             CPABORT("User-defined reference point should be given explicitly")
          END IF
+      END IF
+
+      !SOC subsection
+      soc_section => section_vals_get_subs_vals(t_section, "SOC")
+      CALL section_vals_get(soc_section, explicit=explicit)
+      IF (explicit) THEN
+         t_control%do_soc = .TRUE.
       END IF
 
       ! MGRID subsection

src/header.F
@@ -27,6 +27,7 @@ MODULE header
    PUBLIC :: qmmm_header, atom_header, atom_footer, band_header, xtb_header
    PUBLIC :: tmc_header, tmc_ana_header
    PUBLIC :: sirius_header
+   PUBLIC :: tddfpt_soc_header
 
 CONTAINS
 
@@ -133,6 +134,27 @@ CONTAINS
          "*******************************************************************************"
 
    END SUBROUTINE tddfpt_header
+
+! **************************************************************************************************
+!> \brief ...
+!> \param iw ...
+! **************************************************************************************************
+   SUBROUTINE tddfpt_soc_header(iw)
+      INTEGER, INTENT(in)                                :: iw
+
+      IF (iw < 0) RETURN
+      WRITE (iw, '(/,(T2,A79))') &
+         "*******************************************************************************", &
+         "**                                                                           **", &
+         "**                            ####### ####### #######                        **", &
+         "**                        #   ##      ##   ## ##                             **", &
+         "**                       ###  ######  ##   ## ##                             **", &
+         "**                        #       ##  ##   ## ##                             **", &
+         "**                            ######  ####### #######                        **", &
+         "**                 Based on the xas module from Augustin Bussy               **", &
+         "*******************************************************************************"
+
+   END SUBROUTINE tddfpt_soc_header
 
 ! **************************************************************************************************
 !> \brief ...

src/input_cp2k_properties_dft.F
@@ -1603,6 +1603,34 @@ CONTAINS
       CALL section_add_subsection(section, subsection)
       CALL section_release(subsection)
 
+      ! SOC functional
+
+      CALL section_create(subsection, __LOCATION__, name="SOC", &
+                          description="Is jet to be implemented", &
+                          n_keywords=2, n_subsections=0, repeats=.FALSE.)
+
+      CALL keyword_create(keyword, __LOCATION__, name="EPS_FILTER", &
+                          variants=s2a("EPS_FILTER_MATRIX"), &
+                          description="The threshold used for sparse matrix operations", &
+                          usage="EPS_FILTER {real}", &
+                          type_of_var=real_t, &
+                          default_r_val=1.0E-10_dp)
+      CALL section_add_keyword(subsection, keyword)
+      CALL keyword_release(keyword)
+
+      CALL keyword_create(keyword, __LOCATION__, name="GRID", &
+                          variants=(/"ATOMIC_GRID"/), &
+                          description="Specification of the atomic angular and radial grids for "// &
+                          "a atomic kind. This keyword must be repeated for all kinds!  "// &
+                          "Usage: GRID < LEBEDEV_GRID > < RADIAL_GRID >", &
+                          usage="GRID {string} {integer} {integer}", &
+                          n_var=3, type_of_var=char_t, repeats=.TRUE.)
+      CALL section_add_keyword(subsection, keyword)
+      CALL keyword_release(keyword)
+
+      CALL section_add_subsection(section, subsection)
+      CALL section_release(subsection)
+
       ! kernel XC functional
       CALL create_xc_section(subsection)
       CALL section_add_subsection(section, subsection)
@@ -1757,6 +1785,33 @@ CONTAINS
       CALL keyword_release(keyword)
       CALL keyword_create(keyword, __LOCATION__, name="SCALE_WITH_PHASES", &
                           description="Scale ES eigenvectors with phases of occupied and virtuals MOs.", &
+                          default_l_val=.FALSE., lone_keyword_l_val=.TRUE.)
+      CALL section_add_keyword(print_key, keyword)
+      CALL keyword_release(keyword)
+      CALL section_add_subsection(subsection, print_key)
+      CALL section_release(print_key)
+
+      !! SOC PRINT SECTION
+      CALL cp_print_key_section_create(print_key, __LOCATION__, name="SOC_PRINT", &
+                                       description="Controls the printout of the tddfpt2_soc modul", &
+                                       print_level=debug_print_level + 1, filename="SOC")
+      CALL keyword_create(keyword, __LOCATION__, name="UNIT_eV", &
+                          description="Will detrement if output in eVolt will be printef.", &
+                          default_l_val=.TRUE., lone_keyword_l_val=.TRUE.)
+      CALL section_add_keyword(print_key, keyword)
+      CALL keyword_release(keyword)
+      CALL keyword_create(keyword, __LOCATION__, name="UNIT_wn", &
+                          description="Will detrement if output in wavenumbers will be printed.", &
+                          default_l_val=.FALSE., lone_keyword_l_val=.TRUE.)
+      CALL section_add_keyword(print_key, keyword)
+      CALL keyword_release(keyword)
+      CALL keyword_create(keyword, __LOCATION__, name="SPLITTING", &
+                          description="Will add the SOC-Splitting as additional output", &
+                          default_l_val=.FALSE., lone_keyword_l_val=.TRUE.)
+      CALL section_add_keyword(print_key, keyword)
+      CALL keyword_release(keyword)
+      CALL keyword_create(keyword, __LOCATION__, name="SOME", &
+                          description="Will add the SOC-Matrix as additional output in a different file", &
                           default_l_val=.FALSE., lone_keyword_l_val=.TRUE.)
       CALL section_add_keyword(print_key, keyword)
       CALL keyword_release(keyword)

commit c980ceb41ca5d2b89fd3c5d91c02c9e071c5c7ee

Fix TDDFPT restart (#2975)
This PR fixes the TDDFPT restart functionality broken since PR #2859.

Adds a regtest testing the restart functionality.

src/qs_tddfpt2_methods.F
@@ -882,6 +882,8 @@ CONTAINS
 
       ! reuse Ritz vectors from the previous calculation if available
       IF (tddfpt_control%is_restart .AND. .NOT. do_soc) THEN
+         CALL get_qs_env(qs_env, blacs_env=blacs_env)
+
          nstates_read = tddfpt_read_restart( &
                         evects=evects, &
                         evals=evals, &

tests/QS/regtest-tddfpt/H2O_tddfpt_NTO_restart.inp
@@ -1,0 +1,72 @@
+&GLOBAL
+  PROJECT test
+  RUN_TYPE ENERGY
+  PRINT_LEVEL LOW
+&END GLOBAL
+&FORCE_EVAL
+  METHOD Quickstep
+  &PROPERTIES
+    &TDDFPT
+       RESTART      T
+       NSTATES      3
+       MAX_ITER    10
+       MAX_KV      10
+       CONVERGENCE 1.0e-5
+       &XC
+         &XC_FUNCTIONAL PBE
+         &END XC_FUNCTIONAL
+         &XC_GRID
+            XC_DERIV SPLINE2_SMOOTH
+         &END XC_GRID
+       &END XC
+       &PRINT
+          &NTO_ANALYSIS
+             CUBE_FILES
+             THRESHOLD  0.99
+             INTENSITY_THRESHOLD 0.02
+             STATE_LIST 1 3
+          &END
+          &MOS_MOLDEN
+          &END
+       &END
+    &END TDDFPT
+  &END PROPERTIES
+
+  &DFT
+    CHARGE 1
+    LSD
+    BASIS_SET_FILE_NAME BASIS_SET
+    POTENTIAL_FILE_NAME POTENTIAL
+    &MGRID
+      CUTOFF 100
+    &END MGRID
+    &QS
+    &END QS
+    &SCF
+      MAX_SCF 10
+      SCF_GUESS atomic
+    &END SCF
+    &XC
+      &XC_FUNCTIONAL PBE
+      &END XC_FUNCTIONAL
+    &END XC
+  &END DFT
+  &SUBSYS
+    &CELL
+      ABC 5.0 5.0 5.0
+    &END CELL
+    &COORD
+    O   0.000000    0.000000   -0.065587 H2O
+    H   0.000000   -0.757136    0.520545 H2O
+    H   0.000000    0.757136    0.520545 H2O
+    &END COORD
+    &KIND H
+      BASIS_SET DZVP-GTH-PADE
+      POTENTIAL GTH-PADE-q1
+    &END KIND
+    &KIND O
+      BASIS_SET DZVP-GTH-PADE
+      POTENTIAL GTH-PADE-q6
+    &END KIND
+  &END SUBSYS
+&END FORCE_EVAL

tests/QS/regtest-tddfpt/TEST_FILES
@@ -16,5 +16,6 @@ NO_tddfpt-s-3.inp                                     37    4.0E-06             
 NO_tddfpt-t-3.inp                                     37    4.0E-06                   0.358683E+00
 H2O_tddfpt_NTO.inp                                    37    4.0E-06                   0.542432E+00
 H2O_tddfpt_NTO_slist.inp                              37    4.0E-06                   0.542432E+00
+H2O_tddfpt_NTO_restart.inp                            37    4.0E-06                   0.542432E+00
 #
 #EOF