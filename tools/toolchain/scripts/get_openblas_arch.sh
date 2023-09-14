3d06d12abdce2a584ea550c72d3b4de7863883ae
Matthias Krack <matthias.krack@psi.ch>
2023-09-14 11:26:29 +0200

    OpenBLAS 0.3.23 -> 0.3.24

--- tools/toolchain/scripts/get_openblas_arch.sh
+++ tools/toolchain/scripts/get_openblas_arch.sh
@@ -6,8 +6,8 @@
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")" && pwd -P)"
 
-openblas_ver="0.3.23" # Keep in sync with install_openblas.sh
-openblas_sha256="5d9491d07168a5d00116cdc068a40022c3455bf9293c7cb86a65b1054d7e5114"
+openblas_ver="0.3.24" # Keep in sync with install_openblas.sh
+openblas_sha256="ceadc5065da97bd92404cac7254da66cc6eb192679cf1002098688978d4d5132"
 openblas_pkg="OpenBLAS-${openblas_ver}.tar.gz"
 
 source "${SCRIPT_DIR}"/common_vars.sh
