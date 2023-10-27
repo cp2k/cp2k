f9bdf92445d0f3e598d1b37c9fba08770a01e83b
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2023-10-27 09:55:24 +0200

    Docker files for spack stack
    Signed-off-by: Mathieu Taillefumier <mathieu.taillefumier@free.fr>

--- tools/docker/generate_dockerfiles.py
+++ tools/docker/generate_dockerfiles.py
@@ -125,13 +125,13 @@ def main() -> None:
             f.write(test_without_build(name))
 
     with OutputFile(f"Dockerfile.gcc_spack", args.check) as f:
-        f.write(spack_toolchain_cpu())
+        f.write(spack_toolchain(distro="ubuntu:22.04", spec="cp2k@master%gcc build_system=cmake +enable_regtests +cosma +mpi +openmp ^openblas+fortran ^cosma+scalapack+shared ^dbcsr+mpi+shared"))
 
     with OutputFile(f"Dockerfile.gcc_spack_cuda", args.check) as f:
-        f.write(spack_toolchain_cuda())
+        f.write(spack_toolchain(distro="nvidia/cuda:12.2.0-devel-ubuntu22.04", spec="cp2k@master%gcc build_system=cmake +libxc+libint+sirius+elpa+plumed+pexsi+spglib+cosma+mpi+openmp+cuda cuda_arch=60  smm=libxsmm ^openblas+fortran ^cosma+scalapack+shared+cuda ^dbcsr+cuda~shared cuda_arch=60 ^sirius+cuda"))
 
     with OutputFile(f"Dockerfile.gcc_spack_rocm", args.check) as f:
-        f.write(spack_toolchain_rocm())
+        f.write(spack_toolchain(distro="rocm/dev-ubuntu-22.04:5.6.1-complete", spec="cp2k@master%gcc build_system=cmake +sirius +elpa +libxc +libint smm=libxsmm +spglib +cosma +rocm amdgpu_target=gfx90a +pexsi +plumed +libvori +openmp ^openblas+fortran ^dbcsr+mpi+rocm~shared+openmp amdgpu_target=gfx906 ^cosma+shared~tests~apps+rocm"))
 
 
 # ======================================================================================
@@ -788,14 +788,9 @@ COPY ./tools/toolchain/scripts/arch_base.tmpl \
 RUN ./scripts/generate_arch_files.sh && rm -rf ./build
 """.lstrip()
 
-
-# ======================================================================================
-def spack_toolchain_cpu() -> str:
+def spack_toolchain(distro: str, spec: str) -> str:
     return rf"""
-FROM docker.io/ubuntu22.04 as builder
-
-ARG CUDA_ARCH=80
-
+FROM {distro} as builder
 ENV DEBIAN_FRONTEND noninteractive
 
 ENV FORCE_UNSAFE_CONFIGURE 1
@@ -805,7 +800,7 @@ ENV PATH="/spack/bin:${{PATH}}"
 ENV MPICH_VERSION=4.0.3
 ENV CMAKE_VERSION=3.25.2
 RUN apt-get update -qq
-RUN apt-get install -qq --no-install-recommends autoconf autogen automake autotools-dev bzip2 ca-certificates g++ gcc gfortran git less libtool libtool-bin make nano patch pkg-config python3 unzip wget xxd zlib1g-dev cmake gnupg m4 xz-utils libssl-dev libssh-dev
+RUN apt-get install -qq --no-install-recommends autoconf autogen automake autotools-dev bzip2 ca-certificates g++ gcc gfortran git less libtool libtool-bin make nano patch pkg-config python3 unzip wget xxd zlib1g-dev cmake gnupg m4 xz-utils libssl-dev libssh-dev openmpi-common libopenmpi-dev 
 RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_386 && chmod a+x /usr/local/bin/yq
 # get latest version of spack
 RUN git clone https://github.com/spack/spack.git
@@ -819,184 +814,45 @@ RUN spack external find --all --exclude python
 RUN spack compiler find
 # tweaking the arguments
 RUN yq -i '.compilers[0].compiler.flags.fflags = "-fallow-argument-mismatch"' /root/.spack/linux/compilers.yaml
-
-# copy bunch of things from the ci
-COPY ci/spack /root/spack-recipe
-RUN spack repo add /root/spack-recipe/ --scope user
+#RUN spack config add packages:all:target:x86_64
+# set amdgpu_target for all packages
+# RUN spack config add packages:all:variants:amdgpu_target=gfx90a
 
 #install openmpi
-RUN spack install openmpi
+#RUN spack install openmpi
 
 #install few dependencies.
-RUN spack install intel-oneapi-mkl+cluster
+RUN spack install cmake@3.27.3
 RUN spack install openblas+fortran
 RUN spack install libxsmm
 RUN spack install libxc
 RUN spack install gsl
 RUN spack install py-fypp
 RUN spack install spglib
-RUN spack install fftw
-RUN spack install fftw+openmp
 RUN spack install libvori
 
-# full spec
-ENV SPEC_OPENBLAS="cp2k@master%gcc +libxc +libint +sirius +elpa +plumed +pexsi smm=libxsmm +spglib +cosma +mpi +openmp ^openblas+fortran ^cosma+scalapack+shared"
+ENV SPEC_OPENBLAS="{spec}"
 
-# NB : the next 4 lines can be removed normally but it is still better to create
-  additional stages in case of a build failure of cp2k.
-
-# install all dependencies
+# instal all dependencies
+RUN mkdir /cp2k-src
+RUN spack env create --with-view /opt/cp2k cp2k-env
 RUN spack install --only=dependencies --fail-fast $SPEC_OPENBLAS ^openmpi
-RUN spack install --fail-fast $SPEC_OPENBLAS ^openmpi
-RUN spack clean -dfs
+
+# copy source files of the pull request into container
+COPY . /cp2k-src
+RUN spack --color always -e cp2k-env dev-build -q --source-path /cp2k-src $SPEC_OPENBLAS ^openmpi
+
+# Build CP2K with CMake and run regression tests.
+ARG TESTOPTS=""
+RUN /bin/bash -o pipefail -c " \
+    TESTOPTS='${{TESTOPTS}}' \
+    /cp2k-src/tools/docker/scripts/test_regtest_spack.sh |& tee report.log && \
+    rm -rf regtesting"
+
+# Output the report if the image is old and was therefore pulled from the build cache.
+CMD cat $(find ./report.log -mmin +10) | sed '/^Summary:/ s/$/ (cached)/'
+ENTRYPOINT []
 """
-
-
-# ======================================================================================
-def spack_toolchain_cuda() -> str:
-    return rf"""
-FROM docker.io/nvidia/cuda:12.1.0-devel-ubuntu22.04 as builder
-
-ARG CUDA_ARCH=80
-
-ENV DEBIAN_FRONTEND noninteractive
-
-ENV FORCE_UNSAFE_CONFIGURE 1
-ENV LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda/lib64/stubs
-
-ENV PATH="/spack/bin:${{PATH}}"
-
-ENV MPICH_VERSION=4.0.3
-ENV CMAKE_VERSION=3.25.2
-RUN apt-get update -qq
-RUN apt-get install -qq --no-install-recommends autoconf autogen automake autotools-dev bzip2 ca-certificates g++ gcc gfortran git less libtool libtool-bin make nano patch pkg-config python3 unzip wget xxd zlib1g-dev cmake gnupg m4 xz-utils libssl-dev libssh-dev
-RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_386 && chmod a+x /usr/local/bin/yq
-# get latest version of spack
-RUN git clone https://github.com/spack/spack.git
-
-# set the location of packages built by spack
-RUN spack config add config:install_tree:root:/opt/spack
-# set cuda_arch for all packages
-# RUN spack config add packages:all:variants:cuda_arch=${{CUDA_ARCH}}
-
-# find all external packages
-RUN spack external find --all --exclude python
-# find compilers
-RUN spack compiler find
-# tweaking the arguments
-RUN yq -i '.compilers[0].compiler.flags.fflags = "-fallow-argument-mismatch"' /root/.spack/linux/compilers.yaml
-
-# copy bunch of things from the ci
-COPY ci/spack /root/spack-recipe
-RUN spack repo add /root/spack-recipe/ --scope user
-
-#install openmpi
-RUN spack install openmpi
-
-#install few dependencies.
-RUN spack install openblas+fortran
-RUN spack install libxsmm
-RUN spack install gsl
-RUN spack install py-fypp
-RUN spack install spglib
-RUN spack install fftw
-RUN spack install fftw+openmp
-RUN spack install libvori
-RUN ldconfig
-
-# full spec
-ENV SPEC_OPENBLAS="cp2k@master%gcc build_system=cmake +libxc+libint+sirius+elpa+plumed+pexsi+spglib+cosma+mpi+openmp+cuda cuda_arch=80  smm=libxsmm ^openblas+fortran ^cosma+scalapack+shared+cuda ^dbcsr+cuda~shared cuda_arch=70 ^sirius+cuda"
-#ENV SPEC_MKL="cp2k@master%gcc build_system=cmake +libxc+libint+sirius+elpa+plumed+pexsi smm=libxsmm +spglib +mpi +openmp +cuda cuda_arch=80 ^intel-oneapi-mkl+cluster ^dbcsr+cuda+shared cuda_arch=70"
-
-# NB : the next 4 lines can be removed normally but it is still better to create
-#  additional stages in case of a build failure of cp2k.
-
-# install all dependencies
-RUN spack install --only=dependencies --fail-fast $SPEC_OPENBLAS ^openmpi
-# install cp2k itself
-RUN spack install --fail-fast $SPEC_OPENBLAS ^openmpi
-RUN spack clean -dfs
-"""
-
-
-# ======================================================================================
-def spack_toolchain_rocm() -> str:
-    return rf"""
-FROM docker.io/rocm/dev-ubuntu-22.04:5.5.1-complete
-
-ARG ROCM_ARCH=gfx90a
-ARG ROCM_VERSION=5.5.1
-ARG AMDGPU_VERSION=5.4.3
-ENV DEBIAN_FRONTEND=noninteractive
-
-ENV FORCE_UNSAFE_CONFIGURE 1
-
-ENV PATH="/spack/bin:${{PATH}}"
-
-ENV CMAKE_VERSION=3.26.3
-ENV MPICH_VERSION=3.4.3
-
-RUN apt-get -y update && apt-get install -y apt-utils
-
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ca-certificates curl gnupg && \
-  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-  sudo \
-  libelf1 \
-  xz-utils \
-  liblzma-dev \
-  libnuma-dev \
-  build-essential \
-  git \
-  vim-nox \
-  cmake-curses-gui \
-  kmod \
-  file \
-  libbz2-dev \
-  python3 \
-  python3-dev \
-  python3-pip \
-  rocm-dev gcc g++ gfortran clang git make unzip \
-  vim wget pkg-config python3-pip python3-venv curl tcl m4 cpio automake \
-  apt-transport-https ca-certificates gnupg software-properties-common \
-  patchelf meson
-
-# install CMake
-RUN wget https://github.com/Kitware/CMake/releases/download/v${{CMAKE_VERSION}}/cmake-${{CMAKE_VERSION}}-linux-x86_64.tar.gz -O cmake.tar.gz && \
-    tar zxvf cmake.tar.gz --strip-components=1 -C /usr
-
-# get latest version of spack
-RUN git clone https://github.com/spack/spack.git
-
-# set the location of packages built by spack
-RUN spack config add config:install_tree:root:/opt/local
-# set amdgpu_target for all packages
-RUN spack config add packages:all:variants:amdgpu_target=${{ROCM_ARCH}}
-# set basic x86_64 architecture
-RUN spack config add packages:all:target:x86_64
-
-# find gcc and clang compilers
-RUN spack compiler find
-RUN spack external find --all
-
-# install yq (utility to manipulate the yaml files)
-RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_386 && chmod a+x /usr/local/bin/yq
-
-# change the fortran compilers: for gcc the gfortran is already properly set and the change has no effect; add it for clang
-RUN yq -i '.compilers[0].compiler.flags.fflags = "-fallow-argument-mismatch"' /root/.spack/linux/compilers.yaml
-
-# copy bunch of things from the ci
-COPY ci/spack /root/spack-recipe
-RUN spack repo add /root/spack-recipe/ --scope user
-
-# find gcc and clang compilers
-#RUN spack compiler find
-#RUN spack external find --all
-
-ENV SPEC_OPENBLAS="cp2k@master%gcc build_system=cmake +sirius +elpa +libxc +libint smm=libxsmm +spglib +cosma +rocm amdgpu_target=gfx90a +pexsi +plumed +libvori +openmp ^openblas+fortran ^dbcsr+mpi+rocm~shared+openmp amdgpu_target=gfx906 ^cosma+shared~tests~apps+rocm"
-# install all dependencies
-RUN spack install --only=dependencies --fail-fast $SPEC_OPENBLAS ^openmpi
-"""
-
 
 # ======================================================================================
 class OutputFile:
