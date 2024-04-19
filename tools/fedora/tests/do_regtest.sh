5c255f1fefa64bc087bd4c6a8f9cc27473b1fda7
Cristian Le <cristian.le@mpsd.mpg.de>
2024-04-19 16:20:33 +0200

    Synchronize with Fedora downstream
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- tools/fedora/tests/do_regtest.sh
+++ tools/fedora/tests/do_regtest.sh
@@ -7,9 +7,11 @@ rlJournalStart
   rlPhaseStartTest
     [[ -z "${CP2K_VARIANT}" ]] && rlDie "No CP2K_VARIANT environment variable set"
 
-    rlRun "cd ../../../tests" 0 "cd to cp2k tests folder"
+    rlRun "cd ${TMT_TREE}/tests" 0 "cd to cp2k tests folder"
 
-    rlRun "args=\"--maxtasks $(nproc) --ompthreads 2 --skip_unittests\"" 0 "Set base arguments"
+    rlRun "args=\"--maxtasks $(nproc) --ompthreads 2\"" 0 "Set base arguments"
+    [[ "${CP2K_SKIP_UNITTEST,,}" == "true" ]] && rlRun "args=\"\$args --skip_unittests\"" 0 "Skip unit tests"
+    [[ "${CP2K_SKIP_REGTEST,,}" == "true" ]] && rlRun "args=\"\$args --skip_regtests\"" 0 "Skip regression tests"
     [[ "${CP2K_SMOKE_ONLY,,}" == "true" ]] && rlRun "args=\"\$args --smoketest\"" 0 "Set smoketest flag"
 
     if [[ "${CP2K_VARIANT}" != "serial" ]]; then
@@ -17,7 +19,7 @@ rlJournalStart
       rlRun "module load mpi/${CP2K_VARIANT}" 0 "Load MPI module: ${CP2K_VARIANT}"
       rlRun "export CP2K_STEM=$MPI_BIN/cp2k" 0 "Export CP2K_STEM"
       rlRun "args=\"\$args --mpiranks 2\"" 0 "Set MPI arguments"
-      rlRun "args=\"\$args local-${CP2K_VARIANT} psmp\"" 0 "Set run specific arguments"
+      rlRun "args=\"\$args local_${CP2K_VARIANT} psmp\"" 0 "Set run specific arguments"
     else
       rlRun "export CP2K_STEM=/usr/bin/cp2k" 0 "Export CP2K_STEM"
       rlRun "args=\"\$args local ssmp\"" 0 "Set run specific arguments"
