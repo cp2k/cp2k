b279b6b5e665c770a08400e4d41408713a49bccd
Matthias Krack <matthias.krack@psi.ch>
2024-04-23 14:21:14 +0200

    Intel oneapi-hpckit 2023.2.1 -> 2024.0.1

--- tools/docker/generate_dockerfiles.py
+++ tools/docker/generate_dockerfiles.py
@@ -451,7 +451,7 @@ RUN ln -sf /usr/bin/gcc-{gcc_version}      /usr/local/bin/gcc  && \
 # ======================================================================================
 def toolchain_intel() -> str:
     return rf"""
-FROM intel/oneapi-hpckit:2023.2.1-devel-ubuntu22.04
+FROM intel/oneapi-hpckit:2024.0.1-devel-ubuntu22.04
 
 # Workaround expired key.
 RUN curl -sS https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor > /usr/share/keyrings/intel-oneapi-archive-keyring.gpg
