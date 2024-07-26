7a0d0c50df2ec279556d7ced62f96143659ed187
Matthias Krack <matthias.krack@psi.ch>
2024-07-26 07:46:47 +0200

    Add -march compiler flag for CPUID recognition (#3570)

--- tools/toolchain/scripts/stage0/setup_buildtools.sh
+++ tools/toolchain/scripts/stage0/setup_buildtools.sh
@@ -25,19 +25,12 @@ if [ "${with_intel}" != "__DONTUSE__" ]; then
   CFLAGS="-O2 -fPIC -fp-model=precise -funroll-loops -g -qopenmp -qopenmp-simd -traceback"
   if [ "${TARGET_CPU}" = "native" ]; then
     CFLAGS="${CFLAGS} -xHost"
-  elif [ "${TARGET_CPU}" = "generic" ]; then
-    CFLAGS="${CFLAGS} -mtune=${TARGET_CPU}"
   else
     CFLAGS="${CFLAGS} -mtune=${TARGET_CPU}"
   fi
   FFLAGS="${CFLAGS}"
 else
-  CFLAGS="-O2 -fPIC -fno-omit-frame-pointer -fopenmp -g"
-  if [ "${TARGET_CPU}" = "generic" ]; then
-    CFLAGS="${CFLAGS} -mtune=generic ${TSANFLAGS}"
-  else
-    CFLAGS="${CFLAGS} -mtune=${TARGET_CPU} ${TSANFLAGS}"
-  fi
+  CFLAGS="-O2 -fPIC -fno-omit-frame-pointer -fopenmp -g -mtune=${TARGET_CPU} ${TSANFLAGS}"
   FFLAGS="${CFLAGS} -fbacktrace"
 fi
 CXXFLAGS="${CFLAGS}"
