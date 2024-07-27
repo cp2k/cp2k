3466b21136978c400e7c8599ae5da7b9699c4e82
Hans Pabst <hans.pabst@intel.com>
2024-07-27 18:08:08 +0200

    Updated LIBXSMM.

--- tools/toolchain/scripts/stage4/install_libxsmm.sh
+++ tools/toolchain/scripts/stage4/install_libxsmm.sh
@@ -6,9 +6,9 @@
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
-# https://github.com/libxsmm/libxsmm/archive/d009b33e8742a93c9e1549323587fb6197451294.tar.gz
-libxsmm_ver="d009b33e8742a93c9e1549323587fb6197451294"
-libxsmm_sha256="1990d1b7b6aa9333c26ce1e6c6395a0f206c5bcc0648432e5782f00e215db36c"
+# https://github.com/libxsmm/libxsmm/archive/50982e992eefedd44ef9395f946dc73595b8579d.tar.gz
+libxsmm_ver="50982e992eefedd44ef9395f946dc73595b8579d"
+libxsmm_sha256="941579a9a6f9982d6ccbb17d8c42445083ae743f75b0cce9a3adca3a698a2099"
 source "${SCRIPT_DIR}"/common_vars.sh
 source "${SCRIPT_DIR}"/tool_kit.sh
 source "${SCRIPT_DIR}"/signal_trap.sh
