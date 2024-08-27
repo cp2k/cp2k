69522bc6860b0a4938b6b7299fd9236b83f1a3a9
Hans Pabst <hans.pabst@intel.com>
2024-08-27 21:58:36 +0200

    Revised m_cpuid/m_cpuid_name (update+arm), m_cpuid_static shall be updated (arm).

--- src/base/machine_cpuid.h
+++ src/base/machine_cpuid.h
@@ -6,7 +6,15 @@
 /*----------------------------------------------------------------------------*/
 
 #define CP_MACHINE_CPU_GENERIC 0
+
 #define CP_MACHINE_X86_SSE4 1000
 #define CP_MACHINE_X86_AVX 1001
 #define CP_MACHINE_X86_AVX2 1002
 #define CP_MACHINE_X86_AVX512 1003
+
+#define CP_MACHINE_ARM_ARCH64 2000
+#define CP_MACHINE_ARM_SVE128 2100
+#define CP_MACHINE_ARM_SVE256 2200
+#define CP_MACHINE_ARM_SVE512 2300
+
+#define CP_MACHINE_UNKNOWN 3000
