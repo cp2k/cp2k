81d3de907bfd389d7b5be5aa1dba7060cef2317b
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2024-09-11 15:34:29 +0200

    make pretty

--- tools/toolchain/scripts/stage8/install_spfft.sh
+++ tools/toolchain/scripts/stage8/install_spfft.sh
@@ -51,7 +51,7 @@ case "${with_spfft}" in
         -DCMAKE_VERBOSE_MAKEFILE=ON \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-	-DSPFFT_OMP=ON \
+        -DSPFFT_OMP=ON \
         -DSPFFT_MPI=ON \
         -DSPFFT_STATIC=ON \
         -DSPFFT_FORTRAN=ON \
@@ -76,7 +76,7 @@ case "${with_spfft}" in
           -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
           -DSPFFT_OMP=ON \
           -DSPFFT_MPI=ON \
-	  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+          -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
           -DSPFFT_STATIC=ON \
           -DSPFFT_FORTRAN=ON \
           -DSPFFT_INSTALL=ON \
