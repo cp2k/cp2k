92cea75222269ceb0d9fb9904b25f3c2463911c1
Johann Pototschnig <j.pototschnig@hzdr.de>
2024-09-19 16:51:31 +0200

    tblite interface  starting

---
+++ cmake/modules/Findtblite.cmake
@@ -0,0 +1,45 @@
+#!-------------------------------------------------------------------------------------------------!
+#!   CP2K: A general program to perform molecular dynamics simulations                             !
+#!   Copyright 2000-2024 CP2K developers group <https://cp2k.org>                                  !
+#!                                                                                                 !
+#!   SPDX-License-Identifier: GPL-2.0-or-later                                                     !
+#!-------------------------------------------------------------------------------------------------!
+
+include(FindPackageHandleStandardArgs)
+find_package(PkgConfig REQUIRED)
+include(cp2k_utils)
+
+cp2k_set_default_paths(TBLITE "tblite")
+
+if(PKG_CONFIG_FOUND)
+	pkg_check_modules(CP2K_TBLITE QUIET IMPORTED_TARGET GLOBAL tblite)
+endif()
+
+if(NOT ${CP2K_TBLITE_FOUND})
+  cp2k_find_libraries(TBLITE tblite)
+endif()
+
+if(NOT CP2K_TBLITE_INCLUDE_DIRS)
+  cp2k_include_dirs(TBLITE "tblite.h")
+endif()
+
+if(CP2K_TBLITE_INCLUDE_DIRS)
+  find_package_handle_standard_args(tblite DEFAULT_MSG CP2K_TBLITE_INCLUDE_DIRS
+                                    CP2K_TBLITE_LINK_LIBRARIES)
+else()
+  find_package_handle_standard_args(tblite DEFAULT_MSG
+                                    CP2K_TBLITE_LINK_LIBRARIES)
+endif()
+
+if(CP2K_TBLITE_FOUND)
+  if(NOT TARGET cp2k::tblite::tblite)
+    add_library(cp2k::tblite::tblite INTERFACE IMPORTED)
+  endif()
+  set_target_properties(
+    cp2k::tblite::tblite
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CP2K_TBLITE_INCLUDE_DIRS}"
+               INTERFACE_LINK_LIBRARIES "${CP2K_TBLITE_LINK_LIBRARIES}")
+endif()
+
+mark_as_advanced(CP2K_TBLITE_LINK_LIBRARIES CP2K_TBLITE_INCLUDE_DIRS
+                 CP2K_TBLITE_FOUND)
