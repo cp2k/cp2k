2093bbac0bf2c182631b77c12d8afd970711664a
Johann Pototschnig <j.pototschnig@hzdr.de>
2024-10-07 19:45:07 +0200

    trying to improve tblite build

---
+++ cmake/modules/Finds-dftd3.cmake
@@ -0,0 +1,44 @@
+#!-------------------------------------------------------------------------------------------------!
+#!   CP2K: A general program to perform molecular dynamics simulations                             !
+#!   Copyright 2000-2024 CP2K developers group <https://cp2k.org>                                  !
+#!                                                                                                 !
+#!   SPDX-License-Identifier: GPL-2.0-or-later                                                     !
+#!-------------------------------------------------------------------------------------------------!
+
+include(FindPackageHandleStandardArgs)
+find_package(PkgConfig REQUIRED)
+include(cp2k_utils)
+
+cp2k_set_default_paths(S-DFTD3 "s-dftd3")
+
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(S-DFTD3 QUIET IMPORTED_TARGET GLOBAL s-dftd3)
+endif()
+
+if(NOT S-DFTD3_FOUND)
+  cp2k_find_libraries(S-DFTD3 s-dftd3)
+endif()
+
+if(NOT S-DFTD3_INCLUDE_DIRS)
+  cp2k_include_dirs(S-DFTD3 "s-dftd3.h")
+endif()
+
+if(S-DFTD3_INCLUDE_DIRS)
+  find_package_handle_standard_args(s-dftd3 DEFAULT_MSG S-DFTD3_INCLUDE_DIRS
+                                    S-DFTD3_LINK_LIBRARIES)
+else()
+  find_package_handle_standard_args(s-dftd3 DEFAULT_MSG S-DFTD3_LINK_LIBRARIES)
+endif()
+
+if(S-DFTD3_FOUND)
+  if(NOT TARGET s-dftd3::s-dftd3)
+    add_library(s-dftd3::s-dftd3 INTERFACE IMPORTED)
+  endif()
+  get_filename_component(S-DFTD3_LINK_LIB "${S-DFTD3_LINK_LIBRARIES}" PATH)
+  set_target_properties(
+    s-dftd3::s-dftd3
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${S-DFTD3_INCLUDE_DIRS}"
+               INTERFACE_LINK_LIBRARIES "${S-DFTD3_LINK_LIBRARIES}")
+endif()
+
+mark_as_advanced(S-DFTD3_LINK_LIBRARIES S-DFTD3_INCLUDE_DIRS S-DFTD3_FOUND)
--- cmake/modules/Findtblite.cmake
+++ cmake/modules/Findtblite.cmake
@@ -12,88 +12,32 @@ include(cp2k_utils)
 cp2k_set_default_paths(TBLITE "tblite")
 
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(CP2K_TBLITE QUIET IMPORTED_TARGET GLOBAL tblite)
-  pkg_check_modules(CP2K_DFTD3 QUIET IMPORTED_TARGET GLOBAL s-dftd3)
-  pkg_check_modules(CP2K_TOML QUIET IMPORTED_TARGET GLOBAL toml-f)
+  pkg_check_modules(TBLITE QUIET IMPORTED_TARGET GLOBAL tblite)
 endif()
 
-if(NOT CP2K_TBLITE_FOUND)
+if(NOT TBLITE_FOUND)
   cp2k_find_libraries(TBLITE tblite)
 endif()
-if(NOT CP2K_DFTD3_FOUND)
-  cp2k_find_libraries(DFTD3 s-dftd3)
-endif()
-if(NOT CP2K_TOML_FOUND)
-  cp2k_find_libraries(TOML toml-f)
-endif()
 
-if(NOT CP2K_TBLITE_INCLUDE_DIRS)
+if(NOT TBLITE_INCLUDE_DIRS)
   cp2k_include_dirs(TBLITE "tblite.h")
 endif()
-if(NOT CP2K_DFTD3_INCLUDE_DIRS)
-  cp2k_include_dirs(TBLITE "s-dftd3.h;dftd3.h")
-endif()
 
-if(CP2K_TBLITE_INCLUDE_DIRS)
-  if(CP2K_DFTD3_INCLUDE_DIRS)
-    find_package_handle_standard_args(
-      tblite
-      DEFAULT_MSG
-      CP2K_TBLITE_INCLUDE_DIRS
-      CP2K_DFTD3_INCLUDE_DIRS
-      CP2K_TBLITE_LINK_LIBRARIES
-      CP2K_DFTD3_LINK_LIBRARIES
-      CP2K_TOML_LINK_LIBRARIES)
-  else()
-    find_package_handle_standard_args(
-      tblite DEFAULT_MSG CP2K_TBLITE_INCLUDE_DIRS CP2K_TBLITE_LINK_LIBRARIES
-      CP2K_DFTD3_LINK_LIBRARIES CP2K_TOML_LINK_LIBRARIES)
-  endif()
+if(TBLITE_INCLUDE_DIRS)
+  find_package_handle_standard_args(tblite DEFAULT_MSG TBLITE_INCLUDE_DIRS
+                                    TBLITE_LINK_LIBRARIES)
 else()
-  if(CP2K_DFTD3_INCLUDE_DIRS)
-    find_package_handle_standard_args(
-      tblite DEFAULT_MSG CP2K_DFTD3_INCLUDE_DIRS CP2K_TBLITE_LINK_LIBRARIES
-      CP2K_DFTD3_LINK_LIBRARIES CP2K_TOML_LINK_LIBRARIES)
-  else()
-    find_package_handle_standard_args(
-      tblite DEFAULT_MSG CP2K_TBLITE_LINK_LIBRARIES CP2K_DFTD3_LINK_LIBRARIES
-      CP2K_TOML_LINK_LIBRARIES)
-  endif()
+  find_package_handle_standard_args(tblite DEFAULT_MSG TBLITE_LINK_LIBRARIES)
 endif()
 
-if(CP2K_TBLITE_FOUND)
-  if(NOT TARGET cp2k::tblite::tblite)
-    add_library(cp2k::tblite::tblite INTERFACE IMPORTED)
-    if(NOT TARGET cp2k::tblite::s-dftd3)
-      add_library(cp2k::tblite::s-dftd3 INTERFACE IMPORTED)
-    endif()
-    if(NOT TARGET cp2k::tblite::toml-f)
-      add_library(cp2k::tblite::toml-f INTERFACE IMPORTED)
-    endif()
+if(TBLITE_FOUND)
+  if(NOT TARGET tblite::tblite)
+    add_library(tblite::tblite INTERFACE IMPORTED)
   endif()
-
   set_target_properties(
-    cp2k::tblite::tblite PROPERTIES INTERFACE_LINK_LIBRARIES
-                                    "${CP2K_TBLITE_LINK_LIBRARIES}")
-  set_target_properties(
-    cp2k::tblite::s-dftd3 PROPERTIES INTERFACE_LINK_LIBRARIES
-                                     "${CP2K_DFTD3_LINK_LIBRARIES}")
-  set_target_properties(
-    cp2k::tblite::toml-f PROPERTIES INTERFACE_LINK_LIBRARIES
-                                    "${CP2K_TOML_LINK_LIBRARIES}")
-
-  if(CP2K_TBLITE_INCLUDE_DIRS)
-    set_target_properties(
-      cp2k::tblite::tblite PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                      "${CP2K_TBLITE_INCLUDE_DIRS}")
-  endif()
-  if(CP2K_TBLITE_INCLUDE_DIRS)
-    set_target_properties(
-      cp2k::tblite::s-dftd3 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                       "${CP2K_DFTD3_INCLUDE_DIRS}")
-  endif()
+    tblite::tblite
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${TBLITE_INCLUDE_DIRS}"
+               INTERFACE_LINK_LIBRARIES "${TBLITE_LINK_LIBRARIES}")
 endif()
 
-mark_as_advanced(
-  CP2K_TOML_LINK_LIBRARIES CP2K_DFTD3_INCLUDE_DIRS CP2K_DFTD3_LINK_LIBRARIES
-  CP2K_TBLITE_INCLUDE_DIRS CP2K_TBLITE_LINK_LIBRARIES CP2K_TBLITE_FOUND)
+mark_as_advanced(TBLITE_LINK_LIBRARIES TBLITE_INCLUDE_DIRS TBLITE_FOUND)
