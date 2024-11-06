c909fa910068882ac33efef5ec4ef0511bdd1f26
Franz Pöschel <franz.poeschel@gmail.com>
2024-11-06 17:27:17 +0100

    Better clang-format

--- src/binding/openPMD/openPMD.cpp
+++ src/binding/openPMD/openPMD.cpp
@@ -2,36 +2,45 @@
 
 #include <openPMD/openPMD.hpp>
 
-extern "C" {
-struct openPMD_Series_s {
-  openPMD::Series m_series;
-};
+extern "C"
+{
+    struct openPMD_Series_s
+    {
+        openPMD::Series m_series;
+    };
 
-int openPMD_Series_create(char const *filename, openPMD_Access access,
-                          openPMD_Series *series) {
+    int openPMD_Series_create(
+        char const *filename, openPMD_Access access, openPMD_Series *series)
+    {
 
-  auto cxx_access = [&]() {
-    switch (access) {
-    case openPMD_Access_create:
-      return openPMD::Access::CREATE;
-    case openPMD_Access_read_only:
-      return openPMD::Access::READ_ONLY;
+        auto cxx_access = [&]() {
+            switch (access)
+            {
+            case openPMD_Access_create:
+                return openPMD::Access::CREATE;
+            case openPMD_Access_read_only:
+                return openPMD::Access::READ_ONLY;
+            }
+            // unreachable
+            return static_cast<openPMD::Access>(0);
+        }();
+        try
+        {
+            *series =
+                new openPMD_Series_s{openPMD::Series(filename, cxx_access)};
+        }
+        catch (...)
+        {
+            delete series;
+            return 1;
+        }
+        return 0;
     }
-    // unreachable
-    return static_cast<openPMD::Access>(0);
-  }();
-  try {
-    *series = new openPMD_Series_s{openPMD::Series(filename, cxx_access)};
-  } catch (...) {
-    delete series;
-    return 1;
-  }
-  return 0;
-}
 
-int openPMD_Series_close(openPMD_Series series) {
-  series->m_series.close();
-  delete series;
-  return 0;
-}
+    int openPMD_Series_close(openPMD_Series series)
+    {
+        series->m_series.close();
+        delete series;
+        return 0;
+    }
 }
--- src/binding/openPMD/openPMD.h
+++ src/binding/openPMD/openPMD.h
@@ -1,21 +1,27 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef enum { openPMD_Access_create, openPMD_Access_read_only } openPMD_Access;
+    typedef enum
+    {
+        openPMD_Access_create,
+        openPMD_Access_read_only
+    } openPMD_Access;
 
-struct openPMD_Series_s; // opaque
-typedef struct openPMD_Series_s *openPMD_Series;
+    struct openPMD_Series_s; // opaque
+    typedef struct openPMD_Series_s *openPMD_Series;
 
-int openPMD_Series_create(
-    // in
-    char const *filename, openPMD_Access access,
-    // out
-    openPMD_Series *series);
+    int openPMD_Series_create(
+        // in
+        char const *filename,
+        openPMD_Access access,
+        // out
+        openPMD_Series *series);
 
-int openPMD_Series_close(
-    // in
-    openPMD_Series series);
+    int openPMD_Series_close(
+        // in
+        openPMD_Series series);
 
 #ifdef __cplusplus
 }
