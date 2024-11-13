345a46a9add6c5c73738c35b8344fc2de26c3b0a
Franz Pöschel <franz.poeschel@gmail.com>
2024-11-13 12:08:14 +0100

    Cleanup

--- src/binding/openPMD/openPMD.cpp
+++ src/binding/openPMD/openPMD.cpp
@@ -2,27 +2,34 @@
 
 #include <openPMD/openPMD.hpp>
 
+namespace
+{
+auto access_c_to_cxx(openPMD_Access_create access) -> openPMD::Access
+{
+    switch (access)
+    {
+    case openPMD_Access_create:
+        return openPMD::Access::CREATE;
+    case openPMD_Access_read_only:
+        return openPMD::Access::READ_ONLY;
+    }
+    // unreachable
+    return static_cast<openPMD::Access>(0);
+}
+} // namespace
+
 extern "C"
 {
     int openPMD_Series_create(
-        char const *filename, openPMD_Access access, openPMD_Series *series_param)
+        char const *filename,
+        openPMD_Access access,
+        openPMD_Series *series_param)
     {
-        auto series = reinterpret_cast<openPMD::Series**>(series_param);
-        auto cxx_access = [&]() {
-            switch (access)
-            {
-            case openPMD_Access_create:
-                return openPMD::Access::CREATE;
-            case openPMD_Access_read_only:
-                return openPMD::Access::READ_ONLY;
-            }
-            // unreachable
-            return static_cast<openPMD::Access>(0);
-        }();
+        auto series = reinterpret_cast<openPMD::Series **>(series_param);
+        auto cxx_access = access_c_to_cxx(access);
         try
         {
-            *series =
-                new openPMD::Series(filename, cxx_access);
+            *series = new openPMD::Series(filename, cxx_access);
         }
         catch (...)
         {
@@ -34,7 +41,7 @@ extern "C"
 
     int openPMD_Series_close(openPMD_Series series_param)
     {
-        auto series = reinterpret_cast<openPMD::Series*>(series_param);
+        auto series = reinterpret_cast<openPMD::Series *>(series_param);
         series->close();
         delete series;
         return 0;
