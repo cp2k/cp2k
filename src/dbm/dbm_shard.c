aa20381fc87bf448e591504fac5dd4861602d2a3
Hans Pabst <hans.pabst@intel.com>
2025-02-05 20:27:19 +0100

    DBM: improved hashtable_insert and dbm_shard_lookup
    - DBM_LOOKUP_LINEAR for predictable linear access.
    - Enables more efficient lookup or vectorization.
    - Removed hashtable_mask from dbm_shard_t.

--- src/dbm/dbm_shard.c
+++ src/dbm/dbm_shard.c
@@ -14,6 +14,8 @@
 #include "dbm_hyperparams.h"
 #include "dbm_shard.h"
 
+#define DBM_LOOKUP_LINEAR 64
+
 /*******************************************************************************
  * \brief Internal routine for finding a power of two greater than given number.
  * \author Ole Schuett
@@ -51,7 +53,6 @@ static void hashtable_init(dbm_shard_t *shard) {
   // Choosing size as power of two allows to replace modulo with bitwise AND.
   shard->hashtable_size =
       next_power2(HASHTABLE_FACTOR * shard->nblocks_allocated);
-  shard->hashtable_mask = shard->hashtable_size - 1;
   shard->hashtable_prime = next_prime(shard->hashtable_size);
   shard->hashtable = calloc(shard->hashtable_size, sizeof(int));
   assert(shard->hashtable != NULL);
@@ -94,7 +95,6 @@ void dbm_shard_copy(dbm_shard_t *shard_a, const dbm_shard_t *shard_b) {
     assert(shard_a->hashtable != NULL);
   }
   shard_a->hashtable_size = shard_b->hashtable_size;
-  shard_a->hashtable_mask = shard_b->hashtable_mask;
   shard_a->hashtable_prime = shard_b->hashtable_prime;
 
   if (shard_a->data_allocated < shard_b->data_size) {
@@ -142,21 +142,35 @@ static inline unsigned int hash(const unsigned int row,
 }
 
 /*******************************************************************************
+ * \brief Internal routine for masking a slot in the hash-table.
+ * \author Hans Pabst
+ ******************************************************************************/
+static inline int hashtable_mask(const dbm_shard_t *shard) {
+  return shard->hashtable_size - 1;
+}
+
+/*******************************************************************************
  * \brief Private routine for inserting a block into a shard's hashtable.
  * \author Ole Schuett
  ******************************************************************************/
 static void hashtable_insert(dbm_shard_t *shard, const int block_idx) {
   assert(0 <= block_idx && block_idx < shard->nblocks);
   const dbm_block_t *blk = &shard->blocks[block_idx];
-  const int row = blk->row, col = blk->col;
-  int slot = (shard->hashtable_prime * hash(row, col)) & shard->hashtable_mask;
-  while (true) {
-    if (shard->hashtable[slot] == 0) {
-      shard->hashtable[slot] = block_idx + 1; // 1-based because 0 means empty
+  const unsigned int h = hash(blk->row, blk->col);
+  int slot = (shard->hashtable_prime * h) & hashtable_mask(shard);
+  for (;; slot = (slot + 1) & hashtable_mask(shard)) { // linear probing
+    int *hashtable = shard->hashtable + slot;
+    if ((slot + DBM_LOOKUP_LINEAR) <= hashtable_mask(shard)) {
+      for (int i = 0; i < DBM_LOOKUP_LINEAR; ++i) {
+        if (hashtable[i] == 0) {        // 0 means empty
+          hashtable[i] = block_idx + 1; // 1-based
+          return;
+        }
+      }
+    } else if (*hashtable == 0) { // 0 means empty
+      *hashtable = block_idx + 1; // 1-based
       return;
     }
-    // linear probing
-    slot = (slot + 1) & shard->hashtable_mask;
   }
 }
 
@@ -166,19 +180,32 @@ static void hashtable_insert(dbm_shard_t *shard, const int block_idx) {
  ******************************************************************************/
 dbm_block_t *dbm_shard_lookup(const dbm_shard_t *shard, const int row,
                               const int col) {
-  int slot = (shard->hashtable_prime * hash(row, col)) & shard->hashtable_mask;
-  while (true) {
-    const int block_idx = shard->hashtable[slot] - 1; // 1-based, 0 means empty.
-    if (block_idx < 0) {
-      return NULL; // block not found
+  int slot = (shard->hashtable_prime * hash(row, col)) & hashtable_mask(shard);
+  for (;; slot = (slot + 1) & hashtable_mask(shard)) { // linear probing
+    const int *hashtable = shard->hashtable + slot;
+    if ((slot + DBM_LOOKUP_LINEAR) <= hashtable_mask(shard)) {
+      for (int i = 0; i < DBM_LOOKUP_LINEAR; ++i) {
+        const int block_idx = hashtable[i];
+        if (block_idx == 0) { // 1-based, 0 means empty
+          return NULL;        // block not found
+        }
+        assert(0 < block_idx && block_idx <= shard->nblocks);
+        dbm_block_t *blk = &shard->blocks[block_idx - 1];
+        if (blk->row == row && blk->col == col) {
+          return blk;
+        }
+      }
+    } else {
+      const int block_idx = *hashtable;
+      if (block_idx == 0) { // 1-based, 0 means empty
+        return NULL;        // block not found
+      }
+      assert(0 < block_idx && block_idx <= shard->nblocks);
+      dbm_block_t *blk = &shard->blocks[block_idx - 1];
+      if (blk->row == row && blk->col == col) {
+        return blk;
+      }
     }
-    assert(0 <= block_idx && block_idx < shard->nblocks);
-    dbm_block_t *blk = &shard->blocks[block_idx];
-    if (blk->row == row && blk->col == col) {
-      return blk;
-    }
-    // linear probing
-    slot = (slot + 1) & shard->hashtable_mask;
   }
 }
 
@@ -210,7 +237,7 @@ dbm_block_t *dbm_shard_promise_new_block(dbm_shard_t *shard, const int row,
   new_block->col = col;
   new_block->offset = shard->data_promised;
   shard->data_promised += block_size;
-  // The data_size will be increase after the memory is allocated and zeroed.
+  // The data_size will be increased after the memory is allocated and zeroed.
   hashtable_insert(shard, new_block_idx);
   return new_block;
 }
