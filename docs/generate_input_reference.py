fb643211e1d94ac8f7bd422da2e86fc6a1ce1bc6
Ole Schütt <ole.schuett@cp2k.org>
2025-02-14 11:20:43 +0100

    Docs: Fix keywords with arbitrary number of values

--- docs/generate_input_reference.py
+++ docs/generate_input_reference.py
@@ -336,7 +336,7 @@ def render_keyword(
 
     # Use Sphinx's py:data directive to document keywords.
     output += [f"```{{py:data}}  {canonical_name}"]
-    n_var_brackets = f"[{n_var}]" if n_var > 1 else ""
+    n_var_brackets = f"[{n_var}]" if n_var > 1 else "[ ]" if n_var == -1 else ""
     if section_xref:
         output += [f":module: {section_xref}"]
     else:
