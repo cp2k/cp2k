3a79bc6a8242a76c167b0689764bee232d832309
Matthias Krack <matthias.krack@psi.ch>
2025-04-24 10:45:48 +0200

    Use build type RelWithDebInfo for SIRIUS

--- tools/toolchain/scripts/stage8/install_sirius.sh
+++ tools/toolchain/scripts/stage8/install_sirius.sh
@@ -146,6 +146,7 @@ case "$with_sirius" in
       CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${GSL_ROOT}:${SPGLIB_ROOT}:${LIBXC_ROOT}:${SpFFT_DIR}:${SpLA_DIR}:${COSTA_DIR}:${PUGIXML_ROOT}" cmake \
         -DCMAKE_INSTALL_PREFIX="${pkg_install_dir}" \
         -DCMAKE_INSTALL_LIBDIR="lib" \
+        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_CXX_FLAGS_RELEASE="${SIRIUS_OPT}" \
         -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="${SIRIUS_DBG}" \
         -DCMAKE_CXX_COMPILER="${MPICXX}" \
@@ -173,6 +174,7 @@ case "$with_sirius" in
         CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${GSL_ROOT}:${SPGLIB_ROOT}:${LIBXC_ROOT}:${SpFFT_DIR}:${SpLA_DIR}:${COSTA_DIR}" cmake \
           -DCMAKE_INSTALL_PREFIX=${pkg_install_dir}/cuda \
           -DCMAKE_INSTALL_LIBDIR="lib" \
+          -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
           -DCMAKE_CXX_FLAGS_RELEASE="${SIRIUS_OPT}" \
           -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="${SIRIUS_DBG}" \
           -DCMAKE_CUDA_FLAGS="-std=c++14 -allow-unsupported-compiler" \
