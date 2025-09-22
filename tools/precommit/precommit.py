ff7efb93267ab9eb7589cf0f5c32aa455096b277
Franz Pöschel <franz.poeschel@gmail.com>
2025-09-22 15:17:35 +0200

    Further formatting

--- tools/precommit/precommit.py
+++ tools/precommit/precommit.py
@@ -225,11 +225,12 @@ def process_file(fn: str, allow_modifications: bool) -> None:
         run_remote_tool("clangformat", fn)
 
     if re.match(r".*\.(cc|cpp|cxx|hcc|hpp|hxx)$", fn):
-        if fn.endswith("/torch_c_api.cpp"):
-            # Begrudgingly tolerated because PyTorch has no C API.
-            run_remote_tool("clangformat", fn)
-        elif fn.endswith("/ace_c_api.cpp"):
-            # same as PyTorch
+        if (
+            fn.endswith("/torch_c_api.cpp")
+            or fn.endswith("/ace_c_api.cpp")
+            or fn.endswith("/openPMD.cpp")
+        ):
+            # Begrudgingly tolerated because PyTorch/Ace/openPMD have no C API.
             run_remote_tool("clangformat", fn)
         else:
             raise Exception(f"C++ is not supported.")
