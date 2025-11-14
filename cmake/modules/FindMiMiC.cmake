1280caf188f2af0793bda386efdbb91a5cf8f2cc
Andrej Antalik <andrej.antalik@epfl.ch>
2025-11-14 16:32:20 +0100

    Applied prettify

--- cmake/modules/FindMiMiC.cmake
+++ cmake/modules/FindMiMiC.cmake
@@ -5,18 +5,17 @@
 #!   SPDX-License-Identifier: GPL-2.0-or-later                                                     !
 #!-------------------------------------------------------------------------------------------------!
 
-
 include(FindPackageHandleStandardArgs)
 include(cp2k_utils)
 
-#! Use PkgConfig to look for modules named mclf and mcl, LOOK for .pc files
+# ! Use PkgConfig to look for modules named mclf and mcl, LOOK for .pc files
 find_package(PkgConfig REQUIRED)
 
 if(PKG_CONFIG_FOUND)
   pkg_check_modules(CP2K_MIMIC IMPORTED_TARGET GLOBAL mclf mcl)
 endif()
 
-#! Using CP2K_UTILS
+# ! Using CP2K_UTILS
 if(NOT CP2K_MIMIC_FOUND)
   cp2k_set_default_paths(MIMIC "MiMiC")
   cp2k_find_libraries(MIMIC mclf)
@@ -30,7 +29,8 @@ if(NOT CP2K_MIMIC_FOUND)
 endif()
 
 if(CP2K_MIMIC_FOUND)
-  set(CP2K_MIMIC_LINK_LIBRARIES "${CP2K_MIMIC_LIBRARIES};${CP2K_MIMICc_LIBRARIES}")
+  set(CP2K_MIMIC_LINK_LIBRARIES
+      "${CP2K_MIMIC_LIBRARIES};${CP2K_MIMICc_LIBRARIES}")
 endif()
 
 if(NOT CP2K_MIMIC_INCLUDE_DIRS)
@@ -42,10 +42,12 @@ if(NOT CP2K_MIMIC_INCLUDE_DIRS)
 endif()
 
 if(CP2K_MIMIC_INCLUDE_DIRS)
-  find_package_handle_standard_args(MiMiC DEFAULT_MSG CP2K_MIMIC_FOUND CP2K_MIMIC_LINK_LIBRARIES
-                                                      CP2K_MIMIC_INCLUDE_DIRS)
+  find_package_handle_standard_args(
+    MiMiC DEFAULT_MSG CP2K_MIMIC_FOUND CP2K_MIMIC_LINK_LIBRARIES
+    CP2K_MIMIC_INCLUDE_DIRS)
 else()
-  find_package_handle_standard_args(MiMiC DEFAULT_MSG CP2K_MIMIC_FOUND CP2K_MIMIC_LINK_LIBRARIES)
+  find_package_handle_standard_args(MiMiC DEFAULT_MSG CP2K_MIMIC_FOUND
+                                    CP2K_MIMIC_LINK_LIBRARIES)
 endif()
 
 if(CP2K_MIMIC_FOUND)
@@ -56,19 +58,13 @@ if(CP2K_MIMIC_FOUND)
 
   if(CP2K_MIMIC_INCLUDE_DIRS)
     set_target_properties(
-      CP2K::MIMIC::mclf 
-      CP2K::MIMIC::mcl 
-      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CP2K_MIMIC_INCLUDE_DIRS}"
-    )
+      CP2K::MIMIC::mclf CP2K::MIMIC::mcl
+      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CP2K_MIMIC_INCLUDE_DIRS}")
   endif()
-  target_link_libraries(
-    CP2K::MIMIC::mclf
-    INTERFACE "${CP2K_MIMIC_LINK_LIBRARIES}"
-  )
-  target_link_libraries(
-    CP2K::MIMIC::mcl
-    INTERFACE "${CP2K_MIMIC_LINK_LIBRARIES}"
-  )
+  target_link_libraries(CP2K::MIMIC::mclf
+                        INTERFACE "${CP2K_MIMIC_LINK_LIBRARIES}")
+  target_link_libraries(CP2K::MIMIC::mcl
+                        INTERFACE "${CP2K_MIMIC_LINK_LIBRARIES}")
 endif()
 
 mark_as_advanced(CP2K_MIMIC_FOUND CP2K_MIMIC_LINK_LIBRARIES
