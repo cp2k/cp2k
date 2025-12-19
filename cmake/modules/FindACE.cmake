388ddf72599c7a5d363f6c8a0be55b81080b00aa
Taillefumier Mathieu <29380261+mtaillefumier@users.noreply.github.com>
2025-12-19 11:31:12 +0100

    Fix typo in vdw functional name and various CMake issues

--- cmake/modules/FindACE.cmake
+++ cmake/modules/FindACE.cmake
@@ -12,42 +12,25 @@ cp2k_set_default_paths(ACE "ace")
 
 cp2k_include_dirs(
   ACE "ace/ace_couplings.h;ace-evaluator/ace_types.h;yaml-cpp/yaml.h")
-find_package_handle_standard_args(ACE DEFAULT_MSG CP2K_ACE_INCLUDE_DIRS)
-
 cp2k_find_libraries(ACE "pace")
-find_package_handle_standard_args(ACE DEFAULT_MSG CP2K_ACE_LINK_LIBRARIES)
-
 cp2k_find_libraries(ACE_YAML "yaml-cpp-pace")
-find_package_handle_standard_args(ACE DEFAULT_MSG CP2K_ACE_YAML_LINK_LIBRARIES)
-
 cp2k_find_libraries(ACE_CNPY "cnpy")
-find_package_handle_standard_args(ACE DEFAULT_MSG CP2K_ACE_CNPY_LINK_LIBRARIES)
+
+find_package_handle_standard_args(
+  ACE DEFAULT_MSG CP2K_ACE_LINK_LIBRARIES CP2K_ACE_CNPY_LINK_LIBRARIES
+  CP2K_ACE_INCLUDE_DIRS CP2K_ACE_YAML_LINK_LIBRARIES)
 
 if(CP2K_ACE_FOUND)
-  if(NOT TARGET ACE::pace)
-    add_library(ACE::pace INTERFACE IMPORTED)
+  if(NOT TARGET cp2k::ACE)
+    add_library(cp2k::ACE INTERFACE IMPORTED)
   endif()
   set_target_properties(
-    ACE::pace
-    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CP2K_ACE_INCLUDE_DIRS}"
-               INTERFACE_LINK_LIBRARIES "${CP2K_ACE_LINK_LIBRARIES}")
-endif()
-
-if(CP2K_ACE_YAML_FOUND)
-  if(NOT TARGET ACE::yaml-cpp-pace)
-    add_library(ACE::yaml-cpp-pace INTERFACE IMPORTED)
-  endif()
-  set_target_properties(
-    ACE::yaml-cpp-pace PROPERTIES INTERFACE_LINK_LIBRARIES
-                                  "${CP2K_ACE_YAML_LINK_LIBRARIES}")
-endif()
-
-if(CP2K_ACE_CNPY_FOUND)
-  if(NOT TARGET ACE::cnpy)
-    add_library(ACE::cnpy INTERFACE IMPORTED)
-  endif()
-  set_target_properties(ACE::cnpy PROPERTIES INTERFACE_LINK_LIBRARIES
-                                             "${CP2K_ACE_CNPY_LINK_LIBRARIES}")
+    cp2k::ACE
+    PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${CP2K_ACE_INCLUDE_DIRS}"
+      INTERFACE_LINK_LIBRARIES
+      "${CP2K_ACE_LINK_LIBRARIES};${CP2K_ACE_YAML_LINK_LIBRARIES};${CP2K_ACE_CNPY_LINK_LIBRARIES}"
+  )
 endif()
 
 mark_as_advanced(CP2K_ACE_FOUND CP2K_ACE_INCLUDE_DIRS CP2K_ACE_LINK_LIBRARIES
