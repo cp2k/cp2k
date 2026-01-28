35340893ddcec84790bc59a143881682b93b4776
Max Graml <78024843+MGraml@users.noreply.github.com>
2026-01-28 17:47:31 +0100

    GW: performance improvements

--- src/dbm/dbm_multiply_comm.c
+++ src/dbm/dbm_multiply_comm.c
@@ -68,6 +68,16 @@ typedef struct {
 } plan_t;
 
 /*******************************************************************************
+ * \brief Private routine for calculating tick indices in pack plans.
+ * \author Maximilian Graml
+ ******************************************************************************/
+static inline unsigned long long calculate_tick_index(int sum_index,
+                                                      int nticks) {
+  // 1021 is used as a random prime to scramble the index
+  return ((unsigned long long)sum_index * 1021ULL) % (unsigned long long)nticks;
+}
+
+/*******************************************************************************
  * \brief Private routine for planing packs.
  * \author Ole Schuett
  ******************************************************************************/
@@ -94,8 +104,8 @@ static void create_pack_plans(const bool trans_matrix, const bool trans_dist,
       for (int iblock = 0; iblock < shard->nblocks; iblock++) {
         const dbm_block_t *blk = &shard->blocks[iblock];
         const int sum_index = (trans_matrix) ? blk->row : blk->col;
-        const int itick = (1021 * sum_index) % nticks; // 1021 = a random prime
-        const int ipack = itick / dist_ticks->nranks;
+        unsigned long long itick64 = calculate_tick_index(sum_index, nticks);
+        const int ipack = itick64 / dist_ticks->nranks;
         nblks_mythread[ipack]++;
       }
     }
@@ -124,11 +134,11 @@ static void create_pack_plans(const bool trans_matrix, const bool trans_dist,
         const dbm_block_t *blk = &shard->blocks[iblock];
         const int free_index = (trans_matrix) ? blk->col : blk->row;
         const int sum_index = (trans_matrix) ? blk->row : blk->col;
-        const int itick = (1021 * sum_index) % nticks; // Same mapping as above.
-        const int ipack = itick / dist_ticks->nranks;
+        unsigned long long itick64 = calculate_tick_index(sum_index, nticks);
+        const int ipack = itick64 / dist_ticks->nranks;
         // Compute rank to which this block should be sent.
         const int coord_free_idx = dist_indices->index2coord[free_index];
-        const int coord_sum_idx = itick % dist_ticks->nranks;
+        const int coord_sum_idx = itick64 % dist_ticks->nranks;
         const int coords[2] = {(trans_dist) ? coord_sum_idx : coord_free_idx,
                                (trans_dist) ? coord_free_idx : coord_sum_idx};
         const int rank = cp_mpi_cart_rank(comm, coords);
