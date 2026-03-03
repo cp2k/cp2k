11a29d83da09e2e9b31cb6ea51a65d501193775b
Matthias Krack <matthias.krack@psi.ch>
2026-03-03 23:14:51 +0100

    Disable pexsi with pdbg

--- tools/docker/generate_dockerfiles.py
+++ tools/docker/generate_dockerfiles.py
@@ -56,7 +56,7 @@ def main() -> None:
     # Spack/CMake based testers
 
     with OutputFile(f"Dockerfile.test_spack_pdbg", args.check) as f:
-        f.write(install_cp2k_spack("pdbg", mpi_mode="mpich", feature_flags=""))
+        f.write(install_cp2k_spack("pdbg", mpi_mode="mpich", feature_flags="-df pexsi"))
 
     with OutputFile(f"Dockerfile.test_spack_psmp", args.check) as f:
         f.write(
