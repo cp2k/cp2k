2ed09c55f51f6910017bed1c1075c4c9f6587057
SY Wang <uwsy1059@qq.com>
2026-03-24 16:59:43 +0800

    Fix test-farm failure of RPM build (#4987)

--- tools/fedora/tests/do_regtest.sh
+++ tools/fedora/tests/do_regtest.sh
@@ -9,7 +9,10 @@ rlJournalStart
 
     rlRun "pushd tests" 0 "cd to cp2k tests folder"
 
-    rlRun "args=\"--maxtasks $(nproc) --ompthreads 2\"" 0 "Set base arguments"
+    workdir="/tmp/cp2k-regtest-${CP2K_VARIANT}"
+    rlRun "rm -rf ${workdir}" 0 "Clean previous workdir"
+    rlRun "args=\"--workbasedir ${workdir} --maxtasks $(nproc) --ompthreads 2\"" 0 "Set base arguments"
+
     [[ "${CP2K_SKIP_UNITTEST,,}" == "true" ]] && rlRun "args=\"\$args --skip_unittests\"" 0 "Skip unit tests"
     [[ "${CP2K_SKIP_REGTEST,,}" == "true" ]] && rlRun "args=\"\$args --skip_regtests\"" 0 "Skip regression tests"
     [[ "${CP2K_SMOKE_ONLY,,}" == "true" ]] && rlRun "args=\"\$args --smoketest\"" 0 "Set smoketest flag"
@@ -22,7 +25,21 @@ rlJournalStart
     else
       rlRun "args=\"\$args /usr/bin ssmp\"" 0 "Set run specific arguments"
     fi
+
+    if [[ "${CP2K_VARIANT}" != "serial" ]]; then
+      rlRun "ls ${MPI_BIN}/cp2k.psmp" 0 "Verify CP2K MPI binary exists"
+    else
+      rlRun "ls /usr/bin/cp2k.ssmp" 0 "Verify CP2K serial binary exists"
+    fi
+
+
     rlRun "./do_regtest.py $args" 0 "Run regression tests"
+
+    if [[ -d "${workdir}" ]]; then
+      for file in ${workdir}/TEST-*/status.txt regtest-${CP2K_VARIANT}-status.txt; do
+        rlFileSubmit $file
+      done
+    fi
 
     rlRun "popd"
   rlPhaseEnd
