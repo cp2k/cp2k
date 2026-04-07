eb3ce9a72fd1f1616a4b235fa4c750dd285a552c
Franz Pöschel <franz.poeschel@gmail.com>
2026-04-07 11:40:52 +0200

    Formatting

--- src/xc/xc_gauxc_c.c
+++ src/xc/xc_gauxc_c.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 
-void print_c_string(const char* c_message) {
-    if (c_message != NULL) {
-        printf("%s", c_message);
-    }
+void print_c_string(const char *c_message) {
+  if (c_message != NULL) {
+    printf("%s", c_message);
+  }
 }
