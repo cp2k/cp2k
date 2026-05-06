6864774f09b8badf3151b624d42614a7472afb9d
ritama@1996 <karritama@gmail..com>
2026-05-06 10:53:48 +0200

    ACE: input keyword is set

--- tools/toolchain/scripts/stage7/install_stage7.sh
+++ tools/toolchain/scripts/stage7/install_stage7.sh
@@ -3,7 +3,7 @@
 # TODO: Review and if possible fix shellcheck errors.
 # shellcheck disable=all
 
-./scripts/stage7/install_hdf5.sh
+#./scripts/stage7/install_hdf5.sh
 ./scripts/stage7/install_libvdwxc.sh
 ./scripts/stage7/install_spglib.sh
 ./scripts/stage7/install_libvori.sh
