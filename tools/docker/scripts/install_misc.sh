e85f8ecd600a8e78e5298e786bc04aee40ba5d15
Ole Schütt <ole.schuett@cp2k.org>
2026-05-12 11:07:31 +0200

    Docker: Reduce number of pinned python packages

--- tools/docker/scripts/install_misc.sh
+++ tools/docker/scripts/install_misc.sh
@@ -26,7 +26,12 @@ python3 -m venv /opt/venv
 export PATH="/opt/venv/bin:$PATH"
 
 # install python packages
-pip3 install -r pao-ml-requirements.txt
+pip3 install -r pao-ml-requirements.txt \
+  numpy==2.3.5 \
+  matplotlib==3.10.7 \
+  requests==2.33.0 \
+  types-requests==2.32.4.20250913 \
+  mypy==1.19.0
 
 # download inputs for minimax_to_fortran_source.py
 wget -q https://www.cp2k.org/static/downloads/1_xData.zip
