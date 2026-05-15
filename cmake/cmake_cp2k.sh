7f57f22fd5db490a0d4d6a670dab28fd0b6aa565
Ole Schütt <ole.schuett@cp2k.org>
2026-05-15 20:01:07 +0200

    Docker: Disable LibFCI for toolchain cuda builds (#5207)

--- cmake/cmake_cp2k.sh
+++ cmake/cmake_cp2k.sh
@@ -169,6 +169,7 @@ elif [[ "${PROFILE}" == "toolchain_cuda_"* ]] && [[ "${VERSION}" == "psmp" ]]; t
     -DCP2K_USE_VORI=OFF \
     -DCP2K_USE_TREXIO=OFF \
     -DCP2K_USE_MIMIC=OFF \
+    -DCP2K_USE_LIBFCI=OFF \
     -Werror=dev \
     .. |& tee ./cmake.log
   CMAKE_EXIT_CODE=$?
