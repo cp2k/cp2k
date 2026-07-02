6193dd13ebe3f2dee5efaff18c7747c2018e4c93
Growl <uwsy1059@qq.com>
2026-07-02 20:48:39 +0800

    Docker: Disable GauXC in generic toolchain tester

--- tools/docker/scripts/cmake_cp2k.sh
+++ tools/docker/scripts/cmake_cp2k.sh
@@ -173,6 +173,7 @@ elif [[ "${PROFILE}" == "toolchain_generic" ]] && [[ "${VERSION}" == "psmp" ]]; 
     -DCP2K_USE_PEXSI=OFF \
     -DCP2K_USE_DEEPMD=OFF \
     -DCP2K_USE_OPENPMD=OFF \
+    -DCP2K_USE_GAUXC=OFF \
     -Werror=dev \
     .. |& tee ./cmake.log
   CMAKE_EXIT_CODE=$?
