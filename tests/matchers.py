678364bc47497351bf4295df58ee914d8434cc09
Ziwei Chai <ziwei.chai@chem.uzh.ch>
2026-07-31 02:53:37 +0800

    Implement tensorial DFT+U+J and self consistent MTLR (#5631)
    Co-authored-by: Ole Schütt <ole@schuett.name>
    Co-authored-by: SY Wang <uwsy1059@qq.com>

--- tests/matchers.py
+++ tests/matchers.py
@@ -353,4 +353,7 @@ registry["E_RIRS_LUMO"] = GenericMatcher(r"G0W0 conduction band minimum", col=6)
 # Floquet Calculations
 registry["Quasienergy"] = GenericMatcher(r"  4", col=2)
 registry["Floquet_DOS"] = GenericMatcher(r"-1.690", col=2)
+
+# MTLR Calculations
+registry["MTLR_U_MINUS_J"] = GenericMatcher(r"U_MINUS_J [eV]", col=4)
 # EOF
