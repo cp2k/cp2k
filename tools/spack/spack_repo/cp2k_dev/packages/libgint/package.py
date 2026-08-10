42db5d27261ca9f0321e9614c26ca255a540a683
Matthias Krack <matthias.krack@psi.ch>
2026-08-10 17:40:52 +0200

    Revise spack build of libgint (#5702)

--- tools/spack/spack_repo/cp2k_dev/packages/libgint/package.py
+++ tools/spack/spack_repo/cp2k_dev/packages/libgint/package.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
 
 from spack.package import *
 
 
-class Libgint(MakefilePackage):
+class Libgint(MakefilePackage, CudaPackage):
     """libGint: GPU library for four-centre two-electron integrals and
     Hartree–Fock exchange calculations."""
 
@@ -19,18 +20,35 @@ class Libgint(MakefilePackage):
 
     license("MIT", checked_by="mkrack")
 
+    version("main", branch="main")
     version(
         "release_v1",
         url="https://github.com/marcelloPuligheddu/libGint/archive/refs/tags/release_v1.tar.gz",
         sha256="cc0dfeb6022ebfe0c3028a131045ff49b4c1005ad9cb77c5736fb3dac045b192",
     )
 
-    depends_on("cuda")
+    variant(
+        "cuda_arch",
+        values=CudaPackage.cuda_arch_values,
+        default="70",
+        multi=False,
+        description="CUDA architecture",
+    )
+
+    conflicts("~cuda")
+
+    depends_on("mpi")
+
+    def build(self, spec, prefix):
+        arch = spec.variants["cuda_arch"].value
+        make(f"ARCH_NUM={arch}")
 
     def install(self, spec, prefix):
-        make("install", "PREFIX={0}".format(prefix))
-        install_tree("src", prefix.include)
+        mkdirp(prefix.lib)
+        mkdirp(prefix.include)
+        install("libcp2kGint.a", prefix.lib)
+        install("libgint.mod", prefix.include)
 
     @property
     def libs(self):
-        return find_libraries(["libcp2kGint"], root=self.prefix, recursive=True, shared=False)
+        return find_libraries(["cp2kGint"], root=self.prefix, recursive=True, shared=False)
