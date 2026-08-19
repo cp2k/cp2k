de46fc4d82038b9135ddf5a54b6795a29b80cca4
Matthias Krack <matthias.krack@psi.ch>
2026-08-19 12:03:58 +0200

    Try recent libfabric for Cray CI tester

--- make_cp2k.sh
+++ make_cp2k.sh
@@ -1279,7 +1279,6 @@ if [[ ! -f "${SPACK_BUILD_PATH}/BUILD_DEPENDENCIES_COMPLETED" ]]; then
     sed -E \
       -e '/\s*#\s*-\s+"netmod=ofi"/ s/#/ /' \
       -e 's/~xpmem/+xpmem/' \
-      -e 's/"libfabric@[.0-9]*"/"libfabric@1.22.0"/' \
       -i "${CP2K_CONFIG_FILE}"
   fi
 
