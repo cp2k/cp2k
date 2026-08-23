54a22f7eb6260821a05350cc76d8b4a0e2c3d975
Thomas D. Kuehne <tkuehne@cp2k.org>
2026-08-23 22:36:48 +0200

    OT: Make lattice FFT AUTO cost aware

--- tests/matchers.py
+++ tests/matchers.py
@@ -84,6 +84,8 @@ registry["OT_kpoint_ref_refresh"] = TextPresenceMatcher(
 registry["OT_added_mos_auto_grow"] = TextPresenceMatcher(
     "K-point ADDED_MOS AUTO: growing virtual-space buffer"
 )
+registry["OT_lattice_fft_selected"] = TextPresenceMatcher("selected: T")
+registry["OT_lattice_fft_fallback"] = TextPresenceMatcher("selected: F")
 registry["Kubo_sigma_iso"] = GenericMatcher(r"KUBO_TRANSPORT| sigma_iso[S/cm]", col=3)
 registry["Kubo_sigma_iso_2d"] = GenericMatcher(r"KUBO_TRANSPORT| sigma_iso[S]", col=3)
 registry["Kubo_sigma_iso_1d"] = GenericMatcher(r"KUBO_TRANSPORT| sigma_iso[S*m]", col=3)
