1baa07df99843d2fe25a6259994d00165089000e
Dynamics of Condensed Matter <30792324+DCM-Uni-Paderborn@users.noreply.github.com>
2026-08-26 14:27:11 +0200

    Write effective charges to additional GPW cube files (#5718)
    Co-authored-by: Thomas D. Kuehne <tkuehne@cp2k.org>

--- tests/matchers.py
+++ tests/matchers.py
@@ -29,6 +29,12 @@ registry["E_total"] = GenericMatcher(r"Total energy:", col=3)
 registry["COMMUTATOR_HR_X"] = GenericMatcher(r"COMMUTATOR_HR| CheckSum X =", col=5)
 registry["COMMUTATOR_HR_Y"] = GenericMatcher(r"COMMUTATOR_HR| CheckSum Y =", col=5)
 registry["COMMUTATOR_HR_Z"] = GenericMatcher(r"COMMUTATOR_HR| CheckSum Z =", col=5)
+registry["Cube_Si_effective_charge"] = GenericMatcher(
+    r"^\s*14\s+([-+0-9.EeDd]+)\s+", col=1, regex=True, first=True
+)
+registry["Cube_O_effective_charge"] = GenericMatcher(
+    r"^\s*8\s+([-+0-9.EeDd]+)\s+", col=1, regex=True, first=True
+)
 
 registry["M002"] = GenericMatcher(r"MD| Potential energy", col=5)
 registry["M003"] = GenericMatcher(r"Total energy [eV]:", col=4)
