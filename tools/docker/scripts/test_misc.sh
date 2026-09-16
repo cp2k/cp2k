78f330873b773e164acce4f3ded8d699f6685c2b
Thomas D. Kuehne <tkuehne@cp2k.org>
2026-09-16 18:44:45 +0200

    Regtesting: Respect CPU affinity when scheduling workers

--- tools/docker/scripts/test_misc.sh
+++ tools/docker/scripts/test_misc.sh
@@ -31,6 +31,7 @@ unzip -q -d ./tools/minimax_tools/1_xData 1_xData.zip
 run_test ./tools/precommit/format_fortran_test.py
 run_test ./tools/minimax_tools/minimax_to_fortran_source.py --check
 run_test ./tools/docker/generate_dockerfiles.py --check
+run_test python3 -m unittest discover -s ./tests -p 'test_do_regtest.py'
 
 # Test pao-ml training.
 # Passing example.pao twice to have enough samples to split off 20% for validation.
@@ -59,6 +60,7 @@ run_test mypy --strict ./tools/docker/scripts/plot_performance.py
 run_test mypy --strict ./tools/conventions/redirect_gfortran_output.py
 run_test mypy --strict ./tools/conventions/analyze_gfortran_ast.py
 run_test mypy --strict ./tests/do_regtest.py
+run_test mypy --strict ./tests/test_do_regtest.py
 run_test mypy --strict ./docs/generate_input_reference.py
 run_test mypy --strict ./docs/fix_github_links.py
 run_test mypy --strict ./tools/vibronic_spec/
