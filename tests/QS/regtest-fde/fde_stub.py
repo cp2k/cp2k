73e1e542fa47672ba57e4e5e028f0bd39120ae36
Lukas Schreder <lukas@schreder.xyz>
2026-09-18 20:49:02 +0200

    make pretty

--- tests/QS/regtest-fde/fde_stub.py
+++ tests/QS/regtest-fde/fde_stub.py
@@ -13,12 +13,14 @@ E_NUC = 0.0
 
 with open(GRID_FILE) as fh:
     npoints = int(fh.readline().split()[0])
-    coords = [tuple(float(v) for v in fh.readline().split()[:3]) for _ in range(npoints)]
+    coords = [
+        tuple(float(v) for v in fh.readline().split()[:3]) for _ in range(npoints)
+    ]
 
 with open(RHOA_FILE, "w") as fh:
     fh.write("%d\n" % npoints)
-    for (x, y, z) in coords:
-        d2 = (x - R0[0])**2 + (y - R0[1])**2 + (z - R0[2])**2
+    for x, y, z in coords:
+        d2 = (x - R0[0]) ** 2 + (y - R0[1]) ** 2 + (z - R0[2]) ** 2
         fh.write("%.10E\n" % math.exp(-ALPHA * d2))
 
 with open(RES_FILE, "w") as fh:
