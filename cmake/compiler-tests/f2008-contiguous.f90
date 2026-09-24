6ad94ecf8bf51bcd0124a73679d2182e466ea892
cheemschem <723598266@qq.com>
2026-09-24 17:14:59 +0800

    Remove obsolete GCC 8 FINDLOC fallback and test IS_CONTIGUOUS

--- cmake/compiler-tests/f2008-contiguous.f90
+++ cmake/compiler-tests/f2008-contiguous.f90
@@ -10,13 +10,19 @@
 program main
    implicit none
 
-   ! test whether the compiler supports the CONTIGUOUS keyword
+   ! test whether the compiler supports the CONTIGUOUS keyword and the
+   ! IS_CONTIGUOUS intrinsic, both part of the Fortran 2008 standard
    integer, allocatable, target :: targ(:)
    integer, contiguous, pointer :: ptr(:)
+   integer, target :: targ2d(10, 10)
 
    ! allocated data is always contiguous
    allocate (targ(10))
    ptr => targ
 
-   ! IS_CONTIGUOUS was implemented in gcc-9 and is therefore not tested for yet
+   ! IS_CONTIGUOUS has been available since GCC 9 and is required by CP2K
+   if (.not. IS_CONTIGUOUS(targ)) error stop 1
+   if (.not. IS_CONTIGUOUS(ptr)) error stop 1
+   ! a strided array section is not contiguous
+   if (IS_CONTIGUOUS(targ2d(1:10:2, 1))) error stop 2
 end program
